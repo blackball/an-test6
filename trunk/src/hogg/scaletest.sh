8157afda481d67bc579508acf6dea24296d74dd6
David W. Hogg <dwhogg@gmail.com>
2005-07-30 01:17:03 +0000

    rm

--- trunk/src/hogg/scaletest.sh
+++ trunk/src/hogg/scaletest.sh
@@ -4,11 +4,15 @@
 ../quads/startree -f allstarsweep ;
 
 # find lots of quads, saving std and err out
-numquads=100 ;
+\rm -rvf *?.[01].[013][013].?*
+numquads=10000000 ;
 for scale in 0.01 0.03 0.10 0.30 1.00
   do
+  echo $scale ;
   \ln -s allstarsweep.objs allstarsweep.$scale.objs ;
   \ln -s allstarsweep.skdt allstarsweep.$scale.skdt ;
-  echo "../quads/getquads -f allstarsweep.$scale -s $scale -q $numquads > getquads.$scale.log 2> getquads.$scale.errlog" ;
-  echo "../quads/quadidx -f allstarsweep.$scale > quadidx.$scale.log 2> quadidx.$scale.errlog" ;
+  echo running getquads ;
+  ../quads/getquads -f allstarsweep.$scale -s $scale -q $numquads > getquads.$scale.log 2> getquads.$scale.errlog ;
+  echo running quadidx ;
+  ../quads/quadidx -f allstarsweep.$scale > quadidx.$scale.log 2> quadidx.$scale.errlog ;
 done
