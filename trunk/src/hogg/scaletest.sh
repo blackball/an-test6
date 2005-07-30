4184f784e3baf0f27772c0a212ef7bf8a51e0c2d
David W. Hogg <dwhogg@gmail.com>
2005-07-30 01:33:13 +0000

    hogg

--- trunk/src/hogg/scaletest.sh
+++ trunk/src/hogg/scaletest.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # make tree for USNO-B1.0 stars
+echo running startree... ;
 ../quads/startree -f allstarsweep > startree.log 2> startree.errlog ;
+echo ...done ;
 
 # find lots of quads, saving std and err out
-\rm -rvf *?.[01].[013][013].?*
+\rm -rvf *?.0.000[013][013].?*
 numquads=10000000 ;
-for scale in 0.01 0.03 0.10 0.30 1.00
+for scale in 0.00001 0.00003 0.00010 0.00030
   do
-  echo $scale ;
+  echo scale = $scale ;
   \ln -s allstarsweep.objs allstarsweep.$scale.objs ;
   \ln -s allstarsweep.skdt allstarsweep.$scale.skdt ;
-  echo running getquads ;
+  echo running getquads... ;
   ../quads/getquads -f allstarsweep.$scale -s $scale -q $numquads > getquads.$scale.log 2> getquads.$scale.errlog ;
-  echo running quadidx ;
+  echo ...done ;
+  echo running quadidx... ;
   ../quads/quadidx -f allstarsweep.$scale > quadidx.$scale.log 2> quadidx.$scale.errlog ;
+  echo ...done ;
 done
-
