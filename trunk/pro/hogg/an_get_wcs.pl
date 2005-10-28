5645bf2cd292574fab2f10bd1ac6e416a1028295
David W. Hogg <dwhogg@gmail.com>
2005-10-28 07:51:06 +0000

    works on GALEX now, still not ready for prime time

--- trunk/pro/hogg/an_get_wcs.pl
+++ trunk/pro/hogg/an_get_wcs.pl
@@ -1,74 +1,80 @@
 #!/usr/bin/env perl
-
+#
+# ./an_get_wcs.pl ../../data/galex-field-100
+#
+my $parity= 0;
 my $name = $ARGV[0];
 my $input = `cat $name.hits`;
-my $parity= 0;
-@records = split "--------------------\n", $input;
-printf("set_plot, \'PS\'\n");
+my $minu= 0.0;
+my $minv= 0.0;
+my $maxu= 0.0;
+my $maxv= 0.0;
+printf("set_plot,'ps'\n");
 printf("device\n");
-foreach $record (@records){
-    if ($record =~ /field\s(\d+)\s/){
-	my $field = $1;
-	my $minu= 0.0;
-	my $minv= 0.0;
-	my $maxu= 0.0;
-	my $maxv= 0.0;
-	my $minx= 0.0;
-	my $miny= 0.0;
-	my $minz= 0.0;
-	my $maxx= 0.0;
-	my $maxy= 0.0;
-	my $maxz= 0.0;
+if ($input =~ /\s*parity_flip\s*=\s*True/s ){
+    $parity= 1;
+}
+if ($input =~ /\s*min_uv_corner\s*=\(([\-\d\.]+),([\-\d\.]+)\)/s ){
+    $minu= $1;
+    $minv= $2;
+}
+if ($input =~ /\s*max_uv_corner\s*=\(([\-\d\.]+),([\-\d\.]+)\)/s ){
+    $maxu= $1;
+    $maxv= $2;
+}
 
-	@lines = split "\n", $record;
-	foreach $line (@lines){
-	    if ($line =~ /min\s+uv\=\((\-?\d+.\d+)\,(\-*\d+.\d+)\)/){
-		$minu= $1;
-		$minv= $2;
-	    }
-	    if ($line =~ /max\s+uv\=\((\-?\d+.\d+)\,(\-*\d+.\d+)\)/){
-		$maxu= $1;
-		$maxv= $2;
-	    }
-	    if ($line =~ /min\s+xyz\=\((\-?\d+.\d+)\,(\-*\d+.\d+)\,(\-*\d+.\d+)\)/){
+if ($input =~ /\squads\s*=\s*\[(.+)\]/s ){
+    $input= $1;
+    my $minx= 0.0;
+    my $miny= 0.0;
+    my $minz= 0.0;
+    my $maxx= 0.0;
+    my $maxy= 0.0;
+    my $maxz= 0.0;
+    while ($input =~ /\s*dict\s*\((.+)/s ){
+	$input= $1;
+	if ($input =~ /\s*quad\s*=\s*(\d+)\,/){
+	    my $quadid= $1;
+	    if ($input =~ /\s*min_xyz\s*=\(([\-\d\.]+),([\-\d\.]+),([\-\d\.]+)\)/s ){
 		$minx+= $1;
 		$miny+= $2;
 		$minz+= $3;
 	    }
-	    if ($line =~ /max\s+xyz\=\((\-?\d+.\d+)\,(\-*\d+.\d+)\,(\-*\d+.\d+)\)/){
+	    if ($input =~ /\s*max_xyz\s*=\(([\-\d\.]+),([\-\d\.]+),([\-\d\.]+)\)/s ){
 		$maxx+= $1;
 		$maxy+= $2;
 		$maxz+= $3;
 	    }
 	}
+    }
+
+    my $minnorm= sqrt($minx*$minx+$miny*$miny+$minz*$minz);
+    if ($minnorm > 0.99){
+	$minx/= $minnorm;
+	$miny/= $minnorm;
+	$minz/= $minnorm;
+	my $maxnorm= sqrt($maxx*$maxx+$maxy*$maxy+$maxz*$maxz);
+	$maxx/= $maxnorm;
+	$maxy/= $maxnorm;
+	$maxz/= $maxnorm;
 	
-	$parity= 1;
-    
-	my $minnorm= sqrt($minx*$minx+$miny*$miny+$minz*$minz);
-	if ($minnorm > 0.99){
-	    $minx/= $minnorm;
-	    $miny/= $minnorm;
-	    $minz/= $minnorm;
-	    my $maxnorm= sqrt($maxx*$maxx+$maxy*$maxy+$maxz*$maxz);
-	    $maxx/= $maxnorm;
-	    $maxy/= $maxnorm;
-	    $maxz/= $maxnorm;
-	
-	    printf("!P.TITLE=\'field %d\'\n",$field);
-	    printf("; min u , v = %f , %f\n",$minu,$minv);
-	    printf("; min x , y , z = %f , %f , %f\n",$minx,$miny,$minz);
-	    printf("; max u , v = %f , %f\n",$maxu,$maxv);
-	    printf("; max x , y , z = %f , %f , %f\n",$maxx,$maxy,$maxz);
-	    printf("cat= mrdfits(\'%s.fits*\',%d)\n",$name,$field+1);
-	    printf("astr= an_get_wcs([[%f,%f,%f,%f,%f], \$\n",
-		   $minu,$minv,$minx,$miny,$minz);
-            printf("                  [%f,%f,%f,%f,%f]], \$\n",
-		   $maxu,$maxv,$maxx,$maxy,$maxz);
-	    printf("                 cat.rowc,cat.colc,siporder=2, \$\n");
-	    printf("                 parity=%d)\n",
-		   $parity);
-	    printf(";\n");
-	}
+	printf(";\n");
+	printf("!P.TITLE=\'field %d\'\n",$field);
+	printf("cat= mrdfits(\'%s.fits*\',%d)\n",$name,$field+1);
+	printf("astr= an_get_wcs([[%f,%f,%f,%f,%f], \$\n",
+	       $minu,$minv,$minx,$miny,$minz);
+	printf("                  [%f,%f,%f,%f,%f]], \$\n",
+	       $maxu,$maxv,$maxx,$maxy,$maxz);
+
+# for SDSS
+#	printf("                 cat.rowc,cat.colc,siporder=2, \$\n");
+
+# for GALEX
+	printf("                 cat.nuv_x,cat.nuv_y,siporder=3, \$\n");
+
+	printf("                 parity=%d)\n",
+	       $parity);
+	printf(";\n");
     }
 }
-printf("device, /close\n");
+printf("device,/close\n");
--- trunk/pro/hogg/an_get_wcs.pro
+++ trunk/pro/hogg/an_get_wcs.pro
@@ -40,8 +40,23 @@ astr= hogg_wcs_tweak(astr,uu,vv,siporder=siporder,jitter=2.0, $
                      nsigma=nsigma,usno=usno)
 astr= hogg_wcs_tweak(astr,uu,vv,siporder=siporder,jitter=2.0, $
                      nsigma=nsigma,usno=usno)
-plot, uu,vv,psym=1
-ad2xy, usno.ra,usno.dec,astr,xx,yy
-oplot, xx,yy,psym=6
+xy2ad, uu,vv,astr,ra,dec
+if (max(usno.ra)-min(usno.ra)) GT 180.0 then begin
+    bad= where(ra GT 180,nbad)
+    if (nbad GT 0) then ra[bad]= ra[bad]-360D0
+    bad= where(usno.ra GT 180,nbad)
+    if (nbad GT 0) then usno[bad].ra= usno[bad].ra-360D0
+endif
+hogg_plot_defaults
+xrange= weighted_quantile(usno.ra,quant=[0.95,0.05])
+yrange= weighted_quantile(usno.dec,quant=[0.05,0.95])
+plot, ra,dec,psym=6,symsize=0.3, $
+  xrange=xrange,yrange=yrange
+oplot, usno.ra,usno.dec,psym=1,symsize=0.3
+xrange= weighted_quantile(ra,quant=[0.75,0.25])
+yrange= weighted_quantile(dec,quant=[0.25,0.75])
+plot, ra,dec,psym=6,symsize=0.3, $
+  xrange=xrange,yrange=yrange
+oplot, usno.ra,usno.dec,psym=1,symsize=0.3
 return, astr
 end
