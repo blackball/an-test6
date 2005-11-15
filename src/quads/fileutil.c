c3e55ce2db5fa504cfa0734500f39e7bd60f185a
Sam Roweis <roweis@cs.nyu.edu>
2005-11-15 00:45:28 +0000

    solvexy now reads ascii again

--- src/quads/fileutil.c
+++ src/quads/fileutil.c
@@ -208,18 +208,28 @@ char read_quad_header(FILE *fid, qidx *numquads, sidx *numstars,
 
 xyarray *readxy(FILE *fid, char ParityFlip)
 {
+	char ASCII = 0;
 	qidx ii, jj, numxy, numfields;
 	magicval magic;
 	xyarray *thepix = NULL;
+	int tmpchar;
+
 	if (fread(&magic, sizeof(magic), 1, fid) != 1) {
 		fprintf(stderr, "ERROR (readxy) -- bad magic value in field file.\n");
 		return ((xyarray *)NULL);
 	}
-	{
+	if (magic == ASCII_VAL) {
+		ASCII = 1;
+		if (fscanf(fid, "mFields=%lu\n", &numfields) != 1) {
+			fprintf(stderr, "ERROR (readxy) -- bad first line in field file.\n");
+			return ((xyarray *)NULL);
+		}
+	} else {
 		if (magic != MAGIC_VAL) {
 			fprintf(stderr, "ERROR (readxy) -- bad magic value in field file.\n");
 			return ((xyarray *)NULL);
 		}
+		ASCII = 0;
 		if (fread(&numfields, sizeof(numfields), 1, fid) != 1) {
 			fprintf(stderr, "ERROR (readxy) -- bad numfields fread in field file.\n");
 			return ((xyarray *)NULL);
@@ -227,14 +237,30 @@ xyarray *readxy(FILE *fid, char ParityFlip)
 	}
 	thepix = mk_xyarray(numfields);
 	for (ii = 0;ii < numfields;ii++) {
-	  fread(&numxy, sizeof(numxy), 1, fid); // CHECK THE RETURN VALUE MORON!
-	  thepix->array[ii] = mk_xy(numxy);
+		if (ASCII) {
+			tmpchar = fgetc(fid);
+			while (tmpchar == COMMENT_CHAR) {
+				fscanf(fid, "%*[^\n]");
+				fgetc(fid);
+				tmpchar = fgetc(fid);
+			}
+			ungetc(tmpchar, fid);
+			fscanf(fid, "%lu", &numxy); // CHECK THE RETURN VALUE MORON!
+		} else
+			fread(&numxy, sizeof(numxy), 1, fid); // CHECK THE RETURN VALUE MORON!
+		thepix->array[ii] = mk_xy(numxy);
 		if (xya_ref(thepix, ii) == NULL) {
 			fprintf(stderr, "ERROR (readxy) - out of memory at field %lu\n", ii);
 			free_xyarray(thepix);
 			return (xyarray *)NULL;
 		}
-		fread(thepix->array[ii]->farr, sizeof(double), DIM_XY*numxy, fid);
+		if (ASCII) {
+			for (jj = 0;jj < numxy;jj++)
+				fscanf(fid, ",%lf,%lf", (thepix->array[ii]->farr) + 2*jj,
+				       (thepix->array[ii]->farr) + 2*jj + 1 );
+			fscanf(fid, "\n");
+		} else
+			fread(thepix->array[ii]->farr, sizeof(double), DIM_XY*numxy, fid);
 
 		if (ParityFlip) {
 			double swaptmp;
@@ -246,6 +272,7 @@ xyarray *readxy(FILE *fid, char ParityFlip)
 			}
 		}
 	}
+
 	return thepix;
 }
 
