68f02103e2beef90b889a04456f226a69d87e28c
Dustin Lang <dstndstn@gmail.com>
2006-01-20 20:21:13 +0000

    fix memory leak.

--- trunk/src/quads/codedensity.c
+++ trunk/src/quads/codedensity.c
@@ -13,136 +13,129 @@
 #include "dualtree.h"
 #include "dualtree_max.h"
 
-#define OPTIONS "ht:"
+#define OPTIONS "ht:c"
 
 extern char *optarg;
 extern int optind, opterr, optopt;
 
 void print_help(char* progname) {
-    fprintf(stderr, "Usage: %s -t <tree-file>\n\n", progname);
+    fprintf(stderr, "Usage: %s -t <tree-file> [-c]\n\n-c = check results against naive search.",
+	    progname);
 }
 
 inline double square(double d) {
     return d*d;
 }
 
-int Nbuckets;
-//int bucket_sum;
-int* bucket_counts;
-double* bucket_mindist2;
-double* bucket_maxdist2;
+/*
+  ;
+  This code was for looking at the distances of ALL pairs of points.
+   
+  int Nbuckets;
+  //int bucket_sum;
+  int* bucket_counts;
+  double* bucket_mindist2;
+  double* bucket_maxdist2;
 
-void pointwise_result(node* n1, node* n2) {
-    int i, j, k;
-    if (node_is_leaf(n1) && node_is_leaf(n2)) {
-	for (i=0; i<n1->num_points; i++) {
-	    dyv* p1 = dyv_array_ref(n1->points, i);
-	    for (j=0; j<n2->num_points; j++) {
-		double dist2;
-		dyv* p2 = dyv_array_ref(n2->points, j);
-		dist2 = dyv_dyv_dsqd(p1, p2);
-		for (k=0; k<Nbuckets; k++) {
-		    if ((dist2 <= bucket_maxdist2[k]) &&
-			(dist2 >= bucket_mindist2[k])) {
+  void pointwise_result(node* n1, node* n2) {
+  int i, j, k;
+  if (node_is_leaf(n1) && node_is_leaf(n2)) {
+  for (i=0; i<n1->num_points; i++) {
+  dyv* p1 = dyv_array_ref(n1->points, i);
+  for (j=0; j<n2->num_points; j++) {
+  double dist2;
+  dyv* p2 = dyv_array_ref(n2->points, j);
+  dist2 = dyv_dyv_dsqd(p1, p2);
+  for (k=0; k<Nbuckets; k++) {
+  if ((dist2 <= bucket_maxdist2[k]) &&
+  (dist2 >= bucket_mindist2[k])) {
 
-			bucket_counts[k]++;
-			/*
-			  bucket_sum++;
-			  printf("points: bucket %i (%i total)\n",
-			  k, bucket_sum);
-			*/
-			break;
-		    }
-		}
-	    }
-	}
-    } else if (!node_is_leaf(n1)) {
-	pointwise_result(n1->child1, n2);
-	pointwise_result(n1->child2, n2);
-    } else { //if (!node_is_leaf(n2)) {
-	pointwise_result(n1, n2->child1);
-	pointwise_result(n1, n2->child2);
-    }
-}
+  bucket_counts[k]++;
+  //bucket_sum++;
+  //printf("points: bucket %i (%i total)\n", k, bucket_sum);
+  break;
+  }
+  }
+  }
+  }
+  } else if (!node_is_leaf(n1)) {
+  pointwise_result(n1->child1, n2);
+  pointwise_result(n1->child2, n2);
+  } else { //if (!node_is_leaf(n2)) {
+  pointwise_result(n1, n2->child1);
+  pointwise_result(n1, n2->child2);
+  }
+  }
 
-void result(void* extra, node* n1, node* n2) {
-    double min2, max2;
-    int i;
-    min2 = hrect_hrect_min_dsqd(n1->box, n2->box);
-    max2 = hrect_hrect_max_dsqd(n1->box, n2->box);
-    /*
-      printf("result: %i x %i: bounds: [ %g, %g ].\n",
-      n1->num_points, n2->num_points, sqrt(min2),
-      sqrt(max2));
-    */
-    for (i=0; i<Nbuckets; i++) {
-	if ((max2 <= bucket_maxdist2[i]) &&
-	    (min2 >= bucket_mindist2[i])) {
-	    // each pair of particles in these nodes have
-	    // distances that fall inside bucket i.
-	    bucket_counts[i] += (n1->num_points *
-				 n2->num_points);
-	    /*
-	      bucket_sum += (n1->num_points * n2->num_points);
-	      printf("bucket %i: %i x %i (%i total)\n", i,
-	      n1->num_points, n2->num_points,
-	      bucket_sum);
-	    */
-	    return;
-	}
-    }
-    printf("no match.\n");
-    pointwise_result(n1, n2);
-}
+  void result(void* extra, node* n1, node* n2) {
+  double min2, max2;
+  int i;
+  min2 = hrect_hrect_min_dsqd(n1->box, n2->box);
+  max2 = hrect_hrect_max_dsqd(n1->box, n2->box);
+  debug("result: %i x %i: bounds: [ %g, %g ].\n",
+  n1->num_points, n2->num_points, sqrt(min2),
+  sqrt(max2));
+  for (i=0; i<Nbuckets; i++) {
+  if ((max2 <= bucket_maxdist2[i]) &&
+  (min2 >= bucket_mindist2[i])) {
+  // each pair of particles in these nodes have
+  // distances that fall inside bucket i.
+  bucket_counts[i] += (n1->num_points *
+  n2->num_points);
+  //bucket_sum += (n1->num_points * n2->num_points);
+  //printf("bucket %i: %i x %i (%i total)\n", i, n1->num_points, n2->num_points, bucket_sum);
+  return;
+  }
+  }
+  printf("no match.\n");
+  pointwise_result(n1, n2);
+  }
 
-bool within_range(void* vparams, node* n1, node* n2) {
-    double min2, max2;
-    int i;
-    min2 = hrect_hrect_min_dsqd(n1->box, n2->box);
-    max2 = hrect_hrect_max_dsqd(n1->box, n2->box);
-    /*
-      printf("%i x %i: bounds: [ %g, %g ].\n",
-      n1->num_points, n2->num_points, sqrt(min2),
-      sqrt(max2));
-    */
-    for (i=0; i<Nbuckets; i++) {
-	if ((max2 <= bucket_maxdist2[i]) &&
-	    (min2 >= bucket_mindist2[i])) {
-	    // each pair of particles in these nodes have
-	    // distances that fall inside bucket i.
-	    bucket_counts[i] += (n1->num_points *
-				 n2->num_points);
-	    //printf("bucket %i\n", i);
-	    // no need to recurse.
-	    return FALSE;
-	}
-    }
-    /*
-      printf("no match: (leaf1,leaf2) = ( %c , %c )\n",
-      slow_node_is_leaf(n1) ? 'y':'n',
-      slow_node_is_leaf(n2) ? 'y':'n');
-      if (n1->child1) {
-      printf("n1->child1 has %i points.\n", n1->child1->num_points);
-      }
-      if (n1->child2) {
-      printf("n1->child2 has %i points.\n", n1->child2->num_points);
-      }
-      if (n2->child1) {
-      printf("n2->child1 has %i points.\n", n2->child1->num_points);
-      }
-      if (n2->child2) {
-      printf("n2->child2 has %i points.\n", n2->child2->num_points);
-      }
-    */
-    /*
-      if (node_is_leaf(n1) || node_is_leaf(n2)) {
-      // HACK - compute individual distances.
-      printf("Warning: one node is a leaf, but the bounds don't fall "
-      "within a bucket.\n");
-      }
-    */
-    return TRUE;
-}
+  bool within_range(void* vparams, node* n1, node* n2) {
+  double min2, max2;
+  int i;
+  min2 = hrect_hrect_min_dsqd(n1->box, n2->box);
+  max2 = hrect_hrect_max_dsqd(n1->box, n2->box);
+  debug("%i x %i: bounds: [ %g, %g ].\n",
+  n1->num_points, n2->num_points, sqrt(min2),
+  sqrt(max2));
+  for (i=0; i<Nbuckets; i++) {
+  if ((max2 <= bucket_maxdist2[i]) &&
+  (min2 >= bucket_mindist2[i])) {
+  // each pair of particles in these nodes have
+  // distances that fall inside bucket i.
+  bucket_counts[i] += (n1->num_points *
+  n2->num_points);
+  //printf("bucket %i\n", i);
+  // no need to recurse.
+  return FALSE;
+  }
+  }
+  #if 0
+  printf("no match: (leaf1,leaf2) = ( %c , %c )\n",
+  slow_node_is_leaf(n1) ? 'y':'n',
+  slow_node_is_leaf(n2) ? 'y':'n');
+  if (n1->child1) {
+  printf("n1->child1 has %i points.\n", n1->child1->num_points);
+  }
+  if (n1->child2) {
+  printf("n1->child2 has %i points.\n", n1->child2->num_points);
+  }
+  if (n2->child1) {
+  printf("n2->child1 has %i points.\n", n2->child1->num_points);
+  }
+  if (n2->child2) {
+  printf("n2->child2 has %i points.\n", n2->child2->num_points);
+  }
+  if (node_is_leaf(n1) || node_is_leaf(n2)) {
+  // HACK - compute individual distances.
+  printf("Warning: one node is a leaf, but the bounds don't fall "
+  "within a bucket.\n");
+  }
+  #endif
+  return TRUE;
+  }
+*/
 
 void bounds_function(void* extra, node* query, node* search, double thresh,
 		     double* lower, double* upper) {
@@ -166,7 +159,6 @@ int* bests;
 void max_start_results(void* extra, node* query, blocklist* leaves, double* pruning_thresh) {
     int i, N;
     //printf("start results for %p: threshold %g\n", query, *pruning_thresh);
-
     N = query->num_points;
     pruning_threshs = (double*)malloc(N * sizeof(double));
     bests = (int*)malloc(N * sizeof(int));
@@ -232,7 +224,7 @@ void max_results(void* extra, node* query, node* search, double* pruning_thresho
 
 void write_histogram(char* fn) {
     FILE* fout;
-    int Nbins = 100;
+    int Nbins = 200;
     int i;
     double bin_size;
     int* bincounts;
@@ -282,6 +274,7 @@ void write_histogram(char* fn) {
 
     fclose(fout);
     printf("Wrote file %s\n", fn);
+    fflush(stdout);
 }
 
 void max_end_results(void* extra, node* query) {
@@ -313,6 +306,7 @@ void max_end_results(void* extra, node* query) {
 	    }
 	}
 	printf("%i done.  Range [%g, %g]\n", Ndone+N, mn, mx);
+	fflush(stdout);
     }
     if ((Ndone+N)/100000 > (Ndone/100000)) {
 	// write partial result.
@@ -335,7 +329,7 @@ int main(int argc, char *argv[]) {
       double maxdist2;
       dualtree_callbacks callbacks;
     */
-    dyv_array* array;
+    dyv_array* array = NULL;
     bool check = FALSE;
 
     if (argc <= 2) {
@@ -345,6 +339,9 @@ int main(int argc, char *argv[]) {
 
     while ((argchar = getopt (argc, argv, OPTIONS)) != -1)
 	switch (argchar) {
+	case 'c':
+	    check = TRUE;
+	    break;
 	case 't':
 	    treefname = optarg;
 	    break;
@@ -371,7 +368,6 @@ int main(int argc, char *argv[]) {
     fprintf(stdout, "done\n    (%d points, %d nodes, depth %d).\n",
 	    kdtree_num_points(tree), kdtree_num_nodes(tree),
 	    kdtree_max_depth(tree));
-
 
     if (check) {
 	array = mk_dyv_array_from_kdtree(tree);
@@ -407,6 +403,8 @@ int main(int argc, char *argv[]) {
 	double best;
 	int i, j;
 	double eps = 1e-15;
+	printf("Checking against exhaustive search...\n");
+	fflush(stdout);
 	for (i=0; i<N; i++) {
 	    best = 1e300;
 	    dyv* point1 = dyv_array_ref(array, i);
@@ -440,6 +438,8 @@ int main(int argc, char *argv[]) {
     */
 
     write_histogram("/tmp/nndists.m");
+
+    free(nndists);
 
     free_kdtree(tree);
     return 0;
