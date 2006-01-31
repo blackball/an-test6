657f8fa43160b999574245d58d8df017ff492d01
Dustin Lang <dstndstn@gmail.com>
2006-01-31 18:35:01 +0000

    * realized a stupid mistake: we can't do deduplication during 'startree' because the index of stars in the startree array must correspond to their positions in the catalogue.
    * add -k (keep) option to 'deduplicate' to avoid finding
    nearest-neighbours on the whole catalogue rather than the cut in which we
    are interested.
    
    * add "createindex" and "makeobjlinks" scripts, which are used during the
    index-creation phase.

--- trunk/src/quads/startree.c
+++ trunk/src/quads/startree.c
@@ -3,18 +3,23 @@
 #include "starutil.h"
 #include "kdutil.h"
 #include "fileutil.h"
-#include "dualtree_rangesearch.h"
-#include "blocklist.h"
+/*
+  #include "dualtree_rangesearch.h"
+  #include "blocklist.h"
+*/
 
 #include "KD/amma.h"
 
-#define OPTIONS "hR:f:k:d:"
+#define OPTIONS "hR:f:k:"
+//d:"
 const char HelpString[] =
     "startree -f fname [-R KD_RMIN] [-k keep] [-d radius]\n"
     "  KD_RMIN: (default 50) is the max# points per leaf in KD tree\n"
-    "  keep: is the number of stars read from the catalogue\n"
-    "  radius: (in arcseconds) is the de-duplication radius: a star found within\n"
-    "      this radius of another star will be discarded\n";
+"  keep: is the number of stars read from the catalogue\n";
+/*
+  "  radius: (in arcseconds) is the de-duplication radius: a star found within\n"
+  "      this radius of another star will be discarded\n";
+*/
 
 char *treefname = NULL;
 char *catfname = NULL;
@@ -22,10 +27,19 @@ char *catfname = NULL;
 extern char *optarg;
 extern int optind, opterr, optopt;
 
+/*
+  I used to have de-duplication incorporated into the startree process,
+  but we can't just remove the duplicates from the array of stars, because
+  the position of a star in the array is significant: it's assumed to be the
+  same as the position of the star in the .objs catalogue.
+  Therefore, we instead have to do de-duplication of the objs file, creating
+  a fresh objs file with consistent indices.
+*/
 
-blocklist* duplicates = NULL;
-void duplicate_found(void* nil, int ind1, int ind2, double dist2);
-
+/*
+  blocklist* duplicates = NULL;
+  void duplicate_found(void* nil, int ind1, int ind2, double dist2);
+*/
 
 int main(int argc, char *argv[])
 {
@@ -38,7 +52,7 @@ int main(int argc, char *argv[])
 	stararray *thestars = NULL;
 	kdtree *starkd = NULL;
 	int nkeep = 0;
-	double duprad = -1.0;
+	//double duprad = -1.0;
 
 	if (argc <= 2) {
 		fprintf(stderr, HelpString);
@@ -57,13 +71,15 @@ int main(int argc, char *argv[])
 			exit(-1);
 		  }
 		  break;
-		case 'd':
+		  /*
+		    case 'd':
 		    duprad = atof(optarg);
 		    if (duprad < 0.0) {
-			printf("Couldn't parse \'radius\': \"%s\"\n", optarg);
-			exit(-1);
+		    printf("Couldn't parse \'radius\': \"%s\"\n", optarg);
+		    exit(-1);
 		    }
 		    break;
+		  */
 		case 'f':
 			treefname = mk_streefn(optarg);
 			catfname = mk_catfn(optarg);
@@ -112,106 +128,108 @@ int main(int argc, char *argv[])
 	fprintf(stderr, "done (%d nodes, depth %d).\n",
 	        starkd->num_nodes, starkd->max_depth);
 
+	/*
+	  while (duprad >= 0.0) {
+	  // convert from arcseconds to distance on the unit sphere.
+	  double radians;
+	  double dist;
+	  radians = (duprad / (60.0 * 60.0)) * (M_PI / 180.0);
+	  dist = sqrt(2.0*(1.0 - cos(radians)));
 
-	while (duprad >= 0.0) {
-	    // convert from arcseconds to distance on the unit sphere.
-	    double radians;
-	    double dist;
-	    radians = (duprad / (60.0 * 60.0)) * (M_PI / 180.0);
-	    dist = sqrt(2.0*(1.0 - cos(radians)));
+	  duplicates = blocklist_int_new(256);
 
-	    duplicates = blocklist_int_new(256);
+	  // de-duplicate.
+	  dualtree_rangesearch(starkd, starkd,
+	  RANGESEARCH_NO_LIMIT, dist,
+	  duplicate_found, NULL);
 
-	    // de-duplicate.
-	    dualtree_rangesearch(starkd, starkd,
-				 RANGESEARCH_NO_LIMIT, dist,
-				 duplicate_found, NULL);
+	  if (blocklist_count(duplicates) == 0) {
+	  fprintf(stderr, "No duplicate stars found.\n");
+	  blocklist_int_free(duplicates);
+	  duplicates = NULL;
+	  break;
+	  }
 
-	    if (blocklist_count(duplicates) == 0) {
-		fprintf(stderr, "No duplicate stars found.\n");
-		blocklist_int_free(duplicates);
-		duplicates = NULL;
-		break;
-	    }
+	  free_kdtree(starkd);
+	  starkd = NULL;
 
-	    free_kdtree(starkd);
-	    starkd = NULL;
+	  // remove duplicate entries from the star array.
+	  fprintf(stderr, "Removing %i duplicate stars (%i stars remain)...\n",
+	  blocklist_count(duplicates), dyv_array_size(thestars) - blocklist_count(duplicates));
+	  fflush(stderr);
+	  {
+	  int dupind = 0;
+	  int skipind = -1;
+	  int srcind;
+	  int destind = 0;
+	  int i, N;
 
-	    // remove duplicate entries from the star array.
-	    fprintf(stderr, "Removing %i duplicate stars (%i stars remain)...\n",
-		    blocklist_count(duplicates), dyv_array_size(thestars) - blocklist_count(duplicates));
-	    fflush(stderr);
-	    {
-		int dupind = 0;
-		int skipind = -1;
-		int srcind;
-		int destind = 0;
-		int i, N;
+	  // remove repeated entries from "duplicates"
+	  // (yes, it can happen)
+	  for (i=0; i<blocklist_count(duplicates)-1; i++) {
+	  int a, b;
+	  a = blocklist_int_access(duplicates, i);
+	  b = blocklist_int_access(duplicates, i+1);
+	  if (a == b) {
+	  blocklist_remove_index(duplicates, i);
+	  i--;
+	  }
+	  }
 
-		// remove repeated entries from "duplicates"
-		// (yes, it can happen)
-		for (i=0; i<blocklist_count(duplicates)-1; i++) {
-		    int a, b;
-		    a = blocklist_int_access(duplicates, i);
-		    b = blocklist_int_access(duplicates, i+1);
-		    if (a == b) {
-			blocklist_remove_index(duplicates, i);
-			i--;
-		    }
-		}
+	  N = dyv_array_size(thestars);
+	  for (srcind=0; srcind<N; srcind++) {
+	  // find the next index to skip
+	  if (skipind == -1) {
+	  if (dupind < blocklist_count(duplicates)) {
+	  skipind = blocklist_int_access(duplicates, dupind);
+	  dupind++;
+	  }
+	  // shouldn't happen, I think...
+	  if ((skipind > 0) && (skipind < srcind)) {
+	  fprintf(stderr, "Warning: duplicate index %i skipped.  ", skipind);
+	  fprintf(stderr, "skipind=%i, srcind=%i\n", skipind, srcind);
+	  skipind = -1;
+	  }
+	  }
+	  if (srcind == skipind) {
+	  dyv* v;
+	  v = dyv_array_ref(thestars, srcind);
+	  thestars->array[srcind] = NULL;
+	  free_dyv(v);
+	  skipind = -1;
+	  continue;
+	  }
 
-		N = dyv_array_size(thestars);
-		for (srcind=0; srcind<N; srcind++) {
-		    // find the next index to skip
-		    if (skipind == -1) {
-			if (dupind < blocklist_count(duplicates)) {
-			    skipind = blocklist_int_access(duplicates, dupind);
-			    dupind++;
-			}
-			// shouldn't happen, I think...
-			if ((skipind > 0) && (skipind < srcind)) {
-			    fprintf(stderr, "Warning: duplicate index %i skipped.  ", skipind);
-			    fprintf(stderr, "skipind=%i, srcind=%i\n", skipind, srcind);
-			    skipind = -1;
-			}
-		    }
-		    if (srcind == skipind) {
-			dyv* v;
-			v = dyv_array_ref(thestars, srcind);
-			thestars->array[srcind] = NULL;
-			free_dyv(v);
-			skipind = -1;
-			continue;
-		    }
+	  // optimization: if source and dest are the same...
+	  if (srcind == destind) {
+	  destind++;
+	  continue;
+	  }
 
-		    // optimization: if source and dest are the same...
-		    if (srcind == destind) {
-			destind++;
-			continue;
-		    }
+	  thestars->array[destind] = dyv_array_ref(thestars, srcind);
 
-		    thestars->array[destind] = dyv_array_ref(thestars, srcind);
+	  destind++;
+	  }
+	  thestars->size -= blocklist_count(duplicates);
+	  }
 
-		    destind++;
-		}
-		thestars->size -= blocklist_count(duplicates);
-	    }
+	  blocklist_int_free(duplicates);
+	  duplicates = NULL;
 
-	    blocklist_int_free(duplicates);
-	    duplicates = NULL;
+	  fprintf(stderr, "Rebuilding star KD tree...");
+	  fflush(stderr);
+	  starkd = mk_starkdtree(thestars, kd_Rmin);
 
-	    fprintf(stderr, "Rebuilding star KD tree...");
-	    fflush(stderr);
-	    starkd = mk_starkdtree(thestars, kd_Rmin);
+	  if (starkd == NULL) {
+	  return 2;
+	  }
+	  fprintf(stderr, "done (%d nodes, depth %d).\n",
+	  starkd->num_nodes, starkd->max_depth);
 
-	    if (starkd == NULL) {
-		return 2;
-	    }
-	    fprintf(stderr, "done (%d nodes, depth %d).\n",
-		    starkd->num_nodes, starkd->max_depth);
+	  break;
+	  }
+	*/
 
-	    break;
-	}
 	free_stararray(thestars);
 
 	fprintf(stderr, "  Writing star KD tree to %s...", treefname);
@@ -224,7 +242,6 @@ int main(int argc, char *argv[])
 
 	free_kdtree(starkd);
 
-	//basic_am_malloc_report();
 #if !defined(USE_OS_MEMORY_MANAGEMENT)
 	// free Andrew Moore's memory pool (so Valgrind works)
 	am_free_to_os();
@@ -232,10 +249,12 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void duplicate_found(void* nil, int ind1, int ind2, double dist2) {
-    if (ind1 <= ind2) return;
-    // append the larger of the two.
-    blocklist_int_insert_ascending(duplicates, ind1);
-}
+/*
+  void duplicate_found(void* nil, int ind1, int ind2, double dist2) {
+  if (ind1 <= ind2) return;
+  // append the larger of the two.
+  blocklist_int_insert_ascending(duplicates, ind1);
+  }
+*/
 
 
