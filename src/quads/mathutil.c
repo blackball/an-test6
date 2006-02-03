32a1b825c70841113011e4233f4237238eeb1398
xxx <nobody@astrometry.net>
2006-02-03 18:25:25 +0000

    Fix pointer insanity

--- src/quads/mathutil.c
+++ src/quads/mathutil.c
@@ -168,15 +168,15 @@ double *fit_transform(xy *ABCDpix, char order, star *A, star *B, star *C, star *
 	vv = Av * Av + Bv * Bv + Cv * Cv + Dv * Dv;
 	sumu = Au + Bu + Cu + Du;
 	sumv = Av + Bv + Cv + Dv;
-	*(matQ + 0) = uu;
-	*(matQ + 1) = uv;
-	*(matQ + 2) = sumu;
-	*(matQ + 3) = uv;
-	*(matQ + 4) = vv;
-	*(matQ + 5) = sumv;
-	*(matQ + 6) = sumu;
-	*(matQ + 7) = sumv;
-	*(matQ + 8) = 4.0;
+	matQ[0] = uu;
+	matQ[1] = uv;
+	matQ[2] = sumu;
+	matQ[3] = uv;
+	matQ[4] = vv;
+	matQ[5] = sumv;
+	matQ[6] = sumu;
+	matQ[7] = sumv;
+	matQ[8] = 4.0;
 
 	// take the inverse of Q in-place, so Q=inv(M*M')
 	det = inverse_3by3(matQ);
@@ -191,45 +191,45 @@ double *fit_transform(xy *ABCDpix, char order, star *A, star *B, star *C, star *
 	}
 
 	// set R to be the 4x3 matrix M'*inv(M*M')=M'*Q
-	*(matR + 0) = *(matQ + 0) * Au + *(matQ + 3) * Av + *(matQ + 6);
-	*(matR + 1) = *(matQ + 1) * Au + *(matQ + 4) * Av + *(matQ + 7);
-	*(matR + 2) = *(matQ + 2) * Au + *(matQ + 5) * Av + *(matQ + 8);
-	*(matR + 3) = *(matQ + 0) * Bu + *(matQ + 3) * Bv + *(matQ + 6);
-	*(matR + 4) = *(matQ + 1) * Bu + *(matQ + 4) * Bv + *(matQ + 7);
-	*(matR + 5) = *(matQ + 2) * Bu + *(matQ + 5) * Bv + *(matQ + 8);
-	*(matR + 6) = *(matQ + 0) * Cu + *(matQ + 3) * Cv + *(matQ + 6);
-	*(matR + 7) = *(matQ + 1) * Cu + *(matQ + 4) * Cv + *(matQ + 7);
-	*(matR + 8) = *(matQ + 2) * Cu + *(matQ + 5) * Cv + *(matQ + 8);
-	*(matR + 9) = *(matQ + 0) * Du + *(matQ + 3) * Dv + *(matQ + 6);
-	*(matR + 10) = *(matQ + 1) * Du + *(matQ + 4) * Dv + *(matQ + 7);
-	*(matR + 11) = *(matQ + 2) * Du + *(matQ + 5) * Dv + *(matQ + 8);
+	matR[0]  = matQ[0] * Au + matQ[3] * Av + matQ[6];
+	matR[1]  = matQ[1] * Au + matQ[4] * Av + matQ[7];
+	matR[2]  = matQ[2] * Au + matQ[5] * Av + matQ[8];
+	matR[3]  = matQ[0] * Bu + matQ[3] * Bv + matQ[6];
+	matR[4]  = matQ[1] * Bu + matQ[4] * Bv + matQ[7];
+	matR[5]  = matQ[2] * Bu + matQ[5] * Bv + matQ[8];
+	matR[6]  = matQ[0] * Cu + matQ[3] * Cv + matQ[6];
+	matR[7]  = matQ[1] * Cu + matQ[4] * Cv + matQ[7];
+	matR[8]  = matQ[2] * Cu + matQ[5] * Cv + matQ[8];
+	matR[9]  = matQ[0] * Du + matQ[3] * Dv + matQ[6];
+	matR[10] = matQ[1] * Du + matQ[4] * Dv + matQ[7];
+	matR[11] = matQ[2] * Du + matQ[5] * Dv + matQ[8];
 
 	// set Q to be the 3x3 matrix X*R
 
-	*(matQ + 0) = star_ref(A, 0) * (*(matR + 0)) + star_ref(B, 0) * (*(matR + 3)) +
-	              star_ref(C, 0) * (*(matR + 6)) + star_ref(D, 0) * (*(matR + 9));
-	*(matQ + 1) = star_ref(A, 0) * (*(matR + 1)) + star_ref(B, 0) * (*(matR + 4)) +
-	              star_ref(C, 0) * (*(matR + 7)) + star_ref(D, 0) * (*(matR + 10));
-	*(matQ + 2) = star_ref(A, 0) * (*(matR + 2)) + star_ref(B, 0) * (*(matR + 5)) +
-	              star_ref(C, 0) * (*(matR + 8)) + star_ref(D, 0) * (*(matR + 11));
+	matQ[0] = star_ref(A, 0) * matR[0] + star_ref(B, 0) * matR[3] +
+	          star_ref(C, 0) * matR[6] + star_ref(D, 0) * matR[9];
+	matQ[1] = star_ref(A, 0) * matR[1] + star_ref(B, 0) * matR[4] +
+	          star_ref(C, 0) * matR[7] + star_ref(D, 0) * matR[10];
+	matQ[2] = star_ref(A, 0) * matR[2] + star_ref(B, 0) * matR[5] +
+	          star_ref(C, 0) * matR[8] + star_ref(D, 0) * matR[11];
 
-	*(matQ + 3) = star_ref(A, 1) * (*(matR + 0)) + star_ref(B, 1) * (*(matR + 3)) +
-	              star_ref(C, 1) * (*(matR + 6)) + star_ref(D, 1) * (*(matR + 9));
-	*(matQ + 4) = star_ref(A, 1) * (*(matR + 1)) + star_ref(B, 1) * (*(matR + 4)) +
-	              star_ref(C, 1) * (*(matR + 7)) + star_ref(D, 1) * (*(matR + 10));
-	*(matQ + 5) = star_ref(A, 1) * (*(matR + 2)) + star_ref(B, 1) * (*(matR + 5)) +
-	              star_ref(C, 1) * (*(matR + 8)) + star_ref(D, 1) * (*(matR + 11));
+	matQ[3] = star_ref(A, 1) * matR[0] + star_ref(B, 1) * matR[3] +
+	          star_ref(C, 1) * matR[6] + star_ref(D, 1) * matR[9];
+	matQ[4] = star_ref(A, 1) * matR[1] + star_ref(B, 1) * matR[4] +
+	          star_ref(C, 1) * matR[7] + star_ref(D, 1) * matR[10];
+	matQ[5] = star_ref(A, 1) * matR[2] + star_ref(B, 1) * matR[5] +
+	          star_ref(C, 1) * matR[8] + star_ref(D, 1) * matR[11];
 
-	*(matQ + 6) = star_ref(A, 2) * (*(matR + 0)) + star_ref(B, 2) * (*(matR + 3)) +
-	              star_ref(C, 2) * (*(matR + 6)) + star_ref(D, 2) * (*(matR + 9));
-	*(matQ + 7) = star_ref(A, 2) * (*(matR + 1)) + star_ref(B, 2) * (*(matR + 4)) +
-	              star_ref(C, 2) * (*(matR + 7)) + star_ref(D, 2) * (*(matR + 10));
-	*(matQ + 8) = star_ref(A, 2) * (*(matR + 2)) + star_ref(B, 2) * (*(matR + 5)) +
-	              star_ref(C, 2) * (*(matR + 8)) + star_ref(D, 2) * (*(matR + 11));
+	matQ[6] = star_ref(A, 2) * matR[0] + star_ref(B, 2) * matR[3] +
+	          star_ref(C, 2) * matR[6] + star_ref(D, 2) * matR[9];
+	matQ[7] = star_ref(A, 2) * matR[1] + star_ref(B, 2) * matR[4] +
+	          star_ref(C, 2) * matR[7] + star_ref(D, 2) * matR[10];
+	matQ[8] = star_ref(A, 2) * matR[2] + star_ref(B, 2) * matR[5] +
+		  star_ref(C, 2) * matR[8] + star_ref(D, 2) * matR[11];
 
 	{
 		double* copyQ = (double*)malloc(9 * sizeof(double));
-		memset(copyQ, matQ, 9 * sizeof(double));
+		memcpy(copyQ, matQ, 9 * sizeof(double));
 		return copyQ;
 	}
 	//return (matQ);
