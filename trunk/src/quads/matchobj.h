df2ab3760b0c596178e2a62241011b3b8f6ebc5e
Dustin Lang <dstndstn@gmail.com>
2006-02-08 21:54:50 +0000

    oops, forgot to add matchobj.h

---
+++ trunk/src/quads/matchobj.h
@@ -0,0 +1,33 @@
+#ifndef MATCHOBJ_H_
+#define MATCHOBJ_H_
+
+struct match_struct {
+    qidx quadno;
+    sidx iA, iB, iC, iD;
+    sidx fA, fB, fC, fD;
+    double code_err;
+    star *sMin, *sMax;
+    double vector[6];
+
+	double* transform;
+	/*
+	  double corners[4];
+	  double starA[3];
+	  double starB[3];
+	  double starC[3];
+	  double starD[3];
+	  double fieldA[2];
+	  double fieldB[2];
+	  double fieldC[2];
+	  double fieldD[2];
+	  int abcdorder;
+	*/
+};
+typedef struct match_struct MatchObj;
+
+#define MATCH_VECTOR_SIZE 6
+
+#define mk_MatchObj() ((MatchObj *)malloc(sizeof(MatchObj)))
+#define free_MatchObj(m) free(m)
+
+#endif
