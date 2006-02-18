d33660c503f63254fac67629680ea6fcb75849cd
Dustin Lang <dstndstn@gmail.com>
2006-02-18 23:03:30 +0000

    remove ancient debugging code

--- src/quads/hitlist_agreement.c
+++ src/quads/hitlist_agreement.c
@@ -35,7 +35,6 @@ hitlist* hitlist_new() {
 }
 
 void hitlist_free(hitlist* hl) {
-	//hitlist_clear(hl);
 	blocklist_free(hl);
 }
 
@@ -85,7 +84,6 @@ blocklist* hitlist_get_best(hitlist* hl) {
 	bestnum = 0;
 	bestlist = NULL;
     N = blocklist_count(hl);
-	//fprintf(stderr, "radecs=[");
     for (i=0; i<N; i++) {
 		int M;
 		blocklist* hits = (blocklist*)blocklist_pointer_access(hl, i);
@@ -94,32 +92,7 @@ blocklist* hitlist_get_best(hitlist* hl) {
 			bestnum = M;
 			bestlist = hits;
 		}
-		/*
-		  if ((M >= min_matches_to_agree) ||
-		  0) {
-		  double ra1, dec1;
-		  double ra2, dec2;
-		  int lim = 1, j;
-		  //if (verbose)
-		  lim=M;
-		  for (j=0; j<lim; j++) {
-		  MatchObj* mo = (MatchObj*)blocklist_pointer_access(hits, j);
-		  ra1  = xy2ra(mo->vector[0], mo->vector[1]);
-		  dec1 = z2dec(mo->vector[2]);
-		  ra2  = xy2ra(mo->vector[3], mo->vector[4]);
-		  dec2 = z2dec(mo->vector[5]);
-		  ra1  *= 180.0/M_PI;
-		  dec1 *= 180.0/M_PI;
-		  ra2  *= 180.0/M_PI;
-		  dec2 *= 180.0/M_PI;
-		  //fprintf(stderr, "%.12g,%.12g,%.12g,%.12g;", ra1, dec1, ra2, dec2);
-		  //fprintf(stderr, "Match list %i: %i hits: ra,dec (%g, %g)\n", i, M, ra1, dec1);
-		  //fprintf(stderr, "Match list %i: %i hits: ra,dec (%g, %g)\n", i, M, ra1, dec1);
-		  }
-		  }
-		*/
 	}
-	//fprintf(stderr, "];\n");
 	return bestlist;
 }
 
@@ -149,69 +122,25 @@ blocklist* hitlist_get_all(hitlist* bl) {
 }
 
 int hitlist_add_hit(hitlist* hlist, MatchObj* mo) {
-	//, double AgreeTol) {
     int i, N;
     blocklist* newlist;
 
     N = blocklist_count(hlist);
 
-	/*
-	  if (verbose) {
-	  fprintf(stderr, "\n\nFinding matching hit to:\n");
-	  fprintf(stderr, " min (%g, %g, %g)\n", mo->vector[0], mo->vector[1], mo->vector[2]);
-	  fprintf(stderr, " max (%g, %g, %g)\n", mo->vector[3], mo->vector[4], mo->vector[5]);
-	  {
-	  double ra1, dec1, ra2, dec2;
-	  ra1 = xy2ra(mo->vector[0], mo->vector[1]);
-	  dec1 = z2dec(mo->vector[2]);
-	  ra2 = xy2ra(mo->vector[3], mo->vector[4]);
-	  dec2 = z2dec(mo->vector[5]);
-	  ra1  *= 180.0/M_PI;
-	  dec1 *= 180.0/M_PI;
-	  ra2  *= 180.0/M_PI;
-	  dec2 *= 180.0/M_PI;
-	  fprintf(stderr, "ra,dec (%g, %g), (%g, %g)\n",
-	  ra1, dec1, ra2, dec2);
-	  }
-	  fprintf(stderr, "%i other hits.\n", N);
-	  }
-	*/
-
     for (i=0; i<N; i++) {
 		int j, M;
 		blocklist* hits = (blocklist*)blocklist_pointer_access(hlist, i);
 		M = blocklist_count(hits);
-		/*
-		  if (verbose)
-		  fprintf(stderr, "  hit %i: %i elements.\n", i, M);
-		*/
 		for (j=0; j<M; j++) {
 			double d2;
-			//double arcsec;
 			MatchObj* m = (MatchObj*)blocklist_pointer_access(hits, j);
 			d2 = distsq(mo->vector, m->vector, MATCH_VECTOR_SIZE);
-			// DEBUG
-			//arcsec = sqrt(d2) / sqrt(2.0)
-			/*
-			  if (verbose)
-			  fprintf(stderr, "    el %i: dist %g (thresh %g)\n", j, sqrt(d2), AgreeTol);
-			*/
 			if (d2 < square(AgreeTol)) {
 				blocklist_pointer_append(hits, mo);
-				/*
-				  if (verbose)
-				  fprintf(stderr, "match!  (now %i agree)\n",
-				  blocklist_count(hits));
-				*/
 				return blocklist_count(hits);
 			}
 		}
     }
-
-	/*
-	  if (verbose)
-	  fprintf(stderr, "no agreement.\n");
-	*/
 
     // no agreement - create new list.
     newlist = blocklist_pointer_new(10);
