ea808ea7136f4bc02d10431202aea2b6e515aad3
Keir Mierle <mierle@gmail.com>
2006-02-19 01:40:46 +0000

    Added rdls2hpls to figure out what healpixes a field is in

---
+++ trunk/src/quads/rdls2hpls.c
@@ -0,0 +1,112 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "healpix.h"
+#include "starutil.h"
+
+int convert_file(char* fn)
+{
+	int i, j, slen, numfields, npoints, healpixes[12];
+	FILE* rdf, *hpf;
+	char* hpfn;
+
+	// Figure out the name of the .hpls file: Yummy string parsing in C!
+	slen = strlen(fn);
+	i = slen - 1;
+	while (i >= 0 && fn[i--] != '.')
+		;
+	if (i == -1) {
+		fprintf(stderr, "Missing extension on %s? Need .rdls filename\n", fn);
+		return 1;
+	}
+	hpfn = malloc(slen + 1);
+	strcpy(hpfn, fn);
+	hpfn[slen - 1] = 's';
+	hpfn[slen - 2] = 'l';
+	hpfn[slen - 3] = 'p';
+	hpfn[slen - 4] = 'h';
+	fprintf(stderr, "hpfn: %s\n", hpfn);
+
+	// Open the two files for input and output
+	rdf = fopen(fn, "r");
+	if (!rdf) {
+		fprintf(stderr, "Couldn't open %s: %s\n", fn, strerror(errno));
+		return 1;
+	}
+
+	hpf = fopen(hpfn, "w");
+	if (!hpf) {
+		fprintf(stderr, "Couldn't open %s for writing: %s\n", hpfn, strerror(errno));
+		return 1;
+	}
+
+	// First line: numfields
+	if (fscanf(rdf, "NumFields=%i\n", &numfields) != 1) {
+		fprintf(stderr, "parse error in %s: numfields\n", fn);
+		return 1;
+	}
+
+	fprintf(hpf, "Numfields=%i\n", numfields);
+
+	for (j = 0; j < numfields; j++) {
+		// Second line and subsequent lines: npoints,ra,dec,ra,dec,...
+		if (fscanf(rdf, "%i,", &npoints) != 1) {
+			fprintf(stderr, "parse error in %s: npoints\n", fn);
+			return 1;
+		}
+
+		for (i = 0; i < 12; i++) {
+			healpixes[i] = 0;
+		}
+
+		for (i = 0; i < npoints; i++) {
+			double ra, dec;
+			int hp;
+			if (fscanf(rdf, "%lf,%lf", &ra, &dec) != 2) {
+				printf("parse error: points %i\n", i);
+				exit( -1);
+			}
+			if (i != npoints - 1) {
+				fscanf(rdf, ",");
+			}
+
+			ra *= M_PI / 180.0;
+			dec *= M_PI / 180.0;
+
+			hp = radectohealpix(ra, dec);
+			if ((hp < 0) || (hp >= 12)) {
+				printf("hp=%i\n", hp);
+				continue;
+			}
+			healpixes[hp] = 1;
+		}
+		for (i = 0; i < 12; i++) {
+			if (healpixes[i])
+				fprintf(hpf, "%i ", i);
+		}
+		fprintf(hpf, "\n");
+
+	}
+
+	fclose(rdf);
+	fclose(hpf);
+	return 0;
+}
+
+int main(int argc, char** args)
+{
+	int i;
+	if (argc == 1) {
+		fprintf(stderr, "Usage: %s <rdls-file> ...\n", args[0]);
+		return 1;
+	}
+
+	for (i = 1; i < argc; i++)
+		convert_file(args[i]);
+
+	return 0;
+}
