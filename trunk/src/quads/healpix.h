9c36569f3e1146948c31f8b08732182018598c2d
Dustin Lang <dstndstn@gmail.com>
2006-02-22 20:24:11 +0000

    nside healpix.

--- trunk/src/quads/healpix.h
+++ trunk/src/quads/healpix.h
@@ -7,10 +7,27 @@
    http://www.journals.uchicago.edu/ApJ/journal/issues/ApJ/v622n2/61342/61342.web.pdf
 */
 
+
+/**
+   The following two functions convert (ra,dec) or (x,y,z) into the
+   base-level healpix in the range [0, 11].
+*/
 int radectohealpix(double ra, double dec);
 
-int radectohealpix_nside(double ra, double dec, int nside);
-
 int xyztohealpix(double x, double y, double z);
+
+/**
+   The following functions convert (ra,dec) or (x,y,z) into a finely-
+   pixelized healpix index, according to the HIERARCHICAL scheme,
+   in the range [0, 12 Nside^2).
+
+   NOTE: at the moment we can only handle Nside = a power of two.
+
+ */
+int radectohealpix_nside(double ra, double dec, int Nside);
+
+int xyztohealpix_nside(double x, double y, double z, int Nside);
+
+
 
 #endif
