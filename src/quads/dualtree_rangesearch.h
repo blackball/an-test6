80deb686b930a2d8d90761f7b833e8b6feae9c4d
Dustin Lang <dstndstn@gmail.com>
2006-03-14 20:40:20 +0000

    re-add dualtree

---
+++ src/quads/dualtree_rangesearch.h
@@ -0,0 +1,16 @@
+#ifndef DUALTREE_RANGE_SEARCH_H
+#define DUALTREE_RANGE_SEARCH_H
+
+extern double RANGESEARCH_NO_LIMIT;
+
+#include "kdtree/kdtree.h"
+
+typedef void (*result_callback)(void* extra, int xind, int yind,
+								double dist2);
+
+void dualtree_rangesearch(kdtree_t* x, kdtree_t* y,
+						  double mindist, double maxdist,
+						  result_callback callback,
+						  void* callback_param);
+
+#endif
