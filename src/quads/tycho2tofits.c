994595572b82f7cb429f238dc2f18291cc542381
Dustin Lang <dstndstn@gmail.com>
2006-05-04 20:32:24 +0000

    start tycho2 to FITS

---
+++ src/quads/tycho2tofits.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+#include "tycho2.h"
+#include "starutil.h"
+
+#define OPTIONS "ho:"
+
+void print_help(char* progname) {
+    printf("usage:\n"
+		   "  %s -o <output-filename-template>\n"
+		   "  [-N <healpix-nside>]  (default = 8; should be power of two.)\n"
+		   "  <input-file> [<input-file> ...]\n",
+		   progname);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+int main(int argc, char** args) {
+	char* outfn = NULL;
+    int c;
+	int startoptind;
+	uint nrecords, nobs, nfiles;
+	int Nside = 8;
+
+	//usnob_fits** usnobs;
+
+	//int i;
+	int HP;
+
+    while ((c = getopt(argc, args, OPTIONS)) != -1) {
+        switch (c) {
+		case '?':
+        case 'h':
+			print_help(args[0]);
+			exit(0);
+		case 'n':
+			Nside = atoi(optarg);
+			break;
+		case 'o':
+			outfn = optarg;
+			break;
+		}
+    }
+
+	if (!outfn || (optind == argc)) {
+		print_help(args[0]);
+		exit(-1);
+	}
+
+	if (Nside < 1) {
+		fprintf(stderr, "Nside must be >= 1.\n");
+		print_help(args[0]);
+		exit(-1);
+	}
+
+	HP = 12 * Nside * Nside;
+
+	/*
+	  usnobs = malloc(HP * sizeof(usnob_fits*));
+	  memset(usnobs, 0, HP * sizeof(usnob_fits*));
+	*/
+
+	nrecords = 0;
+	nobs = 0;
+	nfiles = 0;
+
+	printf("Reading Tycho-2 files... ");
+	fflush(stdout);
+
+	startoptind = optind;
+	for (; optind<argc; optind++) {
+		char* infn;
+		FILE* fid;
+		char* map;
+		size_t map_size;
+		int i;
+
+		infn = args[optind];
+		fid = fopen(infn, "rb");
+		if (!fid) {
+			fprintf(stderr, "Couldn't open input file %s: %s\n", infn, strerror(errno));
+			exit(-1);
+		}
+
+		if ((optind > startoptind) && ((optind - startoptind) % 100 == 0)) {
+			printf("\nReading file %i of %i: %s\n", optind - startoptind,
+				   argc - startoptind, infn);
+		}
+
+		if (fseeko(fid, 0, SEEK_END)) {
+			fprintf(stderr, "Couldn't seek to end of input file %s: %s\n", infn, strerror(errno));
+			exit(-1);
+		}
+
+		map_size = ftello(fid);
+
+		fseeko(fid, 0, SEEK_SET);
+
+		map = mmap(NULL, map_size, PROT_READ, MAP_SHARED, fileno(fid), 0);
+		if (map == MAP_FAILED) {
+			fprintf(stderr, "Couldn't mmap input file %s: %s\n", infn, strerror(errno));
+			exit(-1);
+		}
+		fclose(fid);
+
+		if (map_size % TYCHO_RECORD_SIZE) {
+			fprintf(stderr, "Warning, input file %s has size %u which is not divisible into %i-byte records.\n",
+					infn, map_size, TYCHO_RECORD_SIZE);
+		}
+
+		for (i=0; i<map_size; i+=TYCHO_RECORD_SIZE) {
+			tycho2_entry entry;
+			//int hp;
+			//int slice;
+
+			if (tycho2_parse_entry(map + i, &entry)) {
+				fprintf(stderr, "Failed to parse TYCHO-2 entry: offset %i in file %s.\n",
+						i, infn);
+				exit(-1);
+			}
+
+			printf("RA, DEC (%g, %g)\n", entry.RA, entry.DEC);
+
+			/*
+			  hp = radectohealpix_nside(deg2rad(entry.ra), deg2rad(entry.dec), Nside);
+
+			  if (!usnobs[hp]) {
+				char fn[256];
+				char val[256];
+				sprintf(fn, outfn, hp);
+				usnobs[hp] = usnob_fits_open_for_writing(fn);
+				if (!usnobs[hp]) {
+				fprintf(stderr, "Failed to initialized FITS file %i (filename %s).\n", hp, fn);
+				exit(-1);
+				}
+
+				// header remarks...
+				sprintf(val, "%u", hp);
+				qfits_header_add(usnobs[hp]->header, "HEALPIX", val, "The healpix number of this catalog.", NULL);
+				sprintf(val, "%u", Nside);
+				qfits_header_add(usnobs[hp]->header, "NSIDE", val, "The healpix resolution.", NULL);
+				qfits_header_add(usnobs[hp]->header, "NOBJS", "0", "", NULL);
+				// etc...
+
+				if (usnob_fits_write_headers(usnobs[hp])) {
+				fprintf(stderr, "Failed to write header for FITS file %s.\n", fn);
+				exit(-1);
+				}
+				}
+
+				if (usnob_fits_write_entry(usnobs[hp], &entry)) {
+				fprintf(stderr, "Failed to write FITS entry.\n");
+				exit(-1);
+				}
+			*/
+
+			nrecords++;
+			nobs += entry.nobs;
+		}
+
+		munmap(map, map_size);
+
+		nfiles++;
+		printf(".");
+		fflush(stdout);
+	}
+	printf("\n");
+
+	// close all the files...
+	/*
+	  for (i=0; i<HP; i++) {
+	  char val[256];
+	  if (!usnobs[i])
+	  continue;
+	  sprintf(val, "%u", usnobs[i]->nentries);
+	  qfits_header_mod(usnobs[i]->header, "NOBJS", val, "Number of objects in this catalog.");
+	  usnob_fits_fix_headers(usnobs[i]);
+	  if (usnob_fits_close(usnobs[i])) {
+	  fprintf(stderr, "Failed to close file %i: %s\n", i, strerror(errno));
+	  }
+	  }
+	*/
+	
+	printf("Read %u files, %u records, %u observations.\n",
+		   nfiles, nrecords, nobs);
+	
+	//free(usnobs);
+
+	return 0;
+}
+
