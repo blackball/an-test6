f75fd988f286aa0369e3e6df68d295cbbe4a0ce0
Dustin Lang <dstndstn@gmail.com>
2006-05-08 15:49:03 +0000

    *** empty log message ***

---
+++ trunk/src/quads/build_an_catalog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+#include "an_catalog.h"
+#include "usnob_fits.h"
+#include "tycho2_fits.h"
+#include "starutil.h"
+#include "healpix.h"
+
+int main(int argc, char** args) {
+	/*
+	  char* outfn = NULL;
+	  int c;
+	  int startoptind;
+	  uint nrecords, nobs;
+	  int Nside = 8;
+	  int i, HP;
+	*/
+	return 0;
+}
+
