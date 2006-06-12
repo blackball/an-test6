021c0c0162cce09b85614c4b3b6b7457dd2fc997
Dustin Lang <dstndstn@gmail.com>
2006-06-12 20:26:55 +0000

    cleanup

--- trunk/src/quads/hitlist_healpix.h
+++ trunk/src/quads/hitlist_healpix.h
@@ -3,9 +3,81 @@
 
 #include "matchobj.h"
 
-#ifndef DONT_DEFINE_HITLIST
-typedef void hitlist;
-#endif
+// per-healpix struct
+struct pixinfo {
+	// list of indexes into the master MatchObj list, of MatchObjects
+	// that belong to this pix.
+	il* matchinds;
+	// list of neighbouring healpixes, terminated by -1.
+	uint* neighbours;
+};
+typedef struct pixinfo pixinfo;
+
+// a hitlist (one per field)
+struct hitlist_struct {
+	int Nside;
+
+	int nbest;
+	int ntotal;
+	il* best;
+	int npix;
+	pixinfo* pix;
+	double agreedist2;
+
+	/*
+	  The way we store lists of agreeing matches is a bit complicated,
+	  because we need to be able to rapidly merge lists that both agree
+	  with a new match.
+
+	  The "matchlist" list contains all the MatchObj (matches) we've seen:
+	  eg, matchlist = [ m0, m1, m2, m3 ].
+
+	  At each healpix, we have the "matchinds" list, which is a list of ints
+	  that indexes into the "matchlist" list.
+	  eg, healpix 0 might have matchinds = [ 0, 3 ], which means it own
+	  matchlist[0] and [3], ie, m0 and m3.
+	  healpix 1 would have [ 1, 2 ]
+
+	  We also have a list of agreeing matches; again, these are lists of ints
+	  that index into the "matchlist".
+	  eg, agreelist = [ a0, a1, a2 ]
+	  agreement cluster a0 = [ 0 ]
+	                    a1 = [ 2, 3 ]
+						a2 = [ 1 ]
+	  which would mean that matchlist[0], ie m0, agrees with itself, and
+	  m2 and m3 agree with each other.
+
+	  Finally, we have the "memberlist", a list of ints, that says, for
+	  each element in matchlist, which agreelist it belongs to.
+	  In our running example,
+	  memberlist = [ 0, 2, 1, 1 ]
+	  because m0 belongs to a0, hence memberlist[0] = 0
+	          m1            a2                  [1] = 2
+			  m2, m3        a1                [2,3] = 1
+	 */
+
+
+	// master list of MatchObj*: matches
+	pl* matchlist;
+
+	// list of ints, same size as matchlist; containing indexes into
+	// agreelist; the list to which the corresponding MatchObj
+	// belongs.
+	il* memberlist;
+
+	// list of il*; the lists of agreeing matches.
+	//    (which are lists of indices into "matchlist".)
+	pl* agreelist;
+
+	// same size as agreelist; contains "intmap*" objects that hold the
+	// mapping between field and catalog objects.
+	// (IFF do_correspond is TRUE).
+	pl* correspondlist;
+
+	int do_correspond;
+};
+typedef struct hitlist_struct hitlist;
+
 
 void hitlist_healpix_compute_vector(MatchObj* mo);
 
