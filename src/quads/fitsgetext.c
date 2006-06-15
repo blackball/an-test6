245398f2100919395f02be00cde01bd1149a1838
Dustin Lang <dstndstn@gmail.com>
2006-06-15 15:13:00 +0000

    add fitsgetext for copying specific extensions of FITS files.

---
+++ src/quads/fitsgetext.c
@@ -0,0 +1,125 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#include "qfits.h"
+#include "bl.h"
+
+char* OPTIONS = "he:i:o:";
+
+void printHelp(char* progname) {
+	printf("%s    -i <input-file>\n"
+		   "      -o <output-file>\n"
+		   "      -e <extension-number> ...\n\n",
+		   progname);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+int main(int argc, char *argv[]) {
+    int argchar;
+
+	char* infn = NULL;
+	char* outfn = NULL;
+	FILE* fin = NULL;
+	FILE* fout = NULL;
+	il* exts;
+	int i;
+	char* map;
+	int mapsize;
+	char* progname = argv[0];
+
+	exts = il_new(16);
+
+    while ((argchar = getopt (argc, argv, OPTIONS)) != -1)
+        switch (argchar) {
+        case 'e':
+			il_append(exts, atoi(optarg));
+            break;
+        case 'i':
+			infn = optarg;
+			break;
+        case 'o':
+			outfn = optarg;
+			break;
+        case '?':
+        case 'h':
+			printHelp(progname);
+            return 0;
+        default:
+            return -1;
+        }
+
+	if (!infn || !outfn || !il_size(exts)) {
+		printHelp(progname);
+		exit(-1);
+	}
+
+	if (infn) {
+		fin = fopen(infn, "rb");
+		if (!fin) {
+			fprintf(stderr, "Failed to open input file %s: %s\n", infn, strerror(errno));
+			exit(-1);
+		}
+	}
+
+	if (outfn) {
+		fout = fopen(outfn, "wb");
+		if (!fout) {
+			fprintf(stderr, "Failed to open output file %s: %s\n", outfn, strerror(errno));
+			exit(-1);
+		}
+	}
+
+	qfits_err_statset(1);
+
+	fseeko(fin, SEEK_END, 0);
+	mapsize = ftello(fin);
+	fseeko(fin, SEEK_SET, 0);
+	map = mmap(NULL, mapsize, PROT_READ, MAP_SHARED, fileno(fin), 0);
+	if (map == MAP_FAILED) {
+		fprintf(stderr, "Failed to mmap input file: %s\n", strerror(errno));
+		exit(-1);
+	}
+	fclose(fin);
+
+	for (i=0; i<il_size(exts); i++) {
+		int hdrstart, hdrlen, datastart, datalen;
+		/*
+		  int pagesize;
+		  off_t start;
+		  int leftover;
+		*/
+		int ext = il_get(exts, i);
+		if (qfits_get_hdrinfo(infn, ext, &hdrstart,  &hdrlen ) ||
+			qfits_get_datinfo(infn, ext, &datastart, &datalen)) {
+			fprintf(stderr, "Error getting extents of extension %i.\n", ext);
+			exit(-1);
+		}
+		printf("Writing extension %i: header start %i, length %i, data start %i, length %i.\n",
+			   ext, hdrstart, hdrlen, datastart, datalen);
+		/*
+		  assert(hdrstart + hdrlen == datastart);
+		  pagesize = getpagesize();
+		  start = (hdrstart / pagesize) * pagesize;
+		  leftover = hdrstart - start;
+		  mapsize = leftover + hdrlen + datalen;
+		*/
+		if ((hdrlen  && (fwrite(map + hdrstart , 1, hdrlen , fout) != hdrlen )) ||
+			(datalen && (fwrite(map + datastart, 1, datalen, fout) != datalen))) {
+			fprintf(stderr, "Failed to write extension %i: %s\n", ext, strerror(errno));
+			exit(-1);
+		}
+	}
+
+	munmap(map, mapsize);
+
+	fclose(fout);
+
+	il_free(exts);
+	return 0;
+}
