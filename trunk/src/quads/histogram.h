67760f1a89e94e734161d0c0c44fb041fa4962c3
Dustin Lang <dstndstn@gmail.com>
2006-06-26 17:44:16 +0000

    *** empty log message ***

--- trunk/src/quads/histogram.h
+++ trunk/src/quads/histogram.h
@@ -1,6 +1,8 @@
 #ifndef HISTOGRAM_H
 #define HISTOGRAM_H
 
+#include <stdio.h>
+
 struct histogram {
 	double zero;
 	double binsize;
@@ -16,5 +18,10 @@ histogram* histogram_new_binsize(double zero, double maximum, double binsize);
 void histogram_free(histogram* h);
 
 int histogram_add(histogram* h, double val);
+
+void histogram_print_matlab(histogram* h, FILE* fid);
+
+// assumes each count is on the left side of the bin.
+double histogram_mean(histogram* h);
 
 #endif
