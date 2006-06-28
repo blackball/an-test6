91fa2f43ce03b24a4ea1e20c4f043053679ca12f
Dustin Lang <dstndstn@gmail.com>
2006-06-28 19:57:38 +0000

    solvedserver stuff

--- trunk/src/quads/solvedfile.c
+++ trunk/src/quads/solvedfile.c
@@ -12,6 +12,7 @@
 #include "solvedfile.h"
 
 static struct sockaddr_in serveraddr = {0, 0, {0}};
+static FILE* fserver = NULL;
 
 int solvedserver_set_server(char* addr) {
 	char buf[256];
@@ -19,6 +20,15 @@ int solvedserver_set_server(char* addr) {
 	struct hostent* he;
 	int len;
 	int port;
+	if (fserver) {
+		if (fflush(fserver) ||
+			fclose(fserver)) {
+			fprintf(stderr, "Failed to close previous connection to server.\n");
+		}
+		fserver = NULL;
+	}
+	if (!addr)
+		return -1;
 	ind = index(addr, ':');
 	if (!ind) {
 		fprintf(stderr, "Invalid IP:port address: %s\n", addr);
@@ -36,58 +46,75 @@ int solvedserver_set_server(char* addr) {
 	port = atoi(ind+1);
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_port = htons(port);
+
+	return 0;
+}
+
+static int connect_to_server() {
+	int sock;
+	if (fserver)
+		return 0;
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		fprintf(stderr, "Couldn't create socket: %s\n", strerror(errno));
+		return -1;
+	}
+	fserver = fdopen(sock, "r+b");
+	if (!fserver) {
+		fprintf(stderr, "Failed to fdopen socket: %s\n", strerror(errno));
+		return -1;
+	}
+	if (connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) {
+		fprintf(stderr, "Couldn't connect to server: %s\n", strerror(errno));
+		fserver = NULL;
+		return -1;
+	}
 	return 0;
 }
 
 int solvedserver_get(int filenum, int fieldnum) {
-	FILE* f;
 	char buf[256];
 	const char* solvedstr = "solved";
+	int nchars;
 	int solved;
-	int sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
- 		fprintf(stderr, "Couldn't create socket: %s\n", strerror(errno));
+
+	if (connect_to_server())
+		return -1;
+	nchars = sprintf(buf, "get %i %i\n", filenum, fieldnum);
+	if ((fwrite(buf, 1, nchars, fserver) != nchars) ||
+		fflush(fserver)) {
+		fprintf(stderr, "Failed to write request to server: %s\n", strerror(errno));
+		fclose(fserver);
+		fserver = NULL;
 		return -1;
 	}
-	f = fdopen(sock, "r+b");
-	if (!f) {
-		fprintf(stderr, "Failed to fdopen socket: %s\n", strerror(errno));
-		return -1;
-	}
-	if (connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) {
- 		fprintf(stderr, "Couldn't connect to server: %s\n", strerror(errno));
-		return -1;
-	}
-	fprintf(f, "get %i %i\n", filenum, fieldnum);
-	fflush(f);
-	if (!fgets(buf, 256, f)) {
+	if (!fgets(buf, 256, fserver)) {
 		fprintf(stderr, "Couldn't read response: %s\n", strerror(errno));
-		fclose(f);
+		fclose(fserver);
+		fserver = NULL;
 		return -1;
 	}
 	solved = (strncmp(buf, solvedstr, strlen(solvedstr)) == 0);
-	fclose(f);
 	return solved;
 }
 
 void solvedserver_set(int filenum, int fieldnum) {
-	FILE* f;
-	int sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
- 		fprintf(stderr, "Couldn't create socket: %s\n", strerror(errno));
+	char buf[256];
+	int nchars;
+	if (connect_to_server())
+		return;
+	nchars = sprintf(buf, "set %i %i\n", filenum, fieldnum);
+	if ((fwrite(buf, 1, nchars, fserver) != nchars) ||
+		fflush(fserver)) {
+		fprintf(stderr, "Failed to send command (%s) to solvedserver: %s\n", buf, strerror(errno));
 		return;
 	}
-	f = fdopen(sock, "wb");
-	if (!f) {
-		fprintf(stderr, "Failed to fdopen socket: %s\n", strerror(errno));
+	// wait for response.
+	if (!fgets(buf, 256, fserver)) {
+		fprintf(stderr, "Couldn't read response: %s\n", strerror(errno));
+		fclose(fserver);
+		fserver = NULL;
 		return;
 	}
-	if (connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) {
- 		fprintf(stderr, "Couldn't connect to server: %s\n", strerror(errno));
-		return;
-	}
-	fprintf(f, "set %i %i\n", filenum, fieldnum);
-	fflush(f);
-	fclose(f);
 }
 
