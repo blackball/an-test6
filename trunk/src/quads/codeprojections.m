c1d818c5268d69eb7145d7541735e6c32d62c012
Dustin Lang <dstndstn@gmail.com>
2006-07-17 19:17:17 +0000

    *** empty log message ***

--- trunk/src/quads/codeprojections.m
+++ trunk/src/quads/codeprojections.m
@@ -21,27 +21,28 @@ colormap hot;
 
 input('next');
 
-mx=max(max(max(max(dhist_1_0,dhist_2_0),max(max(dhist_2_1,dhist_3_0),max(dhist_3_1,dhist_3_2)))));
-mn=min(min(min(min(dhist_1_0,dhist_2_0),min(min(dhist_2_1,dhist_3_0),min(dhist_3_1,dhist_3_2)))));
-for i=1:3,
-  for j=0:(i-1),
-    subplot(3,3,(i-1)*3+j+1);
-    h=eval(sprintf('dhist_%i_%i', i, j));
-    s=surf(h);
-    set(s, 'EdgeColor', 'interp');
-    set(s, 'FaceColor', 'interp');
-    %set(s, 'EdgeAlpha', 0);
-    set(gca,'XTickLabel',{});
-    set(gca,'YTickLabel',{});
-    a=axis;
-    a(5)=0;
-    a(6)=mx*1.1;
-    axis(a);
+if (exist('dhist_xy')),
+  mx=max(max(max(max(dhist_1_0,dhist_2_0),max(max(dhist_2_1,dhist_3_0),max(dhist_3_1,dhist_3_2)))));
+  mn=min(min(min(min(dhist_1_0,dhist_2_0),min(min(dhist_2_1,dhist_3_0),min(dhist_3_1,dhist_3_2)))));
+  for i=1:3,
+    for j=0:(i-1),
+      subplot(3,3,(i-1)*3+j+1);
+      h=eval(sprintf('dhist_%i_%i', i, j));
+      s=surf(h);
+      set(s, 'EdgeColor', 'interp');
+      set(s, 'FaceColor', 'interp');
+      %set(s, 'EdgeAlpha', 0);
+      set(gca,'XTickLabel',{});
+      set(gca,'YTickLabel',{});
+      a=axis;
+      a(5)=0;
+      a(6)=mx*1.1;
+      axis(a);
+    end
   end
+  colormap hot;
+  input('next');
 end
-colormap hot;
-
-input('next');
 
 %figure(2);
 for i=1:3,
@@ -63,24 +64,22 @@ colormap hot;
 
 input('next');
 
-for i=1:3,
-  for j=0:(i-1),
-    subplot(3,3,(i-1)*3+j+1);
-    h=eval(sprintf('dhist_%i_%i', i, j));
-    %s=surf(h);
-    %set(s, 'EdgeAlpha', 0);
-    %view(0,90);
-    imagesc(h);
-    axis tight;
-    axis square;
-    set(gca,'YDir','normal');
-    set(gca,'XTickLabel',{});
-    set(gca,'YTickLabel',{});
+if (exist('dhist_xy')),
+  for i=1:3,
+    for j=0:(i-1),
+      subplot(3,3,(i-1)*3+j+1);
+      h=eval(sprintf('dhist_%i_%i', i, j));
+      imagesc(h);
+      axis tight;
+      axis square;
+      set(gca,'YDir','normal');
+      set(gca,'XTickLabel',{});
+      set(gca,'YTickLabel',{});
+    end
   end
+  colormap hot;
+  input('next');
 end
-colormap hot;
-
-input('next');
 
 %figure(3);
 mx=max([hist_0 hist_1 hist_2 hist_3]);
@@ -94,16 +93,17 @@ end
 
 input('next');
 
-mx=max([dhist_0 dhist_1 dhist_2 dhist_3]);
-for i=1:4,
-  subplot(4,1,i);
-  h=eval(sprintf('dhist_%i', (i-1)));
-  b=bar(h, 1);
-  axis([0 100 0 1.2*mx]);
-  set(gca,'XTickLabel',{});
+if (exist('dhist_xy')),
+  mx=max([dhist_0 dhist_1 dhist_2 dhist_3]);
+  for i=1:4,
+    subplot(4,1,i);
+    h=eval(sprintf('dhist_%i', (i-1)));
+    b=bar(h, 1);
+    axis([0 100 0 1.2*mx]);
+    set(gca,'XTickLabel',{});
+  end
+  input('next');
 end
-
-input('next');
 
 %figure(4);
 subplot(1,1,1);
@@ -123,15 +123,16 @@ colormap hot
 
 input('next');
 
-subplot(1,1,1);
-imagesc(dhist_xy);
-set(gca,'YDir','normal');
-set(gca,'XTickLabel',{});
-set(gca,'YTickLabel',{});
-axis tight;
-axis square;
-xlabel('x')
-ylabel('y') 
-title('cx vs cy + dx vs dy')
-colormap hot
-
+if (exist('dhist_xy')),
+  subplot(1,1,1);
+  imagesc(dhist_xy);
+  set(gca,'YDir','normal');
+  set(gca,'XTickLabel',{});
+  set(gca,'YTickLabel',{});
+  axis tight;
+  axis square;
+  xlabel('x')
+  ylabel('y') 
+  title('cx vs cy + dx vs dy')
+  colormap hot
+end
