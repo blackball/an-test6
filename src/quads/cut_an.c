318d0edeef2626810d9efe28327b0c249b4a3d0d
Dustin Lang <dstndstn@gmail.com>
2006-07-20 23:44:46 +0000

    cut_an: add all-sky flag.

--- src/quads/cut_an.c
+++ src/quads/cut_an.c
@@ -8,12 +8,12 @@
 #include "mathutil.h"
 #include "fitsioutils.h"
 
-#define OPTIONS "ho:i:N:n:m:M:H:d:RGe:"
+#define OPTIONS "ho:i:N:n:m:M:H:d:RGe:A"
 
 void print_help(char* progname) {
     printf("usage:\n"
 		   "  %s -o <output-filename-template> -i <output-idfile-template>\n"
-		   "  [-H <big healpix>]\n"
+		   "  [-H <big healpix>]  or  [-A] (all-sky)\n"
 		   "  [-m <minimum-magnitude-to-use>]\n"
 		   "  [-M <maximum-magnitude-to-use>]\n"
 		   "  [-n <max-stars-per-(small)-healpix>]\n"
@@ -72,6 +72,9 @@ int main(int argc, char** args) {
 	double epsilon = 0.0;
 	bool sdss = FALSE;
 	bool galex = FALSE;
+	bool allsky = FALSE;
+	stardata* sweeplist;
+	int nowned = 0;
 
     while ((c = getopt(argc, args, OPTIONS)) != -1) {
         switch (c) {
@@ -79,6 +82,9 @@ int main(int argc, char** args) {
         case 'h':
 			print_help(args[0]);
 			exit(0);
+		case 'A':
+			allsky = TRUE;
+			break;
 		case 'R':
 			sdss = TRUE;
 			break;
@@ -118,7 +124,8 @@ int main(int argc, char** args) {
 		}
     }
 
-	if (!outfn || !idfn || (optind == argc) || (bighp == -1)) {
+	if (!outfn || !idfn || (optind == argc)) {
+		printf("Specify catalog and idfile names.\n");
 		print_help(args[0]);
 		exit(-1);
 	}
@@ -127,12 +134,20 @@ int main(int argc, char** args) {
 		print_help(args[0]);
 		exit(-1);
 	}
+	if ((bighp == -1) && !allsky) {
+		printf("Either specify a healpix (-H) or all-sky (-A).\n");
+		print_help(args[0]);
+		exit(-1);
+	}
 
 	HP = 12 * Nside * Nside;
 
 	printf("Nside=%i, HP=%i, maxperhp=%i, max number of stars = HP*maxperhp = %i.\n", Nside, HP, maxperhp, HP*maxperhp);
 	printf("Healpix side length: %g arcmin.\n", healpix_side_length_arcmin(Nside));
-	printf("Writing big healpix %i.\n", bighp);
+	if (bighp != -1)
+		printf("Writing big healpix %i.\n", bighp);
+	else
+		printf("Writing an all-sky catalog.\n");
 
 	if (deduprad > 0.0) {
 		printf("Deduplication radius %f arcsec.\n", deduprad);
@@ -142,22 +157,25 @@ int main(int argc, char** args) {
 	starlists = calloc(HP, sizeof(bl*));
 	owned = calloc(HP, sizeof(bool));
 
-	// for each big healpix, find the set of small healpixes it owns
+	// find the set of small healpixes that this big healpix owns
 	// (including a bit of overlap)
-	for (i=0; i<HP; i++) {
-		uint big, x, y;
-		uint nn, neigh[8], k;
-		healpix_decompose(i, &big, &x, &y, Nside);
-		if (big != bighp)
-			continue;
-		owned[i] = 1;
-		if (x == 0 || y == 0 || (x == Nside-1) || (y == Nside-1)) {
-			// add its neighbours.
-			nn = healpix_get_neighbours_nside(i, neigh, Nside);
-			for (k=0; k<nn; k++)
-				owned[neigh[k]] = 1;
+	if (bighp != -1) {
+		for (i=0; i<HP; i++) {
+			uint big, x, y;
+			uint nn, neigh[8], k;
+			healpix_decompose(i, &big, &x, &y, Nside);
+			if (big != bighp)
+				continue;
+			owned[i] = 1;
+			if (x == 0 || y == 0 || (x == Nside-1) || (y == Nside-1)) {
+				// add its neighbours.
+				nn = healpix_get_neighbours_nside(i, neigh, Nside);
+				for (k=0; k<nn; k++)
+					owned[neigh[k]] = 1;
+			}
 		}
-	}
+	} else
+		memset(owned, 1, HP * sizeof(bool));
 
 	nwritten = 0;
 	entries = malloc(BLOCK * sizeof(an_entry));
@@ -171,9 +189,9 @@ int main(int argc, char** args) {
 		exit(-1);
 	}
 	cat->healpix = bighp;
+	if (allsky)
+		qfits_header_add(cat->header, "ALLSKY", "T", "All-sky catalog.", NULL);
 
-	qfits_header_add(cat->header, "COMMENT", "cut_an command line:", NULL, NULL);
-	fits_add_args(cat->header, args, argc);
 
 	sprintf(fn, idfn, bighp);
 	id = idfile_open_for_writing(fn);
@@ -182,6 +200,8 @@ int main(int argc, char** args) {
 		exit(-1);
 	}
 	id->healpix = bighp;
+	if (allsky)
+		qfits_header_add(id->header, "ALLSKY", "T", "All-sky catalog.", NULL);
 
 	qfits_header_add(id->header, "HISTORY", "cut_an command line:", NULL, NULL);
 	fits_add_args(id->header, args, argc);
@@ -200,10 +220,8 @@ int main(int argc, char** args) {
 	startoptind = optind;
 	for (; optind<argc; optind++) {
 		char* infn;
-		int hp;
 		int i, N;
 		an_catalog* ancat;
-		int off, n;
 		int ndiscarded;
 		int nduplicates;
 
@@ -218,130 +236,127 @@ int main(int argc, char** args) {
 		fflush(stdout);
 		ndiscarded = 0;
 		nduplicates = 0;
-		for (off=0; off<N; off+=n) {
+
+		for (i=0; i<N; i++) {
 			stardata sd;
 			int j;
 			double summag;
+			int hp;
 			int nmag;
 			double sumredmag;
 			int nredmag;
-			an_entry* an = entries;
+			an_entry* an;
 
-			if (off + BLOCK > N)
-				n = N - off;
-			else
-				n = BLOCK;
-
-			if (an_catalog_read_entries(ancat, off, n, an)) {
-				fprintf(stderr, "Error reading %i AN catalog entries.\n", n);
+			an = an_catalog_read_entry(ancat);
+			if (!an) {
+				fprintf(stderr, "Failed to read Astrometry.net catalog entry.\n");
 				exit(-1);
 			}
 
-			for (i=0; i<n; i++) {
-				hp = radectohealpix_nside(deg2rad(an[i].ra), deg2rad(an[i].dec), Nside);
+			hp = radectohealpix_nside(deg2rad(an[i].ra), deg2rad(an[i].dec), Nside);
 
-				if (!owned[hp]) {
-					ndiscarded++;
-					continue;
-				}
-
-				sd.ra = an[i].ra;
-				sd.dec = an[i].dec;
-				sd.id = an[i].id;
-
-				// dumbass magnitude averaging!
-				summag = 0.0;
-				nmag = 0;
-				sumredmag = 0.0;
-				nredmag = 0;
-				if (sdss) {
-					for (j=0; j<an[i].nobs; j++) {
-						if (an[i].obs[j].catalog == AN_SOURCE_USNOB) {
-							if (an[i].obs[j].band == 'E' || an[i].obs[j].band == 'F') {
-								summag += an[i].obs[j].mag;
-								nmag++;
-							}
-						} else if (an[i].obs[j].catalog == AN_SOURCE_TYCHO2) {
-							if (an[i].obs[j].band == 'V' || an[i].obs[j].band == 'H') {
-								summag += an[i].obs[j].mag;
-								nmag++;
-							}
-						}
-					}
-				} else if (galex) {
-					for (j=0; j<an[i].nobs; j++) {
-						if (an[i].obs[j].catalog == AN_SOURCE_USNOB) {
-							if (an[i].obs[j].band == 'O' || an[i].obs[j].band == 'J') {
-								summag += an[i].obs[j].mag;
-								nmag++;
-							} else if (an[i].obs[j].band == 'E' || an[i].obs[j].band == 'F') {
-								sumredmag += an[i].obs[j].mag;
-								nredmag++;
-							}
-						} else if (an[i].obs[j].catalog == AN_SOURCE_TYCHO2) {
-							if (an[i].obs[j].band == 'B' || an[i].obs[j].band == 'H') {
-								summag += an[i].obs[j].mag;
-								nmag++;
-							} else if (an[i].obs[j].band == 'V') {
-								sumredmag += an[i].obs[j].mag;
-								nredmag++;
-							}
-						}
-					}
-				}
-				if (!nmag)
-					continue;
-
-				summag /= (double)nmag;
-				sd.mag = summag;
-
-				if (galex && epsilon>0) {
-					if (nredmag) {
-						sumredmag /= (double)nredmag;
-						sd.mag += epsilon * (summag - sumredmag);
-					}
-				}
-
-				if (sd.mag < minmag)
-					continue;
-				if (sd.mag > maxmag)
-					continue;
-
-				if (!starlists[hp])
-					starlists[hp] = bl_new(10, sizeof(stardata));
-
-				if (maxperhp && (bl_size(starlists[hp]) >= maxperhp)) {
-					// is this list full?
-					stardata* last = bl_access(starlists[hp], maxperhp-1);
-					if (sd.mag > last->mag)
-						// this new star is dimmer than the "maxperhp"th one in the list...
-						continue;
-				}
-				if (deduprad > 0.0) {
-					double xyz[3];
-					bool dup = FALSE;
-					radec2xyzarr(deg2rad(sd.ra), deg2rad(sd.dec), xyz);
-					for (j=0; j<bl_size(starlists[hp]); j++) {
-						double xyz2[3];
-						stardata* sd2 = bl_access(starlists[hp], j);
-						radec2xyzarr(deg2rad(sd2->ra), deg2rad(sd2->dec), xyz2);
-						if (!distsq_exceeds(xyz, xyz2, 3, deduprad)) {
-							nduplicates++;
-							dup = TRUE;
-							break;
-						}
-					}
-					if (dup)
-						continue;
-				}
-				bl_insert_sorted(starlists[hp], &sd, sort_stardata_mag);
+			if (!owned[hp]) {
+				ndiscarded++;
+				continue;
 			}
+
+			sd.ra = an[i].ra;
+			sd.dec = an[i].dec;
+			sd.id = an[i].id;
+
+			// dumbass magnitude averaging!
+			summag = 0.0;
+			nmag = 0;
+			sumredmag = 0.0;
+			nredmag = 0;
+			if (sdss) {
+				for (j=0; j<an[i].nobs; j++) {
+					if (an[i].obs[j].catalog == AN_SOURCE_USNOB) {
+						if (an[i].obs[j].band == 'E' || an[i].obs[j].band == 'F') {
+							summag += an[i].obs[j].mag;
+							nmag++;
+						}
+					} else if (an[i].obs[j].catalog == AN_SOURCE_TYCHO2) {
+						if (an[i].obs[j].band == 'V' || an[i].obs[j].band == 'H') {
+							summag += an[i].obs[j].mag;
+							nmag++;
+						}
+					}
+				}
+			} else if (galex) {
+				for (j=0; j<an[i].nobs; j++) {
+					if (an[i].obs[j].catalog == AN_SOURCE_USNOB) {
+						if (an[i].obs[j].band == 'O' || an[i].obs[j].band == 'J') {
+							summag += an[i].obs[j].mag;
+							nmag++;
+						} else if (an[i].obs[j].band == 'E' || an[i].obs[j].band == 'F') {
+							sumredmag += an[i].obs[j].mag;
+							nredmag++;
+						}
+					} else if (an[i].obs[j].catalog == AN_SOURCE_TYCHO2) {
+						if (an[i].obs[j].band == 'B' || an[i].obs[j].band == 'H') {
+							summag += an[i].obs[j].mag;
+							nmag++;
+						} else if (an[i].obs[j].band == 'V') {
+							sumredmag += an[i].obs[j].mag;
+							nredmag++;
+						}
+					}
+				}
+			}
+			if (!nmag)
+				continue;
+
+			summag /= (double)nmag;
+			sd.mag = summag;
+
+			if (galex && epsilon>0) {
+				if (nredmag) {
+					sumredmag /= (double)nredmag;
+					sd.mag += epsilon * (summag - sumredmag);
+				}
+			}
+
+			if (sd.mag < minmag)
+				continue;
+			if (sd.mag > maxmag)
+				continue;
+
+			if (!starlists[hp])
+				starlists[hp] = bl_new(10, sizeof(stardata));
+
+			if (maxperhp && (bl_size(starlists[hp]) >= maxperhp)) {
+				// is this list full?
+				stardata* last = bl_access(starlists[hp], maxperhp-1);
+				if (sd.mag > last->mag)
+					// this new star is dimmer than the "maxperhp"th one in the list...
+					continue;
+			}
+			if (deduprad > 0.0) {
+				double xyz[3];
+				bool dup = FALSE;
+				radec2xyzarr(deg2rad(sd.ra), deg2rad(sd.dec), xyz);
+				for (j=0; j<bl_size(starlists[hp]); j++) {
+					double xyz2[3];
+					stardata* sd2 = bl_access(starlists[hp], j);
+					radec2xyzarr(deg2rad(sd2->ra), deg2rad(sd2->dec), xyz2);
+					if (!distsq_exceeds(xyz, xyz2, 3, deduprad)) {
+						nduplicates++;
+						dup = TRUE;
+						break;
+					}
+				}
+				if (dup)
+					continue;
+			}
+			bl_insert_sorted(starlists[hp], &sd, sort_stardata_mag);
 		}
 
-		printf("Discarded %i stars not in this big healpix.\n", ndiscarded);
+		if (bighp != -1)
+			printf("Discarded %i stars not in this big healpix.\n", ndiscarded);
 		printf("Discarded %i duplicate stars.\n", nduplicates);
 
-		if (maxperhp) {
+		if (maxperhp)
 			for (i=0; i<HP; i++) {
 				int size;
 				stardata* d;
@@ -352,7 +367,6 @@ int main(int argc, char** args) {
 				if (size < maxperhp) continue;
 				bl_remove_index_range(starlists[i], maxperhp, size-maxperhp);
 			}
-		}
 
 		an_catalog_close(ancat);
 	}
@@ -364,62 +378,55 @@ int main(int argc, char** args) {
 			owned[i] = 0;
 
 	// sweep through the healpixes...
-	{
-		stardata* sweeplist;
-		int nowned = 0;
-		for (i=0; i<HP; i++)
-			if (owned[i])
-				nowned++;
-		sweeplist = malloc(nowned * sizeof(stardata));
+	for (i=0; i<HP; i++)
+		if (owned[i])
+			nowned++;
+	sweeplist = malloc(nowned * sizeof(stardata));
 
-		for (k=0;; k++) {
-			nowned = 0;
-			// gather up the stars that will be used in this sweep...
-			for (i=0; i<HP; i++) {
-				stardata* sd;
-				int N;
-				if (!owned[i]) continue;
-				N = bl_size(starlists[i]);
-				if (k >= N) {
-					owned[i] = 0;
-					continue;
-				}
-				sd = bl_access(starlists[i], k);
-				memcpy(sweeplist + nowned, sd, sizeof(stardata));
-				nowned++;
+	for (k=0;; k++) {
+		nowned = 0;
+		// gather up the stars that will be used in this sweep...
+		for (i=0; i<HP; i++) {
+			stardata* sd;
+			int N;
+			if (!owned[i]) continue;
+			N = bl_size(starlists[i]);
+			if (k >= N) {
+				owned[i] = 0;
+				continue;
 			}
+			sd = bl_access(starlists[i], k);
+			memcpy(sweeplist + nowned, sd, sizeof(stardata));
+			nowned++;
+		}
 
-			// sort them by magnitude...
-			qsort(sweeplist, nowned, sizeof(stardata), sort_stardata_mag);
+		// sort them by magnitude...
+		qsort(sweeplist, nowned, sizeof(stardata), sort_stardata_mag);
 
-			// write them out...
-			for (i=0; i<nowned; i++) {
-				double xyz[3];
-				stardata* sd = sweeplist + i;
-				xyz[0] = radec2x(deg2rad(sd->ra), deg2rad(sd->dec));
-				xyz[1] = radec2y(deg2rad(sd->ra), deg2rad(sd->dec));
-				xyz[2] = radec2z(deg2rad(sd->ra), deg2rad(sd->dec));
+		// write them out...
+		for (i=0; i<nowned; i++) {
+			double xyz[3];
+			stardata* sd = sweeplist + i;
+			radec2xyzarr(deg2rad(sd->ra), deg2rad(sd->dec), xyz);
 
-				catalog_write_star(cat, xyz);
-				idfile_write_anid(id, sd->id);
+			catalog_write_star(cat, xyz);
+			idfile_write_anid(id, sd->id);
 				
-				nwritten++;
-				if (nwritten == maxperbighp)
-					break;
-			}
-			printf("sweep %i: got %i stars (%i total)\n", k, nowned, nwritten);
-			fflush(stdout);
-			// if we broke out of the loop...
+			nwritten++;
 			if (nwritten == maxperbighp)
 				break;
-			if (!nowned)
-				break;
 		}
-		printf("Made %i sweeps through the healpixes.\n", k);
+		printf("sweep %i: got %i stars (%i total)\n", k, nowned, nwritten);
 		fflush(stdout);
-
-		free(sweeplist);
+		// if we broke out of the loop...
+		if (nwritten == maxperbighp)
+			break;
+		if (!nowned)
+			break;
 	}
+	printf("Made %i sweeps through the healpixes.\n", k);
+
+	free(sweeplist);
 
 	catalog_fix_header(cat);
 	catalog_close(cat);
