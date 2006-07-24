cfce674f7043b5b4995e2801ec592444ba1f0c26
Dustin Lang <dstndstn@gmail.com>
2006-07-24 23:19:43 +0000

    move closer to prospectus script

--- src/quads/codeprojections.m
+++ src/quads/codeprojections.m
@@ -1,37 +1,30 @@
-figure(1);
-mx=max(max(max(max(hist_1_0,hist_2_0),max(max(hist_2_1,hist_3_0),max(hist_3_1,hist_3_2)))));
-mn=min(min(min(min(hist_1_0,hist_2_0),min(min(hist_2_1,hist_3_0),min(hist_3_1,hist_3_2)))));
-for i=1:3,
-  for j=0:(i-1),
-    subplot(3,3,(i-1)*3+j+1);
-    h=eval(sprintf('hist_%i_%i', i, j));
-    s=surf(h);
-    set(s, 'EdgeColor', 'interp');
-    set(s, 'FaceColor', 'interp');
-    %set(s, 'EdgeAlpha', 0);
-    set(gca,'XTickLabel',{});
-    set(gca,'YTickLabel',{});
-    a=axis;
-    a(5)=0;
-    a(6)=mx*1.1;
-    axis(a);
-  end
+%
+% variables:
+%    print_plots  boolean
+%    plot_suffix  string, default ''
+%    batch        boolean, wait between plots?
+
+if ~exist('plot_suffix')
+  plot_suffix = '';
 end
-colormap hot;
+if ~exist('print_plots')
+  print_plots = 0;
+end
 
-input('next');
+do_density = exist('dhist_xy');
 
-if (exist('dhist_xy')),
-  mx=max(max(max(max(dhist_1_0,dhist_2_0),max(max(dhist_2_1,dhist_3_0),max(dhist_3_1,dhist_3_2)))));
-  mn=min(min(min(min(dhist_1_0,dhist_2_0),min(min(dhist_2_1,dhist_3_0),min(dhist_3_1,dhist_3_2)))));
+figure(1);
+
+if 0,
+  mx=max(max(max(max(hist_1_0,hist_2_0),max(max(hist_2_1,hist_3_0),max(hist_3_1,hist_3_2)))));
+  mn=min(min(min(min(hist_1_0,hist_2_0),min(min(hist_2_1,hist_3_0),min(hist_3_1,hist_3_2)))));
   for i=1:3,
     for j=0:(i-1),
       subplot(3,3,(i-1)*3+j+1);
-      h=eval(sprintf('dhist_%i_%i', i, j));
+      h=eval(sprintf('hist_%i_%i', i, j));
       s=surf(h);
       set(s, 'EdgeColor', 'interp');
       set(s, 'FaceColor', 'interp');
-      %set(s, 'EdgeAlpha', 0);
       set(gca,'XTickLabel',{});
       set(gca,'YTickLabel',{});
       a=axis;
@@ -41,17 +34,44 @@ if (exist('dhist_xy')),
     end
   end
   colormap hot;
-  input('next');
+  if print_plots,
+    print -depsc ['cp_hists', plot_suffix];
+  end
+  if ~batch,
+    input('next');
+  end
+  if do_density,
+    mx=max(max(max(max(dhist_1_0,dhist_2_0),max(max(dhist_2_1,dhist_3_0),max(dhist_3_1,dhist_3_2)))));
+    mn=min(min(min(min(dhist_1_0,dhist_2_0),min(min(dhist_2_1,dhist_3_0),min(dhist_3_1,dhist_3_2)))));
+    for i=1:3,
+      for j=0:(i-1),
+	subplot(3,3,(i-1)*3+j+1);
+	h=eval(sprintf('dhist_%i_%i', i, j));
+	s=surf(h);
+	set(s, 'EdgeColor', 'interp');
+	set(s, 'FaceColor', 'interp');
+	set(gca,'XTickLabel',{});
+	set(gca,'YTickLabel',{});
+	a=axis;
+	a(5)=0;
+	a(6)=mx*1.1;
+	axis(a);
+      end
+    end
+    colormap hot;
+    if print_plots,
+      print -depsc ['cp_dhists', plot_suffix];
+    end
+    if ~batch,
+      input('next');
+    end
+  end
 end
 
-%figure(2);
 for i=1:3,
   for j=0:(i-1),
     subplot(3,3,(i-1)*3+j+1);
     h=eval(sprintf('hist_%i_%i', i, j));
-    %s=surf(h);
-    %set(s, 'EdgeAlpha', 0);
-    %view(0,90);
     imagesc(h);
     axis tight;
     axis square;
@@ -61,10 +81,14 @@ for i=1:3,
   end
 end
 colormap hot;
+if print_plots,
+  print -depsc ['cp_2dhists', plot_suffix];
+end
+if ~batch,
+  input('next');
+end
 
-input('next');
-
-if (exist('dhist_xy')),
+if do_density,
   for i=1:3,
     for j=0:(i-1),
       subplot(3,3,(i-1)*3+j+1);
@@ -78,10 +102,14 @@ if (exist('dhist_xy')),
     end
   end
   colormap hot;
-  input('next');
+  if print_plots,
+    print -depsc ['cp_2ddhists', plot_suffix];
+  end
+  if ~batch,
+    input('next');
+  end
 end
 
-%figure(3);
 mx=max([hist_0 hist_1 hist_2 hist_3]);
 for i=1:4,
   subplot(4,1,i);
@@ -90,10 +118,14 @@ for i=1:4,
   axis([0 100 0 1.2*mx]);
   set(gca,'XTickLabel',{});
 end
+if print_plots,
+  print -depsc ['cp_1dhists', plot_suffix];
+end
+if ~batch,
+  input('next');
+end
 
-input('next');
-
-if (exist('dhist_xy')),
+if do_density,
   mx=max([dhist_0 dhist_1 dhist_2 dhist_3]);
   for i=1:4,
     subplot(4,1,i);
@@ -102,14 +134,16 @@ if (exist('dhist_xy')),
     axis([0 100 0 1.2*mx]);
     set(gca,'XTickLabel',{});
   end
+end
+if print_plots,
+  print -depsc ['cp_1ddhists', plot_suffix];
+end
+if ~batch,
   input('next');
 end
 
-%figure(4);
+
 subplot(1,1,1);
-%s=surf(hist_xy);
-%set(s, 'EdgeAlpha', 0);
-%view(0,90);
 imagesc(hist_xy);
 set(gca,'YDir','normal');
 set(gca,'XTickLabel',{});
@@ -120,10 +154,14 @@ xlabel('x')
 ylabel('y') 
 title('cx vs cy + dx vs dy')
 colormap hot
+if print_plots,
+  print -depsc ['cp_xy', plot_suffix];
+end
+if ~batch,
+  input('next');
+end
 
-input('next');
-
-if (exist('dhist_xy')),
+if do_density,
   subplot(1,1,1);
   imagesc(dhist_xy);
   set(gca,'YDir','normal');
@@ -135,4 +173,10 @@ if (exist('dhist_xy')),
   ylabel('y') 
   title('cx vs cy + dx vs dy')
   colormap hot
+  if print_plots,
+    print -depsc ['cp_dxy', plot_suffix];
+  end
+  if ~batch,
+    input('next');
+  end
 end
