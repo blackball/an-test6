fe98a3eaa73842b7a7bf24feb23b3da9df08dc0a
Dustin Lang <dstndstn@gmail.com>
2006-08-10 17:35:20 +0000

    add image-labelling code

---
+++ src/quads/label.h
@@ -0,0 +1,18 @@
+unsigned char label_image_char(unsigned char* source, int W, int H, unsigned char* dest);
+int  label_image_int(unsigned char* source, int W, int H, int* dest);
+
+struct label_info {
+  int label;
+  int xlow;
+  int xhigh;
+  int ylow;
+  int yhigh;
+  int mass;
+  float xcenter;
+  float ycenter;
+};
+typedef struct label_info label_info;
+
+label_info* label_image_super(unsigned char* source, int W, int H, int* dest, int* N);
+
+inline float roundness(label_info* label, int* labelim, int WW);
