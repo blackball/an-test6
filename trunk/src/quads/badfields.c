599e5478f7d15e251f61623041da38159f452441
Dustin Lang <dstndstn@gmail.com>
2006-08-14 14:19:52 +0000

    *** empty log message ***

--- trunk/src/quads/badfields.c
+++ trunk/src/quads/badfields.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include "solvedfile.h"
 
 struct badfield {
 	int run;
@@ -14,14 +17,42 @@ static badfield baddies[] = {
 #include "badfields.inc"
 };
 
+const char* OPTIONS = "hs:";
+
+void printHelp(char* progname) {
+	fprintf(stderr, "Usage: %s\n"
+			"   [-s <solved-file-template>]\n"
+			"\n", progname);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
 int main(int argc, char** args) {
+    int argchar;
+	char* progname = args[0];
 	int i;
 	int NB = sizeof(baddies) / sizeof(badfield);
+	char* solvedtemplate = NULL;
+
+    while ((argchar = getopt (argc, args, OPTIONS)) != -1) {
+		switch (argchar) {
+		case 's':
+			solvedtemplate = optarg;
+			break;
+		case 'h':
+		default:
+			printHelp(progname);
+			exit(-1);
+		}
+	}
 
 	printf("Number of bad field entries: %i.\n", NB);
-	for (i=0; i<NB; i++) {
-		printf("   run %i, fields %i-%i.\n", baddies[i].run, baddies[i].firstfield, baddies[i].lastfield);
-	}
+	/*
+	  for (i=0; i<NB; i++) {
+	  printf("   run %i, fields %i-%i.\n", baddies[i].run, baddies[i].firstfield, baddies[i].lastfield);
+	  }
+	*/
 
 	for (;;) {
 		char line[256];
@@ -33,8 +64,8 @@ int main(int argc, char** args) {
 			exit(-1);
 		}
 		if (sscanf(line, "%i %i %i %i %i %i %i %i\n",
-				   &filenum, &fieldnum, &run, &field,
-				   &rerun, &camcol, &filter, &ifield) != 8) {
+				   &filenum, &fieldnum, &run, &rerun, 
+				   &camcol, &field, &filter, &ifield) != 8) {
 			fprintf(stderr, "Failed to parse line: %s\n", line);
 			continue;
 		}
@@ -44,8 +75,17 @@ int main(int argc, char** args) {
 				  (field >= bad->firstfield) &&
 				  (field <= bad->lastfield)))
 				continue;
-			printf("%i %i %i %i \"%s\" \"%s\"\n", filenum, fieldnum, run, field,
-				   bad->problem, bad->comments);
+			if (solvedtemplate) {
+				char fn[256];
+				sprintf(fn, solvedtemplate, filenum);
+				if (solvedfile_get(fn, fieldnum) == 0) {
+					printf("%i %i %i %i \"%s\" \"%s\"\n", filenum, fieldnum, run, field,
+						   bad->problem, bad->comments);
+				}
+			} else {
+				printf("%i %i %i %i \"%s\" \"%s\"\n", filenum, fieldnum, run, field,
+					   bad->problem, bad->comments);
+			}
 		}
 	}
 
