814362b7f22b84b6ab66422ed2bdb3f9b4104afd
Dustin Lang <dstndstn@gmail.com>
2006-09-05 13:47:32 +0000

    *** empty log message ***

--- trunk/src/quads/usnobtile.c
+++ trunk/src/quads/usnobtile.c
@@ -3,12 +3,14 @@
 #include <math.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/mman.h>
 
-//#include "usnob_fits.h"
 #include "an_catalog.h"
 #include "starutil.h"
 #include "healpix.h"
 #include "mathutil.h"
+#include "ppm.h"
+#include "pnm.h"
 
 #define OPTIONS "x:y:X:Y:w:h:"
 
@@ -18,24 +20,22 @@ extern int optind, opterr, optopt;
 int main(int argc, char *argv[]) {
     int argchar;
 	bool gotx, goty, gotX, gotY, gotw, goth;
-	double x0, x1, y0, y1;
-	int w, h;
+	double x0=0.0, x1=0.0, y0=0.0, y1=0.0;
+	int w=0, h=0;
 	double xyz0[3];
 	double xyz1[3];
 	double center[3];
 	double r2;
 	uint hp;
 	int Nside = 9;
-	//char fntemplate = "/h/42/dstn/local2/USNOB10/fits/usnob10_hp%03i.fits";
 	char* fntemplate = "/h/42/dstn/local/AN/an_hp%03i.fits";
+	char* map_template = "/h/42/dstn/local/maps/usnob-zoom%i.ppm";
 	char fn[256];
-	//usnob_fits* usnob;
 	an_catalog* cat;
 	int i, N;
 	int valid;
 	uchar* image;
 	double scale;
-	//uchar maxval;
 
 	gotx = goty = gotX = gotY = gotw = goth = FALSE;
 
@@ -72,16 +72,164 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
+	x0 = deg2rad(x0);
+	x1 = deg2rad(x1);
+	y0 = deg2rad(y0);
+	y1 = deg2rad(y1);
+
+	{
+		// projected
+		double px0, py0, px1, py1;
+		double xperpix, yperpix;
+		double xzoom, yzoom;
+		int zoomlevel;
+		char fn[256];
+		FILE* fin;
+		int rows, cols, format;
+		pixval maxval;
+		off_t imgstart;
+		unsigned char* map;
+		size_t mapsize;
+		unsigned char* img;
+		int xpix0, xpix1, ypix0, ypix1;
+		int y;
+
+		px0 = x0 / (2.0 * M_PI);
+		px1 = x1 / (2.0 * M_PI);
+		py0 = asinh(tan(y0));
+		py1 = asinh(tan(y1));
+		xperpix = fabs(px1 - px0) / (double)w;
+		yperpix = fabs(py1 - py0) / (double)h;
+		fprintf(stderr, "Projected X range: [%g, %g]\n", px0, px1);
+		fprintf(stderr, "Projected Y range: [%g, %g]\n", py0, py1);
+		fprintf(stderr, "X,Y pixel scale: %g, %g.\n", xperpix, yperpix);
+		xzoom = 1.0 / (xperpix * 256.0);
+		yzoom = 1.0 / (yperpix * 256.0 / (2.0 * M_PI));
+		fprintf(stderr, "X,Y zoom %g, %g\n", xzoom, yzoom);
+		if ((fabs(xzoom - rint(xzoom)) > 0.05) ||
+			(fabs(xzoom - yzoom) > 0.05)) {
+			fprintf(stderr, "Invalid zoom level.\n");
+			exit(-1);
+		}
+		zoomlevel = (int)rint(log(xzoom) / log(2.0));
+		fprintf(stderr, "Zoom level %i.\n", zoomlevel);
+		sprintf(fn, map_template, zoomlevel);
+		fprintf(stderr, "Loading image from file %s.\n", fn);
+		fin = pm_openr_seekable(fn);
+		ppm_readppminit(fin, &cols, &rows, &maxval, &format);
+		if (PNM_FORMAT_TYPE(format) != PPM_TYPE) {
+			fprintf(stderr, "Map file must be PPM.\n");
+			exit(-1);
+		}
+		if (maxval != 255) {
+			fprintf(stderr, "Error: PPM maxval %i (not 255).\n", maxval);
+			exit(-1);
+		}
+		imgstart = ftello(fin);
+		mapsize = cols * rows * 3;
+		map = mmap(NULL, mapsize, PROT_READ, MAP_SHARED, fileno(fin), 0);
+		if (map == MAP_FAILED) {
+			fprintf(stderr, "Failed to mmap image file.\n");
+			exit(-1);
+		}
+		img = map + imgstart;
+
+		if (px0 < 0.0) {
+			px0 += 1.0;
+			px1 += 1.0;
+			fprintf(stderr, "Wrapping X around to projected range [%g, %g]\n", px0, px1);
+		}
+
+		xpix0 = px0 / xperpix;
+		ypix0 = (py0 + M_PI) / yperpix;
+		xpix1 = px1 / xperpix;
+		ypix1 = (py1 + M_PI) / yperpix;
+
+		fprintf(stderr, "Pixel positions: (%i,%i), (%i,%i) vs (%i,%i)\n", xpix0, ypix0, xpix0+w, ypix0+h, xpix1, ypix1);
+
+		xpix1 = xpix0 + w;
+		ypix1 = ypix0 + h;
+
+		/*
+		  if (xpix1 < xpix0) {
+		  int tmp = xpix0;
+		  xpix0 = xpix1;
+		  xpix1 = tmp;
+		  fprintf(stderr, "Swapping xpix0 and xpix1.\n");
+		  }
+		  if (ypix1 < ypix0) {
+		  int tmp = ypix0;
+		  ypix0 = ypix1;
+		  ypix1 = tmp;
+		  fprintf(stderr, "Swapping ypix0 and ypix1.\n");
+		  }
+		  if (xpix0 < 0) {
+		  fprintf(stderr, "Shifting xpix0 from %i up to 0.\n", xpix0);
+		  xpix0 = 0;
+		  }
+		  if (ypix0 < 0) {
+		  fprintf(stderr, "Shifting ypix0 from %i up to 0.\n", ypix0);
+		  ypix0 = 0;
+		  }
+		  if (xpix0 >= cols) {
+		  fprintf(stderr, "Shifting xpix0 from %i down to %i.\n", xpix0, cols-1);
+		  xpix0 = cols-1;
+		  }
+		  if (ypix0 >= rows) {
+		  fprintf(stderr, "Shifting ypix0 from %i down to %i.\n", ypix0, rows-1);
+		  ypix0 = rows-1;
+		  }
+		  if (xpix1 < 0) {
+		  fprintf(stderr, "Shifting xpix1 from %i up to 0.\n", xpix1);
+		  xpix1 = 0;
+		  }
+		  if (ypix1 < 0) {
+		  fprintf(stderr, "Shifting ypix1 from %i up to 0.\n", ypix1);
+		  ypix1 = 0;
+		  }
+		  if (xpix1 >= cols) {
+		  fprintf(stderr, "Shifting xpix1 from %i down to %i.\n", xpix1, cols-1);
+		  xpix1 = cols-1;
+		  }
+		  if (ypix1 >= rows) {
+		  fprintf(stderr, "Shifting ypix1 from %i down to %i.\n", ypix1, rows-1);
+		  ypix1 = rows-1;
+		  }
+		  if ((xpix1 - xpix0) != w) {
+		  fprintf(stderr, "Changing xpix1 to match image size %i.\n", w);
+		  xpix1 = xpix0 + w;
+		  }
+		  if ((ypix1 - ypix0) != h) {
+		  fprintf(stderr, "Changing ypix1 to match image size %i.\n", h);
+		  ypix1 = ypix0 + h;
+		  }
+		  fprintf(stderr, "Pixel positions: (%i,%i), (%i,%i)\n", xpix0, ypix0, xpix1, ypix1);
+		*/
+
+		if (xpix0 < 0 || ypix0 < 0 || xpix1 > cols || ypix1 > rows) {
+			fprintf(stderr, "Requested pixels (%i,%i) to (%i,%i) aren't within image bounds (0,0),(%i,%i)\n",
+					xpix0, ypix0, xpix1, ypix1, cols, rows);
+			exit(-1);
+		}
+
+		// output PPM.
+		printf("P6 %d %d %d\n", w, h, 255);
+		for (y=ypix0; y<ypix1; y++) {
+			unsigned char* start = img + 3*((y * cols) + xpix0);
+			fwrite(start, 1, w*3, stdout);
+		}
+
+		munmap(map, mapsize);
+
+		return 0;
+	}
+
 	image = calloc(w*h, 1);
 	if (!image) {
 		fprintf(stderr, "Failed to allocate image.\n");
 		exit(-1);
 	}
 
-	x0 = deg2rad(x0);
-	x1 = deg2rad(x1);
-	y0 = deg2rad(y0);
-	y1 = deg2rad(y1);
 	radec2xyzarr(x0, y0, xyz0);
 	radec2xyzarr(x1, y1, xyz1);
 	star_midpoint(center, xyz0, xyz1);
@@ -99,9 +247,6 @@ int main(int argc, char *argv[]) {
 	sprintf(fn, fntemplate, hp);
 
 	fprintf(stderr, "Healpix %i.\n", hp);
-
-	//usnob = usnob_fits_open(fn);
-	//N = usnob_fits_count_entries(usnob);
 
 	// HACK here
 	scale = (double)w / sqrt(r2);
@@ -129,7 +274,6 @@ int main(int argc, char *argv[]) {
 		star_coords(xyz, center, &u, &v);
 		u *= scale;
 		v *= scale;
-		//fprintf(stderr, "uv=(%f, %f)\n", u, v);
 		iu = (int)rint(u);
 		iv = (int)rint(v);
 		if ((iu < 0) || (iv < 0) || (iu >= w) || (iv >= h))
