a7de7c9a6df81f43a5c54479dbee3681303544e3
Dustin Lang <dstndstn@gmail.com>
2006-09-06 17:09:43 +0000

    kdtree: clean up macros.h; add cached statistics to kdtree node.

--- src/quads/kdtree/kdtree_macros.h
+++ src/quads/kdtree/kdtree_macros.h
@@ -9,38 +9,38 @@
 
 /* Most macros operate on a variable kdtree_t *kd, assumed to exist. */
 /* x is a node index, d is a dimension, and n is a point index */
+
 #if defined(KD_DIM)
-
-#define NODE_SIZE      (sizeof(kdtree_node_t) + sizeof(real)*KD_DIM*2)
-#define HIGH_HR(x, d)  ((real*) (((void*)kd->tree) + NODE_SIZE*(x) \
-                                 + sizeof(kdtree_node_t) \
-                                 + sizeof(real)*((d) + KD_DIM)))
-#define COORD(n,d)     ((real*)(kd->data + KD_DIM*(n) + (d)))
-#define NODE_HIGH_BB(n) ((real*)((char*)(n) \
-								 + sizeof(kdtree_node_t) \
-								 + sizeof(real)*KD_DIM))
-
+#define DIMENSION   (KD_DIM)
 #else
-
-#define NODE_SIZE      (sizeof(kdtree_node_t) + sizeof(real)*kd->ndim*2)
-#define NODE_HIGH_BB(n) ((real*)((char*)(n) \
-								 + sizeof(kdtree_node_t) \
-								 + sizeof(real)*kd->ndim))
-#define HIGH_HR(x, d)  ((real*) (((void*)kd->tree) + NODE_SIZE*(x) \
-                                 + sizeof(kdtree_node_t) \
-                                 + sizeof(real)*((d) \
-                                 + kd->ndim)))
-#define COORD(n,d)     ((real*)(kd->data + kd->ndim*(n) + (d)))
-
+#define DIMENSION   (kd->ndim)
 #endif
 
+#define SIZEOF_PT  (sizeof(real)*DIMENSION)
 
+#define NODE_SIZE      (sizeof(kdtree_node_t) + (SIZEOF_PT * 2))
 
+#define HIGH_HR(x, d)  ((real*) (((char*)kd->tree) \
+                                 + NODE_SIZE*(x) \
+                                 + sizeof(kdtree_node_t) \
+                                 + SIZEOF_PT \
+                                 + sizeof(real)*(d)
+
+#define COORD(n,d)     ((real*)(kd->data + DIMENSION*(n) + (d)))
+
+#define NODE_HIGH_BB(n) ((real*)((char*)(n) \
+								 + sizeof(kdtree_node_t) \
+								 + sizeof(real)*DIMENSION))
+
+								 // warning, this only works if "n" has type kdtree_node_t*.
 #define NODE_LOW_BB(n) ((real*)((n)+1))
-#define NODE(x)        ((kdtree_node_t*) (((void*)kd->tree) + NODE_SIZE*(x)))
-#define LOW_HR(x, d)   ((real*) (((void*)kd->tree) + NODE_SIZE*(x) \
+
+#define NODE(x)        ((kdtree_node_t*) (((char*)kd->tree) + NODE_SIZE*(x)))
+
+#define LOW_HR(x, d)   ((real*) (((char*)kd->tree) + NODE_SIZE*(x) \
                                  + sizeof(kdtree_node_t) \
                                  + sizeof(real)*(d)))
+
 #define PARENT(x)      NODE(((x)-1)/2)
 #define CHILD_NEG(x)   NODE(2*(x) + 1)
 #define CHILD_POS(x)   NODE(2*(x) + 2)
