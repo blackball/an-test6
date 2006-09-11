7875cd2b30697cc2169ffcd6f363f364e05b94c6
Keir Mierle <mierle@gmail.com>
2006-09-11 02:35:16 +0000

    Makefile for tweak; compiles

--- src/tweak/src/tweak.c
+++ src/tweak/src/tweak.c
@@ -8,10 +8,37 @@
 #include "libwcs/fitsfile.h"
 #include "libwcs/wcs.h"
 #include "starutil.h"
+#include "fitsio.h"
+
+typedef struct WorldCoor wcs_t;
+
+// hacky. pull the complete hdu from the current hdu, then use wcstools to
+// figure out the wcs
+wcs_t* get_wcs_from_hdu(fitsfile* infptr)
+{
+	int mystatus;
+	int* status = &mystatus;
+	int nkeys, ii;
+
+	if (ffghsp(infptr, &nkeys, NULL, status) > 0) /* get no. of keywords */
+		return NULL;
+
+	/* create a memory buffer to hold the header records */
+	int tmpbufflen = nkeys*FLEN_CARD*sizeof(char);
+	char* tmpbuff = malloc(tmpbufflen);
+	if (!tmpbuff)
+		return NULL;
+
+	/* read all of the header records in the input HDU */
+	for (ii = 0; ii < nkeys; ii++)
+		ffgrec(infptr, ii+1, tmpbuff + (ii * FLEN_CARD), status);
+
+	return wcsninit(tmpbuff, tmpbufflen);
+}
 
 int get_xy(char* filename, double **x, double **y, int *n)
 {
-
+	return 0;
 }
 
 int get_center_and_radius(double* ra, double* dec, int n,
@@ -35,7 +62,7 @@ int get_center_and_radius(double* ra, double* dec, int n,
 	for (i=0; i<n; i++) {
 		double dist2 = 0;
 		for (j=0; j<3; j++) 
-			dist2 = += (xyz_mean[j]-xyz[3*i+j])*(xyz_mean[j]-xyz[3*i+j]);
+			dist2 += (xyz_mean[j]-xyz[3*i+j])*(xyz_mean[j]-xyz[3*i+j]);
 		if (maxdist2 < dist2) {
 			maxdist2 = dist2;
 			maxind = i;
@@ -56,9 +83,72 @@ int get_reference_stars(double ra_mean, double dec_mean, double radius,
 
 int main(int argc, char *argv[])
 {
-	struct WorldCoor* wcs;
+	wcs_t* wcs;
+	/* Are there multiple images? */
+	fitsfile *fptr;         /* FITS file pointer, defined in fitsio.h */
+	fitsfile *ofptr;        /* FITS file pointer to output file */
+	int status = 0; // FIXME should have ostatus too
+	int naxis;
+	long naxisn[2];
+	int kk, jj;
 
-	wcs = GetWCSFITS(argv[1], 0)
+	if (argc != 2) {
+		fprintf(stderr, "Usage: tweak filename.fits \n");
+		fprintf(stderr, "Assumes filename.xy.fits already exists\n");
+		return (0);
+	}
+
+	if (fits_open_file(&fptr, argv[1], READONLY, &status)) {
+		fprintf(stderr, "Error reading file %s\n", argv[1]);
+		fits_report_error(stderr, status);
+		exit(-1);
+	}
+
+	// Are there multiple HDU's?
+	int nhdus;
+	fits_get_num_hdus(fptr, &nhdus, &status);
+	fprintf(stderr, "nhdus=%d\n", nhdus);
+
+	int hdutype;
+	int nimgs = 0;
+
+	// Tweak each HDU independently
+	for (kk=1; kk <= nhdus; kk++) {
+		fits_movabs_hdu(fptr, kk, &hdutype, &status);
+		fits_get_hdu_type(fptr, &hdutype, &status);
+
+		if (hdutype != IMAGE_HDU) 
+			continue;
+
+		fits_get_img_dim(fptr, &naxis, &status);
+		if (status) {
+			fits_report_error(stderr, status);
+			exit( -1);
+		}
+
+		if (naxis != 2) {
+			fprintf(stderr, "Invalid image: NAXIS is not 2 in HDU %d!\n", kk);
+			continue;
+		}
+
+		// At this point, we have an image. Now get the WCS data
+		wcs = get_wcs_from_hdu(fptr);
+		fprintf(stderr, "GOT WCS INFO!!! %p \n", wcs);
+
+	}
+
+
+	if (status == END_OF_FILE)
+		status = 0; /* Reset after normal error */
+
+	fits_close_file(fptr, &status);
+	fits_close_file(ofptr, &status);
+
+
+	if (status)
+		fits_report_error(stderr, status); /* print any error message */
+	return (status);
+	
 
 	/* Get original WCS from image */
 	/* Get xy positions of stars in image */
