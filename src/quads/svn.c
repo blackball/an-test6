caacb8ef500e2720f85d72951d36c3b778382560
Dustin Lang <dstndstn@gmail.com>
2006-09-19 14:15:01 +0000

    work on makeindex

--- src/quads/svn.c
+++ src/quads/svn.c
@@ -10,6 +10,7 @@ const char* svn_date() {
 	   be replaced by the correct, updated string whenever you run "svn up".  All hail
 	   subversion! */
 	const char* datestr = "$Date$";
+	// (I want to trim off the "$Date$" trailing: 7 chars at front and 2 at back)
 	strncpy(rtnval, datestr + 7, strlen(datestr) - 9);
 	return rtnval;
 }
@@ -18,6 +19,8 @@ int svn_revision() {
 	int rev;
 	/* See the comment above; the same thing is true of "revstr". Huzzah! */
 	const char* revstr = "$Revision$";
+	// revstr+1 to avoid having "$" in the format string - otherwise svn seems to
+	// consider it close enough to the Revision keyword anchor to do replacement!
 	if (sscanf(revstr + 1, "Revision: %i $", &rev) != 1)
 		return -1;
 	return rev;
