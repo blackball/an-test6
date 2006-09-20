0a1e89f1835745461853ccf33c6c4d814c3a79a4
Dustin Lang <dstndstn@gmail.com>
2006-09-20 00:07:00 +0000

    fix our URL in boilerplate.

--- src/quads/boilerplate.c
+++ src/quads/boilerplate.c
@@ -5,14 +5,14 @@
 
 void boilerplate_help_header(FILE* fid) {
 	fprintf(fid, "This program is part of the Astrometry.net suite.\n");
-	fprintf(fid, "For details, visit  http://www.astrometry.net .\n");
+	fprintf(fid, "For details, visit  http://astrometry.net .\n");
     fprintf(fid, "Subversion revision %i, date %s.\n", svn_revision(), svn_date());
 }
 
 void boilerplate_add_fits_headers(qfits_header* hdr) {
 	char val[64];
 	qfits_header_add(hdr, "HISTORY", "Created by the Astrometry.net suite.", NULL, NULL);
-	qfits_header_add(hdr, "HISTORY", "For more details, see http://www.astrometry.net .", NULL, NULL);
+	qfits_header_add(hdr, "HISTORY", "For more details, see http://astrometry.net .", NULL, NULL);
 	sprintf(val, "Subversion revision %i", svn_revision());
 	qfits_header_add(hdr, "HISTORY", val, NULL, NULL);
 	sprintf(val, "Subversion date %s", svn_date());
