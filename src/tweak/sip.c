39f609bf2340d495c0ca31473bc8f12f42490c3a
Keir Mierle <mierle@gmail.com>
2006-10-05 03:30:41 +0000

    WCS <-> SIP match

--- src/tweak/sip.c
+++ src/tweak/sip.c
@@ -39,6 +39,8 @@ void pixelxy2radec(sip_t* sip, double px, double py, double *a, double *d)
 	// Convert to radians??
 	u = deg2rad(u);
 	v = deg2rad(v);
+	//u *= -1;
+	//v *= -1;
 
 	//double u = px - sip->crpix[0];
 	//double v = py - sip->crpix[1];
@@ -58,14 +60,20 @@ void pixelxy2radec(sip_t* sip, double px, double py, double *a, double *d)
 				guv += sip->b[p][q]*pow(u,p)*pow(v,q);
 	u += fuv;
 	v += guv;
+	printf("u=%lf v=%lf\n",u,v);
 
 	// Get intermediate world coordinates
 	double x = sip->cd[0][0] * u + sip->cd[0][1] * v;
 	double y = sip->cd[1][0] * u + sip->cd[1][1] * v;
+	printf("x=%lf y=%lf\n",x,y);
+
+	x *= -1;
+	y *= -1;
 
 	// Take r to be the threespace vector of crval
 	double rx, ry, rz;
 	radec2xyz(deg2rad(sip->crval[0]), deg2rad(sip->crval[1]), &rx, &ry, &rz);
+	printf("rx=%lf ry=%lf rz=%lf\n",rx,ry,rz);
 
 	// Form i = r cross north pole, which is in direction of z
 	double ix = ry;
@@ -74,6 +82,8 @@ void pixelxy2radec(sip_t* sip, double px, double py, double *a, double *d)
 	double norm = sqrt(ix*ix + iy*iy);
 	ix /= norm;
 	iy /= norm;
+	printf("ix=%lf iy=%lf iz=0.0\n",ix,iy);
+	printf("r.i = %lf\n",ix*rx+iy*ry);
 
 	// Form j = r cross i, which is in the direction of y
 	double jx =  rx*rz;
@@ -84,6 +94,9 @@ void pixelxy2radec(sip_t* sip, double px, double py, double *a, double *d)
 	jx /= norm;
 	jy /= norm;
 	jz /= norm;
+	printf("jx=%lf jy=%lf jz=%lf\n",jx,jy,jz);
+	printf("r.j = %lf\n",jx*rx+jy*ry+jz*rz);
+	printf("i.j = %lf\n",ix*jx+iy*jy);
 
 	// Form the point on the tangent plane relative to observation point,
 	// and normalize back onto the unit sphere
@@ -94,6 +107,7 @@ void pixelxy2radec(sip_t* sip, double px, double py, double *a, double *d)
 	wx /= norm;
 	wy /= norm;
 	wz /= norm;
+	printf("wx=%lf wy=%lf wz=%lf\n",wx,wy,wz);
 
 	// We're done!
 	xyz2radec(wx,wy,wz,a,d);
@@ -112,11 +126,11 @@ void radec2pixelxy(sip_t* sip, double a, double d, double *px, double *py)
 	cdi[1][0] = -sip->cd[1][0] * inv_det;
 	cdi[1][1] =  sip->cd[0][0] * inv_det;
 
-	/*
 	printf(":: %lf\n",  (sip->cd[0][0]*cdi[0][0] + sip->cd[0][1]*cdi[1][0] ));
 	printf(":: %lf\n",  (sip->cd[0][0]*cdi[0][1] + sip->cd[0][1]*cdi[1][1] ));
 	printf(":: %lf\n",  (sip->cd[1][0]*cdi[0][0] + sip->cd[1][1]*cdi[1][0] ));
 	printf(":: %lf\n",  (sip->cd[1][0]*cdi[0][1] + sip->cd[1][1]*cdi[1][1] ));
+	/*
 	*/
 
 	//assert( (sip->cd[0][0]*cdi[0][0] + sip->cd[0][1]*cdi[1][0] ) == 1.0);
--- src/tweak/test_sip.c
+++ src/tweak/test_sip.c
@@ -182,15 +182,23 @@ void copy_wcs_into_sip(wcs_t* wcs, sip_t* sip)
 	sip->crval[1] = wcs->crval[1];
 	sip->crpix[0] = wcs->crpix[0];
 	sip->crpix[1] = wcs->crpix[1];
+	/*
 	sip->cd[0][0] = wcs->cd[0];
 	sip->cd[0][1] = wcs->cd[2];
 	sip->cd[1][0] = wcs->cd[1];
+	sip->cd[1][1] = wcs->cd[3];
+	*/
+	sip->cd[0][0] = wcs->cd[0];
+	sip->cd[0][1] = wcs->cd[1];
+	sip->cd[1][0] = wcs->cd[2];
 	sip->cd[1][1] = wcs->cd[3];
 }
 
 void tryad(sip_t* sip, wcs_t* wcs, double a, double d, char* name,
 		double *opx, double *opy)
 {
+	printf("AD -> XY Test: %s\n",name);
+	printf("a =%lf, d =%lf\n", a,d);
 	double px, py;
 	radec2pixelxy(sip, a,d,&px,&py);
 
@@ -201,32 +209,78 @@ void tryad(sip_t* sip, wcs_t* wcs, double a, double d, char* name,
 	if (opx) *opx = wcspx;
 	if (opy) *opy = wcspy;
 
-	printf("AD Test: %s\n",name);
 	printf("px =%lf, py =%lf\n", px,py);
 	printf("wpx=%lf, wpy=%lf, offscr=%d\n", wcspx,wcspy, offscr);
-
-	if (fabs(px-wcspx) > epsilon)
+	int fail=0;
+	if (fabs(px-wcspx) > epsilon) {
 		printf("** FAILURE X wcs -> pix \n");
-	if (fabs(py-wcspy) > epsilon)
+		fail++;
+	}
+	if (fabs(py-wcspy) > epsilon) {
 		printf("** FAILURE Y wcs -> pix \n");
+		fail++;
+	}
+
+	if (!fail)
+		printf("\n                                     PASS\n");
+	else
+		printf("\n                                    (    )\n");
+	printf("\n");
 }
 
 void tryxy(sip_t* sip, wcs_t* wcs, double px, double py, char* name)
 {
-	printf("XY Test: %s\n",name);
-	double a, d;
-	double wcsa, wcsd;
+	printf("XY -> AD Test: %s\n",name);
+	double wcsa, wcsd, a, d;
+	printf("TEST px=%lf, py=%lf\n", px,py);
 	pixelxy2radec(sip, px,py, &a, &d);
 	pix2wcs(wcs, px, py, &wcsa, &wcsd);
 	printf("sa=%lf, sd=%lf\n", a,d);
 	printf("wa=%lf, wd=%lf\n", wcsa,wcsd);
-	if (fabs(a-wcsa) > epsilon)
+	int fail = 0;
+	if (fabs(a-wcsa) > epsilon) {
 		printf("** FAILURE A on pix -> wcs\n");
-	if (fabs(d-wcsd) > epsilon)
+		fail++;
+	}
+	if (fabs(d-wcsd) > epsilon) {
 		printf("** FAILURE D on pix -> wcs\n");
+		fail++;
+	}
+	if (!fail)
+		printf("\n                                     PASS\n");
+	else
+		printf("\n                                    (    )\n");
 	printf("\n");
 }
 
+void tryxyadxy(sip_t* sip, wcs_t* wcs, double px, double py, char* name)
+{
+	printf("//// XY -> AD -> XY Test: %s\n",name);
+	printf("test px=%lf, py=%lf\n", px,py);
+
+	double a, d;
+	pixelxy2radec(sip, px,py, &a, &d);
+
+	double wcsa, wcsd;
+	pix2wcs(wcs, px, py, &wcsa, &wcsd);
+
+	printf("sa=%lf, sd=%lf\n", a,d);
+	printf("wa=%lf, wd=%lf\n", wcsa,wcsd);
+
+	double pxp, pyp;
+	radec2pixelxy(sip, a, d, &pxp, &pyp);
+
+	int offscr;
+	double wcspx,wcspy;
+	wcs2pix(wcs, wcsa, wcsd, &wcspx, &wcspy, &offscr);
+
+	printf("dspx=%lf, dspy=%lf\n", fabs(px-pxp),fabs(py-pyp));
+	printf("dwpx=%lf, dwpy=%lf\n", fabs(px-wcspx),fabs(py-wcspy));
+	printf("spx=%lf, spy=%lf\n", pxp,pyp);
+	printf("wpx=%lf, wpy=%lf\n", wcspx,wcspy);
+
+	printf("\n");
+}
 void grinder(sip_t* sip, wcs_t* wcs, int n, char* name)
 {
 
@@ -242,9 +296,19 @@ void grinder(sip_t* sip, wcs_t* wcs, int n, char* name)
 
 	printf("Make sure pixel origin goes to same place\n");
 	tryxy(sip,wcs,0,0,"  ");
+	tryxy(sip,wcs,1000,1000,"  ");
+	tryxy(sip,wcs,2000,2000,"  ");
+	tryxy(sip,wcs,3000,3000,"  ");
+	tryxy(sip,wcs,1000,3000,"  ");
+	tryxy(sip,wcs,0,3000,"  ");
+	tryxy(sip,wcs,3000,0,"  ");
+
+	tryxyadxy(sip,wcs,1000,1000,"  ");
+	tryxyadxy(sip,wcs,2000,2000,"  ");
+	tryxyadxy(sip,wcs,3000,3000,"  ");
+	tryxyadxy(sip,wcs,1000,3000,"  ");
 
 	for (i=0; i<n; i++) {
-		printf("case: \n");
 		double xyz[3];
 		xyz[0] = rand() / (double)RAND_MAX - 0.5;
 		xyz[1] = rand() / (double)RAND_MAX - 0.5;
@@ -286,7 +350,7 @@ void grinder(sip_t* sip, wcs_t* wcs, int n, char* name)
 		a = rad2deg(a);
 		d = rad2deg(d);
 
-		printf("a=%lf, d=%lf\n", a,d);
+		//printf("a=%lf, d=%lf\n", a,d);
 
 		double px,py;
 		tryad(sip,wcs,a,d,"...", &px, &py);
@@ -299,13 +363,13 @@ void grinder(sip_t* sip, wcs_t* wcs, int n, char* name)
 int main()
 {
 	srand(0);
-	int n=6;
+	int n=2;
 	sip_t* sip = createsip();
-	//grinder(sip, getwcs(fobj059), n, "fobj059 without TNX");
-	//grinder(sip, getwcs(simple) , n, "identity transform");
-	//grinder(sip, getwcs(simple2), n, "non-zero crpix");
-	//grinder(sip, getwcs(simple3), n, "non-zero crval");
-	//grinder(sip, getwcs(simple4), n, "non-rotation identity");
+	grinder(sip, getwcs(fobj059), n, "fobj059 without TNX");
+	grinder(sip, getwcs(simple) , n, "identity transform");
+	grinder(sip, getwcs(simple2), n, "non-zero crpix");
+	grinder(sip, getwcs(simple3), n, "non-zero crval");
+	grinder(sip, getwcs(simple4), n, "non-rotation identity");
 	grinder(sip, getwcs(simple5), n, "Simple case at 60,40deg");
 	return 0;
 }
