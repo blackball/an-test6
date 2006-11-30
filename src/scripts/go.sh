1d5fda400bd1d9a434f9ee2a449b8b440742744d
Dustin Lang <dstndstn@gmail.com>
2006-11-30 15:45:48 +0000

    switch to Sam's cut

--- src/scripts/go.sh
+++ src/scripts/go.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-mkdir -p ./tmp_go_dir
-cd tmp_go_dir
+mkdir -p ./astrometry_alpha_test
+cd astrometry_alpha_test
 echo "Removing old files..."
 rm -rf quads pipeline *.tar.gz
 echo "Downloading source..."
 wget http://monte.ai.toronto.edu:8080/quads/quads.tar.gz
 echo "Downloading data..."
-wget http://monte.ai.toronto.edu:8080/quads/cut-02.tar.gz
+wget http://monte.ai.toronto.edu:8080/quads/cut-02new.tar.gz
 echo "Downloading fields..."
 wget http://monte.ai.toronto.edu:8080/quads/sdssfield01-hp02.tar.gz
 echo "Extracting source..."
