84802c6ac14f91c09790039c0bed0fcef46d0a27
Dustin Lang <dstndstn@gmail.com>
2007-01-27 23:38:17 +0000

    gmaps: add indexquad2: like post-mortem

--- src/usnob-map/execs/indexquad2.c
+++ src/usnob-map/execs/indexquad2.c
@@ -29,80 +29,42 @@
 #include "ppm.h"
 #include "pnm.h"
 
-#include "starutil.h"
 #include "starkd.h"
 #include "kdtree.h"
+#include "starutil.h"
 #include "healpix.h"
 #include "mathutil.h"
 #include "bl.h"
+#include "permutedsort.h"
+#include "qidxfile.h"
+#include "quadfile.h"
 
 #define OPTIONS "x:y:X:Y:w:h:f:"
 
 extern char *optarg;
 extern int optind, opterr, optopt;
 
-static void addstar(uchar* img, int x, int y, int W, int H,
-					uchar r, uchar g, uchar b) {
-	/*
-	  int dx[] = { -1,  0,  1, -1,  0,  1, -2, -2, -2,  2,  2,  2 };
-	  int dy[] = { -2, -2, -2,  2,  2,  2, -1,  0,  1, -1,  0,  1 };
-	*/
-	int dx[] = { -1,  0,  1,  2, -1,  0,  1,  2,
-				 -2, -2, -2, -2,  3,  3,  3,  3 };
-	//-2, -2,  3,  3 };
-	int dy[] = { -2, -2, -2, -2,  3,  3,  3,  3,
-				 -1,  0,  1,  2, -1,  0,  1,  2 };
-	//-2,  3, -2,  3 };
-	/*
-	  int dx[] = { -1,  0,  1,  0,  0 };
-	  int dy[] = {  0,  0,  0, -1,  1 };
-	*/
-	int i;
-	for (i=0; i<sizeof(dx)/sizeof(int); i++) {
-		if ((x + dx[i] < 0) || (x + dx[i] >= W)) continue;
-		if ((y + dy[i] < 0) || (y + dy[i] >= H)) continue;
-		img[3*((y+dy[i])*W+(x+dx[i])) + 0] = r;
-		img[3*((y+dy[i])*W+(x+dx[i])) + 1] = g;
-		img[3*((y+dy[i])*W+(x+dx[i])) + 2] = b;
-	}
-}
-
 int main(int argc, char *argv[]) {
     int argchar;
+	char fn[256];
+	int i, j;
+	startree* skdt;
+	qidxfile* qidx;
+	quadfile* qfile;
+
 	bool gotx, goty, gotX, gotY, gotw, goth;
 	double x0=0.0, x1=0.0, y0=0.0, y1=0.0;
 	int w=0, h=0;
-
 	char* filename = NULL;
 	char* basedir = "/home/gmaps/gmaps-indexes/";
-
-	char fn[256];
-	int i;
-
+	double query[3];
+	double maxd2;
 	double px0, py0, px1, py1;
 	double pixperx, pixpery;
 	double xzoom, yzoom;
 	int zoomlevel;
-	int xpix0, xpix1, ypix0, ypix1;
-
-	int pixelmargin = 4;
-
-	uchar* img;
-	float xscale, yscale;
-	int Noob;
-	int Nib;
-	startree* skdt;
-	bool wrapra;
-	double query[3];
-	double maxd2;
-	double xmargin;
-	double ymargin;
-	double xorigin, yorigin;
-
-	double xlo, xhi, ylo, yhi, xlo2, xhi2;
 
 	kdtree_qres_t* res;
-	int j;
 
 	gotx = goty = gotX = gotY = gotw = goth = FALSE;
 
@@ -195,52 +157,29 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	if (px0 < 0.0) {
-		px0 += 1.0;
-		px1 += 1.0;
-		fprintf(stderr, "Wrapping X around to projected range [%g, %g]\n", px0, px1);
-	}
-
-	xpix0 = px0 * pixperx;
-	ypix0 = py0 * pixpery;
-	xpix1 = px1 * pixperx;
-	ypix1 = py1 * pixpery;
-
-	fprintf(stderr, "Pixel positions: (%i,%i), (%i,%i) vs (%i,%i)\n", xpix0, ypix0, xpix0+w, ypix0+h, xpix1, ypix1);
-
-	xpix1 = xpix0 + w;
-	ypix1 = ypix0 + h;
-
-	xorigin = px0;
-	yorigin = py0;
-	wrapra = (px1 > 1.0);
-	xmargin = (double)pixelmargin / pixperx;
-	ymargin= (double)pixelmargin / pixpery;
-
-	xlo = px0 - xmargin;
-	xhi = px1 + xmargin;
-	ylo = py0 - ymargin;
-	yhi = py1 + ymargin;
-
-	if (wrapra) {
-		xlo2 = 0.0;
-		xhi2 = xhi - 1.0;
-		xhi = 1.0;
-	} else {
-		xlo2 = xhi2 = 0.0;
-	}
-
-	img = calloc(w*h*3, sizeof(uchar));
-	if (!img) {
-		fprintf(stderr, "Failed to allocate  image.\n");
+	snprintf(fn, sizeof(fn), "%s%s.skdt.fits", basedir, filename);
+	fprintf(stderr, "Opening file %s...\n", fn);
+	skdt = startree_open(fn);
+	if (!skdt) {
+		fprintf(stderr, "Failed to open startree.\n");
 		exit(-1);
 	}
 
-	xscale = pixperx;
-	yscale = pixpery;
+	snprintf(fn, sizeof(fn), "%s%s.qidx.fits", basedir, filename);
+	fprintf(stderr, "Opening file %s...\n", fn);
+	qidx = qidxfile_open(fn, 0);
+	if (!qidx) {
+		fprintf(stderr, "Failed to open quadidx file.\n");
+		exit(-1);
+	}
 
-	Noob = 0;
-	Nib = 0;
+	snprintf(fn, sizeof(fn), "%s%s.quad.fits", basedir, filename);
+	fprintf(stderr, "Opening file %s...\n", fn);
+	qfile = quadfile_open(fn, 0);
+	if (!qfile) {
+		fprintf(stderr, "Failed to open quad file.\n");
+		exit(-1);
+	}
 
 	{
 		double p0[3], p1[3];
@@ -250,52 +189,56 @@ int main(int argc, char *argv[]) {
 		maxd2 = 0.25 * distsq(p0, p1, 3);
 	}
 
-	snprintf(fn, sizeof(fn), "%s%s", basedir, filename);
-	fprintf(stderr, "Opening file %s...\n", fn);
-	skdt = startree_open(fn);
-	if (!skdt) {
-		fprintf(stderr, "Failed to open startree.\n");
-		exit(-1);
-	}
-
 	res = kdtree_rangesearch_options(skdt->tree, query, maxd2, 0);
 	if (res)
 		fprintf(stderr, "%i results.\n", res->nres);
 
+	printf("<quads>\n");
 	for (j=0; res && j<res->nres; j++) {
-		double ra, dec;
-		double* xyz;
-		double px, py;
-		int ix, iy;
-		xyz = res->results.d + j*3;
-		xyz2radec(xyz[0], xyz[1], xyz[2], &ra, &dec);
-		px = ra / (2.0 * M_PI);
-		py = (M_PI + asinh(tan(dec))) / (2.0 * M_PI);
+		uint* quads;
+		uint nquads;
+		int k, s;
 
-		if (!( (py >= ylo) && (py <= yhi) &&
-			   ( ((px >= xlo) && (px <= xhi)) ||
-				 (wrapra && (px >= xlo2) && (px <= xhi2)) ) ))
-			continue;
+		qidxfile_get_quads(qidx, res->inds[j], &quads, &nquads);
 
-		ix = (int)rint((px - xorigin) * xscale);
-		if (ix + pixelmargin < 0 ||
-			ix - pixelmargin >= w)
-			continue;
-		iy = h - (int)rint((py - yorigin) * yscale);
-		if (iy + pixelmargin < 0 ||
-			iy - pixelmargin >= h)
-			continue;
-		addstar(img, ix, iy, w, h, 0x90, 0xa8, 255);
-		Nib++;
+		for (k=0; k<nquads; k++) {
+			uint stars[4];
+			double xyzs[12];
+			double midab[3];
+			double theta[4];
+			int perm[4];
+			quadfile_get_starids(qfile, quads[k], stars+0, stars+1, stars+2, stars+3);
+			for (s=0; s<4; s++)
+				startree_get(skdt, stars[s], xyzs + 3*s);
+
+			star_midpoint(midab, xyzs + 3*0, xyzs + 3*1);
+			// sort the points by angle from the midpoint so that the
+			// polygon doesn't self-intersect.
+			for (s=0; s<4; s++) {
+				double x, y;
+				star_coords(xyzs + 3*s, midab, &x, &y);
+				theta[s] = atan2(y, x);
+				perm[s] = s;
+			}
+			permuted_sort_set_params(theta, sizeof(double), compare_doubles);
+			permuted_sort(perm, 4);
+
+			printf("  <quad");
+			for (i=0; i<4; i++) {
+				double ra, dec;
+				xyzarr2radec(xyzs + 3*perm[i], &ra, &dec);
+				ra  = rad2deg(ra);
+				dec = rad2deg(dec);
+				printf(" ra%i=\"%g\" dec%i=\"%g\"", i, ra, i, dec);
+			}
+			printf("/>\n");
+		}
 	}
+	printf("</quads>\n");
 
-	kdtree_free_query(res);
 	startree_close(skdt);
-
-	fprintf(stderr, "%i stars inside image bounds.\n", Nib);
-
-	printf("P6 %d %d %d\n", w, h, 255);
-	fwrite(img, 1, 3*w*h, stdout);
-	free(img);
+	qidxfile_close(qidx);
+	quadfile_close(qfile);
 	return 0;
 }
+
