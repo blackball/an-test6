4f4bc67a2ce3e70a3cac29f7ac4b43260895d8e0
Dustin Lang <dstndstn@gmail.com>
2007-02-05 19:10:21 +0000

    use threading and queue (ctrl-C doesn't work though)

--- src/quads/blind-watcher.py
+++ src/quads/blind-watcher.py
@@ -1,38 +1,58 @@
+#! /usr/bin/env python
+
 from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
-import os
+import os, signal
+from Queue import Queue
+
+def sigint(signum, frame):
+    print 'SIGINT received.'
+    eq.quit();
+
+class Enqueuer(ProcessEvent):
+    def __init__(self):
+        self.q = Queue(0)
+
+    def quit(self):
+        self.q.put('QUIT');
+
+    def get(self):
+        try:
+            return self.q.get(True)
+        except KeyboardInterrupt:
+            return 'QUIT'
+        
+    def handle(self, path):
+        self.q.put(path)
+        print "Queue has %i entries." % self.q.qsize()
+
+    def process_IN_CLOSE_WRITE(self, event):
+        print "Closed write: %s" %  os.path.join(event.path, event.name)
+        self.handle(os.path.join(event.path, event.name));
+
+    def process_IN_MOVED_TO(self, event):
+        print "Moved in: %s" %  os.path.join(event.path, event.name)
+        self.handle(os.path.join(event.path, event.name));
+
+
 
 wm = WatchManager()
-
-# IN_CLOSE_WRITE
-# IN_MOVED_TO
-# IN_UNMOUNT
-mask = EventsCodes.IN_DELETE | EventsCodes.IN_CREATE  # watched events
-
-class PTmp(ProcessEvent):
-    def process_IN_CREATE(self, event):
-        print "Create: %s" %  os.path.join(event.path, event.name)
-
-    def process_IN_DELETE(self, event):
-        print "Remove: %s" %  os.path.join(event.path, event.name)
-
-
-notifier = Notifier(wm, PTmp())
-#wdd = wm.add_watch('/tmp', mask, rec=True)
+eq = Enqueuer()
+signal.signal(signal.SIGINT, sigint)
+notifier = ThreadedNotifier(wm, eq)
+notifier.start()
+mask = EventsCodes.IN_CLOSE_WRITE | EventsCodes.IN_MOVED_TO
 wdd = wm.add_watch('/tmp', mask)
 
-while True:  # loop forever
+while True:
     try:
-        # process the queue of events as explained above
-        notifier.process_events()
-        if notifier.check_events():
-            # read notified events and enqeue them
-            notifier.read_events()
-        # you can do some tasks here...
+        filename = eq.get()
+        print "Dequeued: %s" % filename;
+        if (filename == 'QUIT'):
+            break
     except KeyboardInterrupt:
         break
 
-
-#wm.rm_watch(wdd['/tmp'])
+# un-watch
 wm.rm_watch(wdd.values())
 # destroy the inotify's instance on this interrupt (stop monitoring)
 notifier.stop()
