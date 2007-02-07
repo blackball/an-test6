6bef9d940f214f0f3eca3442bbd62f947ded625e
Dustin Lang <dstndstn@gmail.com>
2007-02-07 21:42:21 +0000

    mv

---
+++ src/ontheweb/execs/blind-watcher.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
+import os, signal
+from Queue import Queue
+from subprocess import Popen
+
+BLIND = 'blind';
+IN_SUFFIX  = '.in';
+OUT_SUFFIX = '.out';
+
+def sigint(signum, frame):
+    print 'SIGINT received.'
+    eq.quit()
+
+class Enqueuer(ProcessEvent):
+    def __init__(self):
+        self.q = Queue(0)
+
+    def quit(self):
+        self.q.put('QUIT')
+
+    def get(self):
+        try:
+            return self.q.get(True)
+        except KeyboardInterrupt:
+            return 'QUIT'
+        
+    def handle(self, path):
+        if (not(path.endswith(IN_SUFFIX))):
+            return
+        self.q.put(path)
+        print "Queue has %i entries." % self.q.qsize()
+
+    def process_IN_CLOSE_WRITE(self, event):
+        print "Closed write: %s" %  os.path.join(event.path, event.name)
+        self.handle(os.path.join(event.path, event.name));
+
+    def process_IN_MOVED_TO(self, event):
+        print "Moved in: %s" %  os.path.join(event.path, event.name)
+        self.handle(os.path.join(event.path, event.name));
+
+
+
+wm = WatchManager()
+eq = Enqueuer()
+signal.signal(signal.SIGINT, sigint)
+notifier = ThreadedNotifier(wm, eq)
+notifier.start()
+mask = EventsCodes.IN_CLOSE_WRITE | EventsCodes.IN_MOVED_TO
+wdd = wm.add_watch('/tmp', mask)
+
+while True:
+    try:
+        filename = eq.get()
+        print "Dequeued: %s" % filename;
+        if (filename == 'QUIT'):
+            break
+
+        # run blind with the new input file.
+        outfile = filename[0:len(filename) - len(IN_SUFFIX)] + '.out'
+        print 'Output file: %s' % outfile
+        rtnval = os.system(BLIND + ' <' + filename + ' >' + outfile + ' 2>&1')
+        if (rtnval):
+            print 'Blind failed.'
+        else:
+            print 'Blind succeeded.'
+    except KeyboardInterrupt:
+        break
+
+# un-watch
+wm.rm_watch(wdd.values())
+# destroy the inotify's instance on this interrupt (stop monitoring)
+notifier.stop()
+
