18dfb5d9c3390d72229618e8904d0eaf332c36e3
Dustin Lang <dstndstn@gmail.com>
2007-02-07 03:32:22 +0000

    more input-juggling.

--- src/ontheweb/web/index.php
+++ src/ontheweb/web/index.php
@@ -27,23 +27,41 @@ echo '<hr><pre>';
 print_r($_FILES);
 echo '</pre><hr>';
 
-//$ok_fitsfile = in_array("fitsfile", $headers) && in_array("fitsfile", $_FILES);
-$ok_fitsfile = array_key_exists("fitsfile", $_FILES);
-echo "ok_fitsfile = " . $ok_fitsfile;
-if ($ok_fitsfile) {
-	$fitsfile = $_FILES["fitsfile"];
-	/*
-	echo "<pre>fitsfile=";
-	print_r($fitsfile);
-	echo "</pre>";
-	*/
-	$ok_fitsfile = $ok_fitsfile and ($fitsfile["error"] == 0);
-	echo "ok_fitsfile = " . $ok_fitsfile;
+$got_fitsfile = array_key_exists("fitsfile", $_FILES);
+$got_fitstmpfile = array_key_exists("fits_tmpfile", $headers);
+if ($got_fitstmpfile) {
+	$fitsfilename = $headers["fits_filename"];
+	$fitstempfilename = $headers["fits_tmpfile"];
 }
+if ($got_fitsfile) {
+	$fitsfile = $_FILES["fitsfile"];
+	$got_fitsfile = $got_fitsfile and ($fitsfile["error"] == 0);
+	if ($got_fitsfile) {
+		$fitsfilename = $fitsfile["name"];
+		$fitstempfilename = $fitsfile['tmp_name'];
+	}
+	//echo "got_fitsfile = " . $got_fitsfile;
+}
+$ok_fitsfile = $got_fitsfile or $got_fitstmpfile;
+echo "ok_fitsfile = " . $ok_fitsfile;
+echo "fitsfile = " . $fitsfilename;
+
 $ok_x_col = array_key_exists("x_col", $headers);
 $ok_y_col = array_key_exists("y_col", $headers);
 $ok_fu_lower = array_key_exists("fu_lower", $headers);
+if ($ok_fu_lower) {
+	$fu_lower = (int)$headers["fu_lower"];
+	if ($fu_lower <= 0) {
+		$ok_fu_lower = 0;
+	}
+}
 $ok_fu_upper = array_key_exists("fu_upper", $headers);
+if ($ok_fu_upper) {
+	$fu_upper = (int)$headers["fu_upper"];
+	if ($fu_upper <= 0) {
+		$ok_fu_upper = 0;
+	}
+}
 
 /*
 echo "HTTP Request headers:\n";
@@ -69,44 +87,79 @@ Astrometry.net: Web Edition
 
 <?php
 $fitsfile_ok = $ok_fitsfile and $ok_x_col and $ok_y_col;
-if ($fitsfile_ok) {
-} else {
-	echo "<font color=red>\n";
+if (!$fitsfile_ok) {
+	echo "<font color=red>";
 }
 echo "FITS file containing a table of detected objects, sorted by ";
 echo "brightest objects first:\n<br>\n<ul>\n";
 
-/*
-printf('<li>File: <input type="file"   name="fitsfile" size=30 %s>',
-	   ($fitsfile_ok ? 'value="' . $fitsfile['name'] . '"' : 'class=redinput'));
-*/
 echo '<li>File: ';
 if ($fitsfile_ok) {
-	echo 'using <input type="input" readonly value="' . $fitsfile['name'] . '">';
-	echo '<input type="hidden" name="fits_tmpfile" value="' . $fitsfile['tmp_name'] . '">';
+	echo 'using <input type="input" name="fits_filename" readonly value="' . $fitsfilename . '">';
+	echo '<input type="hidden" name="fits_tmpfile" value="' . $fitstempfilename . '">';
 	echo ' or replace:';
 } else {
 }
 echo '<input type="file" name="fitsfile" size=30>';
+echo "\n";
 
-if ($fitsfile_ok) {
-} else {
-	echo "</font>\n";
+if (!$fitsfile_ok) {
+	echo "</font>";
 }
+
+echo "\n";
+
+printf('<li>X column name: <input type="text" name="x_col" value="%s" size=10%s>',
+	   ($ok_x_col ? $headers["x_col"] : "X"),
+	   ($ok_x_col ? "" : " class=redinput"));
+
+echo "\n";
+
+printf('<li>Y column name: <input type="text" name="y_col" value="%s" size=10%s>',
+	   ($ok_y_col ? $headers["y_col"] : "Y"),
+	   ($ok_y_col ? "" : " class=redinput"));
+
+echo "\n";
+
+echo '</ul><br>';
+
+echo "\n";
+
+$bounds_ok = $ok_fu_lower and $ok_fu_upper;
+if (!$bounds_ok) {
+	 echo "<font color=red>";
+}
+echo 'Bounds on the pixel scale of the image, in arcseconds per pixel:';
+if (!$bounds_ok) {
+	 echo "</font>";
+}
+echo "\n";
+echo "<ul>";
+echo "\n";
+
+printf('<li>Lower bound: <input type="text" name="fu_lower" size=10 %s>',
+	   ($ok_fu_lower ?
+		'value="' . $headers["fu_lower"] . '"' :
+		"class=redinput"));
+echo "\n";
+printf('<li>Upper bound: <input type="text" name="fu_upper" size=10 %s>',
+	   ($ok_fu_upper ?
+		'value="' . $headers["fu_upper"] . '"' :
+		"class=redinput"));
+echo "\n";
+echo "</ul><br>";
+echo "\n";
+
 ?>
 
-<!-- <li>File: <input type="file"   name="file" size=30 class="redinput"> -->
-<li>X column name: <input type="text" name="x_col" value="X" size=10 color=pink>
-<li>Y column name: <input type="text" name="y_col" value="Y" size=10>
-<!-- li --><!-- input type="radio" name="parity" value="1" -->
-</ul>
-<br>
-Bounds on the pixel scale of the image, in arcseconds per pixel:
+<!--
 <ul>
 <li>Lower bound: <input type="text" name="fu_lower" size=10>
 <li>Upper bound: <input type="text" name="fu_upper" size=10>
 </ul>
 <br>
+-->
+
 Index to use:
 <select name="index">
 <option value="marshall-30">Wide-Field Digital Camera</option>
