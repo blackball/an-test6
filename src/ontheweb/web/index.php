b862c9a8ad7f67905394e63a3232e5fb0fbb339f
Dustin Lang <dstndstn@gmail.com>
2007-02-07 14:47:14 +0000

    on-the-web: check more values

--- src/ontheweb/web/index.php
+++ src/ontheweb/web/index.php
@@ -6,9 +6,7 @@ Astrometry.net: Web Edition
 <style type="text/css">
 <!-- 
 input.redinput {
-color:black;
 background-color: pink;
-border:none;
 }
 -->
 </style>
@@ -43,8 +41,9 @@ if ($got_fitsfile) {
 	//echo "got_fitsfile = " . $got_fitsfile;
 }
 $ok_fitsfile = $got_fitsfile or $got_fitstmpfile;
-echo "ok_fitsfile = " . $ok_fitsfile;
-echo "fitsfile = " . $fitsfilename;
+echo "ok_fitsfile = " . $ok_fitsfile . "<br>";
+echo "fitsfilename = " . $fitsfilename . "<br>";
+echo "fitstempfilename = " . $fitstempfilename . "<br>";
 
 $ok_x_col = array_key_exists("x_col", $headers);
 $ok_y_col = array_key_exists("y_col", $headers);
@@ -62,6 +61,21 @@ if ($ok_fu_upper) {
 		$ok_fu_upper = 0;
 	}
 }
+$ok_verify = array_key_exists("verify", $headers);
+if ($ok_verify) {
+	$verify = (int)$headers["verify"];
+	if ($verify <= 0) {
+		$ok_verify = 0;
+	}
+}
+$ok_agree = array_key_exists("agree", $headers);
+if ($ok_agree) {
+	$agree = (int)$headers["agree"];
+	if ($agree <= 0) {
+		$ok_agree = 0;
+	}
+}
+
 
 /*
 echo "HTTP Request headers:\n";
@@ -90,75 +104,79 @@ $fitsfile_ok = $ok_fitsfile and $ok_x_col and $ok_y_col;
 if (!$fitsfile_ok) {
 	echo "<font color=red>";
 }
-echo "FITS file containing a table of detected objects, sorted by ";
-echo "brightest objects first:\n<br>\n<ul>\n";
+?>
 
-echo '<li>File: ';
+FITS file containing a table of detected objects, sorted by
+brightest objects first
+<?php
+if (!$fitsfile_ok) {
+	echo "</font>";
+}
+?>
+
+<br>
+<ul>
+<li>File: 
+<?php
 if ($fitsfile_ok) {
 	echo 'using <input type="input" name="fits_filename" readonly value="' . $fitsfilename . '">';
 	echo '<input type="hidden" name="fits_tmpfile" value="' . $fitstempfilename . '">';
 	echo ' or replace:';
-} else {
 }
-echo '<input type="file" name="fitsfile" size=30>';
-echo "\n";
-
+echo '<input type="file" name="fitsfile" size="30"';
 if (!$fitsfile_ok) {
-	echo "</font>";
+    echo ' class="redinput"';
 }
-
+echo ">\n";
 echo "\n";
+?>
 
-printf('<li>X column name: <input type="text" name="x_col" value="%s" size=10%s>',
+<?php
+printf('<li>X column name: <input type="text" name="x_col" value="%s" size="10"%s>',
 	   ($ok_x_col ? $headers["x_col"] : "X"),
-	   ($ok_x_col ? "" : " class=redinput"));
-
+	   ($ok_x_col ? "" : ' class="redinput"'));
 echo "\n";
-
-printf('<li>Y column name: <input type="text" name="y_col" value="%s" size=10%s>',
+printf('<li>Y column name: <input type="text" name="y_col" value="%s" size="10"%s>',
 	   ($ok_y_col ? $headers["y_col"] : "Y"),
-	   ($ok_y_col ? "" : " class=redinput"));
+	   ($ok_y_col ? "" : ' class="redinput"'));
+?>
 
-echo "\n";
+</ul><br>
 
-echo '</ul><br>';
-
-echo "\n";
-
+<?php
 $bounds_ok = $ok_fu_lower and $ok_fu_upper;
 if (!$bounds_ok) {
-	 echo "<font color=red>";
+	 echo '<font color="red">';
 }
-echo 'Bounds on the pixel scale of the image, in arcseconds per pixel:';
+?>
+'Bounds on the pixel scale of the image, in arcseconds per pixel:';
+<?php
 if (!$bounds_ok) {
 	 echo "</font>";
 }
-echo "\n";
-echo "<ul>";
-echo "\n";
-
-printf('<li>Lower bound: <input type="text" name="fu_lower" size=10 %s>',
-	   ($ok_fu_lower ?
-		'value="' . $headers["fu_lower"] . '"' :
-		"class=redinput"));
-echo "\n";
-printf('<li>Upper bound: <input type="text" name="fu_upper" size=10 %s>',
-	   ($ok_fu_upper ?
-		'value="' . $headers["fu_upper"] . '"' :
-		"class=redinput"));
-echo "\n";
-echo "</ul><br>";
-echo "\n";
-
 ?>
 
-<!--
 <ul>
-<li>Lower bound: <input type="text" name="fu_lower" size=10>
-<li>Upper bound: <input type="text" name="fu_upper" size=10>
+<li>Lower bound: <input type="text" name="fu_lower" size="10"
+<?php
+if ($ok_fu_lower) {
+    echo 'value="' . $headers["fu_lower"] . '"';
+} else {
+    echo 'class="redinput"';
+}
+?>
+>
+<li>Upper bound: <input type="text" name="fu_upper" size="10"
+<?php
+if ($ok_fu_upper) {
+    echo 'value="' . $headers["fu_upper"] . '"';
+} else {
+    echo 'class="redinput"';
+}
+?>
+>
 </ul>
 <br>
--->
 
 Index to use:
 <select name="index">
@@ -167,8 +185,30 @@ Index to use:
 </select>
 <br>
 <br>
+
+<?php
+if (!$ok_verify) {
+    echo '<font color="red">';
+}
+?>
+
 Star positional error (verification tolerance), in arcseconds:
-<input type="text" name="verify" size=10>
+
+<?php
+if (!$ok_verify) {
+    echo "</font>";
+}
+?>
+
+<input type="text" name="verify" size="10"
+<?php
+if ($ok_verify) {
+    echo 'value="' . $verify . '"';
+} else {
+    echo 'class="redinput"'; 
+}
+?>
+>
 <br>
 <br>
 Code tolerance:
@@ -186,8 +226,30 @@ Number of agreeing matches required:
 </select>
 <br>
 <br>
+
+<?php
+if (!$ok_verify) {
+    echo '<font color="red">';
+}
+?>
+
 Agreement radius (in arcseconds):
-<input type="text" name="agreedist" size=10>
+
+<?php
+if (!$ok_verify) {
+    echo "</font>";
+}
+?>
+
+<input type="text" name="agreedist" size="10"
+<?php
+if ($ok_agree) {
+    echo 'value="' . $agree . '"';
+} else {
+    echo 'class="redinput"'; 
+}
+?>
+>
 <br>
 <br>
 
