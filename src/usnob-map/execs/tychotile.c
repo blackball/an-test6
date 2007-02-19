9dd32d2785346442824cda1fbfc48be7ce64bbfe
Dustin Lang <dstndstn@gmail.com>
2007-02-19 22:42:36 +0000

    add tychotile renderer

--- src/usnob-map/execs/tychotile.c
+++ src/usnob-map/execs/tychotile.c
@@ -29,7 +29,7 @@
 #include "healpix.h"
 #include "mathutil.h"
 #include "merctree.h"
-#include "bl.h"
+#include "keywords.h"
 
 #define max(a, b)  ((a)>(b)?(a):(b))
 #define min(a, b)  ((a)<(b)?(a):(b))
@@ -44,9 +44,71 @@ static void node_overlaps(kdtree_t* kd, int node, void* extra);
 static void leaf_node(kdtree_t* kd, int node);
 static void expand_node(kdtree_t* kd, int node);
 static void addstar(double xp, double yp, merc_flux* flux);
-					 
+
+/*
+static void check_merctree(merctree* m, int node) {
+	merc_flux* stats = &(m->stats[node].flux);
+	float r, b, n;
+	float drel;
+	r = b = n = 0.0;
+	if (KD_IS_LEAF(m->tree, node)) {
+		int j, L, R;
+		L = kdtree_left(m->tree, node);
+		R = kdtree_right(m->tree, node);
+		for (j=L; j<=R; j++) {
+			r += m->flux[j].rflux;
+			b += m->flux[j].bflux;
+			n += m->flux[j].nflux;
+		}
+
+		drel = fabs(r - stats->rflux) / (r + stats->rflux);
+		if (drel > 1e-6)
+			fprintf(stderr, "Node %i: r %-15g %-15g\n", node, r, stats->rflux);
+
+		drel = fabs(b - stats->bflux) / (b + stats->bflux);
+		if (drel > 1e-6)
+			fprintf(stderr, "Node %i: b %-15g %-15g\n", node, b, stats->bflux);
+
+		drel = fabs(n - stats->nflux) / (n + stats->nflux);
+		if (drel > 1e-6)
+			fprintf(stderr, "Node %i: n %-15g %-15g\n", node, n, stats->nflux);
+
+	} else {
+		merc_flux* stats1 = &(m->stats[ KD_CHILD_LEFT(node)].flux);
+		merc_flux* stats2 = &(m->stats[KD_CHILD_RIGHT(node)].flux);
+
+		check_merctree(m, KD_CHILD_LEFT(node));
+		check_merctree(m, KD_CHILD_RIGHT(node));
+
+		r = stats1->rflux + stats2->rflux;
+		b = stats1->bflux + stats2->bflux;
+		n = stats1->nflux + stats2->nflux;
+
+		drel = fabs(r - stats->rflux) / (r + stats->rflux);
+		if (drel > 1e-6) {
+			fprintf(stderr, "PNode %i: r %-15g %-15g\n", node, r, stats->rflux);
+			fprintf(stderr, "   Node %i: r %-15g\n", KD_CHILD_LEFT(node), stats1->rflux);
+			fprintf(stderr, "   Node %i: r %-15g\n", KD_CHILD_RIGHT(node), stats2->rflux);
+		}
+
+		drel = fabs(b - stats->bflux) / (b + stats->bflux);
+		if (drel > 1e-6)
+			fprintf(stderr, "PNode %i: b %-15g %-15g\n", node, b, stats->bflux);
+
+		drel = fabs(n - stats->nflux) / (n + stats->nflux);
+		if (drel > 1e-6)
+			fprintf(stderr, "PNode %i: n %-15g %-15g\n", node, n, stats->nflux);
+		
+	}
+}
+*/
+
+/*
+  char* map_template  = "/home/gmaps/usnob-images/tycho-maps/tycho-zoom%i_%02i_%02i.png";
+  char* merc_file     = "/home/gmaps/usnob-images/tycho-merc/tycho.mkdt.fits";
+*/
 char* map_template  = "/home/gmaps/usnob-images/tycho-maps/tycho-zoom%i_%02i_%02i.png";
-char* merc_file     = "/home/gmaps/usnob-images/tycho-merc/tycho.mkdt.fits";
+char* merc_file     = "/h/260/dstn/local/tycho-maps/tycho.mkdt.fits";
 
 float* fluximg;
 float xscale, yscale;
@@ -58,9 +120,32 @@ int w=0, h=0;
 double *qlo, *qhi;
 // The lower-left corner of the image.
 double xorigin, yorigin;
+// The upper-right
+double xlimit, ylimit;
 // The merctree.
 merctree* merc;
 
+/*
+static void write_merctree_node(merctree* m, int L, float* img,
+								int node, int level) {
+	kdtree_t* kd = m->tree;
+	int x0, x1, y0, y1;
+	if (level > L)
+		return;
+	if (level < L) {
+		write_merctree_node(m, L, img, KDTREE_CHILD_LEFT(kd, node), level+1);
+		write_merctree_node(m, L, img, KDTREE_CHILD_RIGHT(kd, node), level+1);
+		return;
+	}
+
+	if ( = (int)((xp - xorigin) * xscale);
+
+}
+
+static void write_merctree(merctree* m, int level, float* img) {
+	write_merctree_node(m, level, img, 0, 0);
+}
+*/
 int main(int argc, char *argv[]) {
     int argchar;
 	bool gotx, goty, gotX, gotY, gotw, goth;
@@ -137,6 +222,14 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Error, py1 < py0 (%g < %g)\n", py1, py0);
 		exit(-1);
 	}
+	if (py0 < 0.0) {
+		fprintf(stderr, "Tweaking py0 %g -> %g.\n", py0, 0.0);
+		py0 = 0.0;
+	}
+	if (py1 > 1.0) {
+		fprintf(stderr, "Tweaking py1 %g -> %g.\n", py1, 1.0);
+		py1 = 1.0;
+	}
 
 	pixperx = (double)w / (px1 - px0);
 	pixpery = (double)h / (py1 - py0);
@@ -170,7 +263,7 @@ int main(int argc, char *argv[]) {
 
 	fprintf(stderr, "Pixel positions: (%i,%i), (%i,%i) vs (%i,%i)\n", xpix0, ypix0, xpix0+w, ypix0+h, xpix1, ypix1);
 
-	if (!forcetree && (zoomlevel <= 2)) {
+	if (!forcetree && (zoomlevel <= 0)) {
 		char buff[1024];
 
 		// set filename so that correct tile is returned
@@ -239,23 +332,67 @@ int main(int argc, char *argv[]) {
 
 
 		fprintf(stderr, "Opening file %s...\n", merc_file);
-		merc = merctree_open(fn);
+		merc = merctree_open(merc_file);
 		if (!merc) {
 			fprintf(stderr, "Failed to open merctree %s\n", merc_file);
 			exit(-1);
 		}
 
-		xorigin = qlo[0];
-		yorigin = qlo[1];
+		//check_merctree(merc, 0);
+
+		/*
+		{
+			int k;
+			for (k=0; k<merc->tree->nlevels; k++) {
+				char fn[256];
+				FILE* f;
+				float* img;
+				float mx;
+				img = calloc(w*h*3, sizeof(float));
+				write_merctree(merc, k, img);
+				sprintf(fn, "merc%i.ppm", k);
+				f = fopen(fn, "wb");
+				if (!f) {
+					fprintf(stderr, "open: %s\n", strerror(errno));
+					exit(-1);
+				}
+				fprintf("P6 %i %i %i\n", w, h, 255);
+				mx = 0.0;
+				for (i=0; i<(3*w*h); i++) {
+					img[i] = pow(img[i], 0.25);
+					mx = max(mx, img[i]);
+				}
+				for (i=0; i<(3*w*h); i++) {
+					unsigned char pix;
+					pix = min(255, 4 * 255.0 / mx * img[i]);
+					fputc(pix);
+				}
+				fclose(f);
+				free(img);
+			}
+		}
+		*/
+
 		qlo = querylow;
 		qhi = queryhigh;
+
+		xorigin = qlo[0];
+		yorigin = qlo[1];
+		xlimit  = qhi[0];
+		ylimit  = qhi[1];
+
+		fprintf(stderr, "NODES CONTAINED\n");
 		kdtree_nodes_contained(merc->tree, qlo, qhi,
 							   node_contained, node_overlaps, NULL);
 	
 		if (wrapra) {
-			xorigin = qlo[0] - 1.0;
+
+			xorigin = querylow [0] - 1.0;
+			xlimit  = queryhigh[0] - 1.0;
+
 			qlo = wraplow;
 			qhi = wraphigh;
+			fprintf(stderr, "NODES CONTAINED (wrap)\n");
 			kdtree_nodes_contained(merc->tree, qlo, qhi,
 								   node_contained, node_overlaps, NULL);
 		}
@@ -266,22 +403,38 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "%i points inside image bounds.\n", Nib);
 
 		{
-			float rmax, bmax, nmax;
+			//float rmax, bmax, nmax;
 			float rscale, bscale, nscale;
 			float sat = 4.0;
-			rmax = bmax = nmax = 0.0;
+			//rmax = bmax = nmax = 0.0;
 			for (i=0; i<(w*h); i++) {
 				fluximg[3*i+0] = pow(fluximg[3*i+0], 0.25);
 				fluximg[3*i+1] = pow(fluximg[3*i+1], 0.25);
 				fluximg[3*i+2] = pow(fluximg[3*i+2], 0.25);
-				if (fluximg[3*i + 0] > rmax) rmax = fluximg[3*i + 0];
-				if (fluximg[3*i + 1] > bmax) bmax = fluximg[3*i + 1];
-				if (fluximg[3*i + 2] > nmax) nmax = fluximg[3*i + 2];
+				/*
+				  if (fluximg[3*i + 0] > rmax) rmax = fluximg[3*i + 0];
+				  if (fluximg[3*i + 1] > bmax) bmax = fluximg[3*i + 1];
+				  if (fluximg[3*i + 2] > nmax) nmax = fluximg[3*i + 2];
+				*/
 			}
 
-			rscale = 255.0 / rmax * sat;
-			bscale = 255.0 / bmax * sat;
-			nscale = 255.0 / nmax * sat;
+			/*
+			  fprintf(stderr, "Maxes: %g, %g, %g.\n", rmax, bmax, nmax);
+			  rscale = 255.0 / rmax * sat;
+			  bscale = 255.0 / bmax * sat;
+			  nscale = 255.0 / nmax * sat;
+			  if (rmax == 0) rscale = 0;
+			  if (bmax == 0) bscale = 0;
+			  if (nmax == 0) nscale = 0;
+			*/
+
+			switch (zoomlevel) {
+			case 1:
+			default:
+				rscale = 255.0 / 1.0 * sat;
+				bscale = 255.0 / 1.0 * sat;
+				nscale = 255.0 / 1.0 * sat;
+			}
 
 			printf("P6 %d %d %d\n", w, h, 255);
 			for (i=0; i<(w*h); i++) {
@@ -302,11 +455,14 @@ int main(int argc, char *argv[]) {
 
 // Non-leaf nodes that are fully contained within the query rectangle.
 static void node_contained(kdtree_t* kd, int node, void* extra) {
+	fprintf(stderr, "Node contained: %i\n", node);
 	expand_node(kd, node);
+	//leaf_node(kd, node);
 }
 
 // Leaf nodes that overlap the query rectangle.
 static void node_overlaps(kdtree_t* kd, int node, void* extra) {
+	fprintf(stderr, "Node overlaps: %i\n", node);
 	leaf_node(kd, node);
 }
 
@@ -315,24 +471,45 @@ static void expand_node(kdtree_t* kd, int node) {
 	int D = 2;
 	double bblo[D], bbhi[D];
 
-	// check if this whole box fits inside a pixel.
-	kdtree_get_bboxes(kd, node, bblo, bbhi);
-	xp0 = (int)floor(xscale * (bblo[0] - xorigin));
-	xp1 = (int) ceil(xscale * (bbhi[0] - xorigin));
-	if (xp1 - xp0 <= 1) {
-		yp0 = (int)floor(yscale * (bblo[1] - yorigin));
-		yp1 = (int) ceil(yscale * (bbhi[1] - yorigin));
-		if (yp1 - yp0 <= 1) {
-			// This node fits inside a single pixel of the output image.
-			// use bblo as the point.
-			addstar(bblo[0], bblo[1], &(merc->stats[node].flux));
-			return;
-		}
-	}
-
 	if (KD_IS_LEAF(kd, node)) {
 		leaf_node(kd, node);
 		return;
+	}
+
+	// check if this whole box fits inside a pixel.
+	if (!kdtree_get_bboxes(kd, node, bblo, bbhi)) {
+		fprintf(stderr, "Error, tree does not have bounding boxes.\n");
+		exit(-1);
+	}
+	/*
+	  xp0 = (int)floor(xscale * (bblo[0] - xorigin));
+	  xp1 = (int) ceil(xscale * (bbhi[0] - xorigin));
+	*/
+	xp0 = (int)(xscale * (bblo[0] - xorigin));
+	xp1 = (int)(xscale * (bbhi[0] - xorigin));
+	//if (xp1 - xp0 <= 1) {
+	if (xp1 == xp0) {
+		/*
+		  yp0 = (int)floor(yscale * (bblo[1] - yorigin));
+		  yp1 = (int) ceil(yscale * (bbhi[1] - yorigin));
+		*/
+		yp0 = (int)(yscale * (bblo[1] - yorigin));
+		yp1 = (int)(yscale * (bbhi[1] - yorigin));
+		//if (yp1 - yp0 <= 1) {
+		if (yp1 == yp0) {
+			// This node fits inside a single pixel of the output image.
+			fprintf(stderr, "Whole box fits in a pixel: %i (%i pts)\n",
+					node, kdtree_npoints(kd, node));
+			//addstar(0.5*(bblo[0]+bbhi[0]), 0.5*(bblo[1]+bbhi[1]), &(merc->stats[node].flux));
+
+			//check_merctree(merc, node);
+
+			fprintf(stderr, "BB: x=[%g, %g], y=[%g, %g]\n",
+					bblo[0], bbhi[0], bblo[1], bbhi[1]);
+
+			addstar(bblo[0], bblo[1], &(merc->stats[node].flux));
+			return;
+		}
 	}
 
 	expand_node(kd,  KD_CHILD_LEFT(node));
@@ -346,6 +523,8 @@ static void leaf_node(kdtree_t* kd, int node) {
 	L = kdtree_left(kd, node);
 	R = kdtree_right(kd, node);
 
+	fprintf(stderr, "Leaf node: %i (%i pts)\n", node, R+1-L);
+
 	for (k=L; k<=R; k++) {
 		double pt[2];
 		merc_flux* flux;
@@ -358,35 +537,75 @@ static void leaf_node(kdtree_t* kd, int node) {
 
 static void addstar(double xp, double yp,
 					merc_flux* flux) {
-	int dx[] = { -1,  0,  1,  0,  0 };
-	int dy[] = {  0,  0,  0, -1,  1 };
-	float scale[] = { 1, 1, 1, 1, 1 };
+	/*
+	  int dx[] = { -1,  0,  1,  0,  0 };
+	  int dy[] = {  0,  0,  0, -1,  1 };
+	  float scale[] = { 1, 1, 1, 1, 1 };
+	  int mindx = -1;
+	  int maxdx = 1;
+	  int mindy = -1;
+	  int maxdy = 1;
+	*/
 	int i;
 	int x, y;
-	int mindx = -1;
-	int maxdx = 1;
-	int mindy = -1;
-	int maxdy = 1;
 
-	x = (int)rint((xp - xorigin) * xscale);
+	int dx[] = { 0 };
+	int dy[] = { 0 };
+	float scale[] = { 1 };
+	int mindx = 0;
+	int maxdx = 0;
+	int mindy = 0;
+	int maxdy = 0;
+
+	//x = (int)rint((xp - xorigin) * xscale);
+	// special-case...
+	if (unlikely(xp == xlimit)) {
+		x = w-1;
+	} else {
+		x = (int)((xp - xorigin) * xscale);
+	}
 	if (x+maxdx < 0 || x+mindx >= w) {
+		fprintf(stderr, "x %i (xo=%g, xp=%g)\n", x, xorigin, xp);
 		Noob++;
 		return;
 	}
 	// flip vertically
-	y = h - (int)rint((yp - qlo[1]) * yscale);
+	//y = (h-1) - (int)rint((yp - yorigin) * yscale);
+	if (unlikely(yp == ylimit)) {
+		y = 0;
+	} else {
+		y = (h-1) - (int)((yp - yorigin) * yscale);
+	}
 	if (y+maxdy < 0 || y+mindy >= h) {
+		fprintf(stderr, "y %i (yo=%g, yp=%g)\n", y, yorigin, yp);
 		Noob++;
 		return;
 	}
 	Nib++;
 
+	/*
+	  fprintf(stderr, "flux %g, %g, %g\n", flux->rflux, flux->bflux, flux->nflux);
+	  fprintf(stderr, "x %i, y %i.\n", x, y);
+	*/
+
 	for (i=0; i<sizeof(dx)/sizeof(int); i++) {
-		if ((x + dx[i] < 0) || (x + dx[i] >= w)) continue;
-		if ((y + dy[i] < 0) || (y + dy[i] >= h)) continue;
-		fluximg[3*(y*w + x) + 0] += flux->rflux * scale[i];
-		fluximg[3*(y*w + x) + 1] += flux->bflux * scale[i];
-		fluximg[3*(y*w + x) + 2] += flux->nflux * scale[i];
+		int ix, iy;
+		float r, g, b;
+		ix = x + dx[i];
+		if ((ix < 0) || (ix >= w)) continue;
+		iy = y + dy[i];
+		if ((iy < 0) || (iy >= h)) continue;
+		/*
+		  fluximg[3*(iy*w + ix) + 0] += flux->rflux * scale[i];
+		  fluximg[3*(iy*w + ix) + 1] += flux->bflux * scale[i];
+		  fluximg[3*(iy*w + ix) + 2] += flux->nflux * scale[i];
+		*/
+		r = flux->rflux;
+		b = flux->bflux;
+		g = sqrt(r * b);
+		fluximg[3*(iy*w + ix) + 0] += r * scale[i];
+		fluximg[3*(iy*w + ix) + 1] += g * scale[i];
+		fluximg[3*(iy*w + ix) + 2] += b * scale[i];
 	}
 }
 
