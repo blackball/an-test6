f4498f7369ef8a99f9dac1812503a10bf25d1733
Dustin Lang <dstndstn@gmail.com>
2007-03-12 18:06:43 +0000

    add comment to EQUINOX field

--- src/tweak/libtweak/sip_qfits.c
+++ src/tweak/libtweak/sip_qfits.c
@@ -14,7 +14,7 @@ static void wcs_hdr_common(qfits_header* hdr, tan_t* tan) {
 	*/
 	qfits_header_add(hdr, "WCSAXES", "2", NULL, NULL);
 
-	qfits_header_add(hdr, "EQUINOX", "2000.0", NULL, NULL);
+	qfits_header_add(hdr, "EQUINOX", "2000.0", "Epoch (yr) of celestial coordinates", NULL);
 	qfits_header_add(hdr, "LONPOLE", "180.0", NULL, NULL);
 	qfits_header_add(hdr, "LATPOLE", "0.0", NULL, NULL);
 
