d81fc528df5c2aa9f7b7be0d56908b80e6508ce4
gmaps alias <dstndstn@gmail.com>
2007-04-04 19:26:24 +0000

    (dstn) bashing at svn.php like a kid at a pinata

--- src/ontheweb/web/svn.php
+++ src/ontheweb/web/svn.php
@@ -1,16 +1,16 @@
 <?php
 function get_svn_rev() {
 	$rev = "$Revision$";
-	return substr($rev, strlen("$Revision: "), -(2 + strlen("$Revision: ")));
+	return substr($rev, strlen("$Revision: "), -2);
 }
 
 function get_svn_url() {
 	$url = "$URL$";
-	return substr($url, strlen("$URL: "), -(2 + strlen("$URL: ")));
+	return substr($url, strlen("$URL: "), -2);
 }
 
 function get_svn_date() {
 	$date = "$Date$";
-	return substr($date, strlen("$Date: "), -(2 + strlen("$Date: ")));
+	return substr($date, strlen("$Date: "), -2);
 }
 ?>
