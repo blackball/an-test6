5615806683c62e48b233a576fd38c4eef7cfdb2e
gmaps alias <dstndstn@gmail.com>
2007-04-04 19:23:11 +0000

    (dstn) another whack at svn.php

--- src/ontheweb/web/svn.php
+++ src/ontheweb/web/svn.php
@@ -1,16 +1,16 @@
 <?php
 function get_svn_rev() {
 	$rev = "$Revision$";
-	return substr($rev, strlen("$Revision: "), -(strlen("$Revision$")));
+	return substr($rev, strlen("$Revision: "), -(2 + strlen("$Revision: ")));
 }
 
 function get_svn_url() {
 	$url = "$URL$";
-	return substr($url, strlen("$URL: "), -(strlen("$URL$")));
+	return substr($url, strlen("$URL: "), -(2 + strlen("$URL: ")));
 }
 
 function get_svn_date() {
 	$date = "$Date$";
-	return substr($date, strlen("$Date: "), -(strlen("$Date$")));
+	return substr($date, strlen("$Date: "), -(2 + strlen("$Date: ")));
 }
 ?>
