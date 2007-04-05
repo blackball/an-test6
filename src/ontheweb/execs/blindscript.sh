9bd94a91f65643a368ee1b843b5c18e0834f1e39
gmaps alias <dstndstn@gmail.com>
2007-04-05 22:33:13 +0000

    (dstn) make local blindscript also run startscript

--- src/ontheweb/execs/blindscript.sh
+++ src/ontheweb/execs/blindscript.sh
@@ -13,8 +13,14 @@ jobid=$(basename $jobdir)
 
 cd $jobdir
 LOG=log
+
+echo "running startscript..." >> $LOG
+./startscript >> $LOG 2>&1
+echo "finished startscript." >> $LOG
+
 echo "running blind..." >> $LOG
 blind < $inputfile >> $LOG 2>&1
+echo "finished blind." >> $LOG
 
 echo "running donescript..." >> $LOG
 ./donescript >> $LOG 2>&1
