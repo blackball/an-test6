cd90e243d2a5e8f7b8984ae2d23d0ccdf0a2da13
Dustin Lang <dstndstn@gmail.com>
2007-04-23 21:46:37 +0000

    usnob pre-rendered tiles... not quite working.

--- src/usnob-map/execs/render_usnob.c
+++ src/usnob-map/execs/render_usnob.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdarg.h>
+#include <sys/mman.h>
 
 #include "tilerender.h"
 #include "starutil.h"
@@ -12,12 +13,13 @@
 #define max(a, b)  ((a)>(b)?(a):(b))
 #define min(a, b)  ((a)<(b)?(a):(b))
 
-//static char* merc_file     = "/home/gmaps/usnob-images/tycho.mkdt.fits";
-//static char* merc_file     = "/h/260/dstn/local/tycho-maps/tycho.mkdt.fits";
-
 static char* merc_template = "/h/260/dstn/raid3/usnob-merctrees/merc_%02i_%02i.mkdt.fits";
+static char* prerendered_template = "/h/260/dstn/raid3/usnob-prerendered/zoom%i/usnob_z%1$i_%02i_%02i.raw";
+
 // Gridding of Mercator space
 static int NM = 32;
+// Max pre-rendered zoom level
+static int PRERENDERED = 5;
 
 static void logmsg(char* format, ...) {
     va_list args;
@@ -40,6 +42,7 @@ int render_usnob(unsigned char* img, render_args_t* args) {
     xhi = (int)(NM * args->xmercmax);
     ylo = (int)(NM * args->ymercmin);
     yhi = (int)(NM * args->ymercmax);
+    logmsg("reading tiles x:[%i, %i], y:[%i, %i]\n", xlo, xhi, ylo, yhi);
     // clamp.
     xlo = min(NM-1, max(0, xlo));
     xhi = min(NM-1, max(0, xhi));
@@ -60,11 +63,115 @@ int render_usnob(unsigned char* img, render_args_t* args) {
 
     logmsg("reading tiles x:[%i, %i], y:[%i, %i]\n", xlo, xhi, ylo, yhi);
 
+	/*
     // HACK!
-    if (!args->makerawfloatimg && ((xhi - xlo) * (yhi - ylo) > 4)) {
-        logmsg("Too many files to read, bailing out.\n");
-        return 0;
-    }
+	  if (!args->makerawfloatimg && ((xhi - xlo) * (yhi - ylo) > 4)) {
+	  logmsg("Too many files to read, bailing out.\n");
+	  return 0;
+	  }
+	*/
+
+    amp = pow(4.0, min(5, args->zoomlevel)) * 32.0 * exp(args->gain * log(4.0));
+
+	if (args->zoomlevel <= PRERENDERED) {
+		char fn[1024];
+		int n = (args->zoomlevel << 1);
+		// assume NM = 2^(PRERENDERED+1) ?
+		int block = NM/n;
+		// HACK...
+		int WH = 256;
+		FILE* f;
+		size_t mapsize = WH * WH * 3 * sizeof(float);
+		void* map;
+		float* flux;
+		double mstep;
+
+		logmsg("using prerendered zoom level %i.\n", args->zoomlevel);
+		logmsg("using prerendered tiles x:[%i,%i], y:[%i,%i]\n",
+			   xlo/block, xhi/block, ylo/block, yhi/block);
+
+		mstep = 1 / (double)(n * WH);
+
+		for (i=xlo/block; i<=xhi/block; i++) {
+			for (j=ylo/block; j<=yhi/block; j++) {
+				double mx, my;
+				int xp, yp;
+				// merc position of the first pixel of this pre-rendered tile.
+				mx = i / (double)n;
+				my = j / (double)n;
+				logmsg("  (%i,%i): merc (%g,%g)\n", i, j, mx, my);
+				logmsg("  merc step %g (%g for %i pixels)\n", mstep, mstep*WH, WH);
+
+				//snprintf(fn, sizeof(fn), prerendered_template, args->zoomlevel, i, j);
+				snprintf(fn, sizeof(fn), prerendered_template, args->zoomlevel, j, i);
+				logmsg("  reading file %s\n", fn);
+				f = fopen(fn, "rb");
+				if (!f) {
+					logmsg("failed to read prerendered file %s\n", fn);
+					return -1;
+				}
+				map = mmap(NULL, mapsize, PROT_READ, MAP_SHARED, fileno(f), 0);
+				fclose(f);
+				if (map == MAP_FAILED) {
+					logmsg("failed to mmap file %s\n", fn);
+					return -1;
+				}
+				flux = map;
+
+				for (yp=0; yp<WH; yp++) {
+					int ix, iy;
+					//iy = ymerc2pixel(my + mstep * (WH - 1 - yp), args);
+					iy = ymerc2pixel(my + mstep * yp, args);
+					for (xp=0; xp<WH; xp++) {
+						float r,g,b, R,G,B,I, maxRGB, f;
+						unsigned char* pix;
+
+						ix = xmerc2pixel(mx + mstep * xp, args);
+
+						if (!in_image(ix, iy, args))
+							continue;
+
+						/*
+						  r = flux[((WH-1-yp) * WH + xp) *3 + 0];
+						  g = flux[((WH-1-yp) * WH + xp) *3 + 1];
+						  b = flux[((WH-1-yp) * WH + xp) *3 + 2];
+						*/
+						r = flux[(yp * WH + xp) *3 + 0];
+						g = flux[(yp * WH + xp) *3 + 1];
+						b = flux[(yp * WH + xp) *3 + 2];
+
+						I = (r + g + b) / 3;
+						if (I == 0.0) {
+							R = G = B = 0.0;
+						} else {
+							if (args->arc) {
+								f = asinh(I * amp);
+							} else {
+								f = pow(I * amp, 0.25);
+							}
+							R = f*r/I;
+							G = f*g/I;
+							B = f*b/I;
+							maxRGB = max(R, max(G, B));
+							if (maxRGB > 1.0) {
+								R /= maxRGB;
+								G /= maxRGB;
+								B /= maxRGB;
+							}
+						}
+						pix = pixel(ix, iy, img, args);
+						pix[0] = min(255, 255.0*R);
+						pix[1] = min(255, 255.0*G);
+						pix[2] = min(255, 255.0*B);
+						pix[3] = 255;
+					}
+				}
+
+				munmap(map, mapsize);
+			}
+		}
+		return 0;
+	}
 
     fluximg = calloc(args->W * args->H * 3, sizeof(float));
     if (!fluximg) {
@@ -106,8 +213,6 @@ int render_usnob(unsigned char* img, render_args_t* args) {
         }
         return 0;
     }
-
-    amp = pow(4.0, min(5, args->zoomlevel)) * 32.0 * exp(args->gain * log(4.0));
 
     for (j=0; j<args->H; j++) {
         for (i=0; i<args->W; i++) {
