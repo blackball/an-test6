db2726d846f5aacb0c1df2cad659b3140d157176
Dustin Lang <dstndstn@gmail.com>
2007-05-04 01:32:04 +0000

    new verify

--- src/quads/verify.c
+++ src/quads/verify.c
@@ -32,11 +32,13 @@
 #include "blind_wcs.h"
 
 // FOR TESTING
+/*
 static void write_prob_terrain(kdtree_t* itree, int NF, int NI,
 							   double fieldarea, double logprob_background,
 							   double logprob_distractor, double* qc,
 							   double verify_pix2, double rquad2,
 							   double* field);
+*/
 
 #define DEBUGVERIFY 0
 #if DEBUGVERIFY
@@ -55,17 +57,15 @@ void verify_hit(startree* skdt,
 				double fieldH,
 				double logratio_tobail,
 				int min_nfield) {
-	int i, j;
+	int i;
 	double fieldcenter[3];
 	double fieldr2;
 	kdtree_qres_t* res;
 	// number of stars in the index that are within the bounds of the field.
 	int NI;
-	kdtree_t* itree;
+	kdtree_t* ftree;
 	int Nleaf = 5;
 	double* indexpix;
-
-	intmap* map;
 
 	// quad center and radius
 	double qc[2];
@@ -75,24 +75,29 @@ void verify_hit(startree* skdt,
 	double logprob_background;
 
 	double logodds = 0.0;
-	int nmatch, nnomatch, nconflict;
+	int nmatch, nnomatch;
 
 	double bestlogodds;
-	int bestnmatch, bestnnomatch, bestnconflict;
+	int bestnmatch, bestnnomatch;
 
 	// FIXME - the value 1.5 is simulation-based.
-	double gamma2 = square(1.5);
+	//double gamma2 = square(1.5);
 
 	int Nmin;
 
 	double crvalxyz[3];
 	kdtree_t* startree = skdt->tree;
+	unsigned char* sweeps = NULL;
+	int s, maxsweep;
+
+	double* fieldcopy;
+	int K;
 
 	assert(mo->wcs_valid);
 	assert(startree);
+	assert(skdt->sweep);
 
-	// find all the index stars that are inside the circle that bounds
-	// the field.
+	// find the center and radius of the field in xyz space.
 	star_midpoint(fieldcenter, mo->sMin, mo->sMax);
 	fieldr2 = distsq(fieldcenter, mo->sMin, 3);
 
@@ -103,6 +108,8 @@ void verify_hit(startree* skdt,
 		debug("%g, %g\n", fieldr, fieldarcsec);
 	}
 
+	// find all the index stars that are inside the circle that bounds
+	// the field.
 	// 1.01 is a little safety factor.
 	res = kdtree_rangesearch_options(startree, fieldcenter, fieldr2 * 1.01,
 									 KD_OPTIONS_SMALL_RADIUS);
@@ -125,8 +132,10 @@ void verify_hit(startree* skdt,
 		}
 		debug(" -> good\n");
 
+		// Here we compact the "res" array(s) so that when we're done,
+		// the NI indices that are inside the field are in the bottom
+		// NI elements of the res->inds array.
 		res->inds[NI] = res->inds[i];
-
 		if (DEBUGVERIFY)
 			memmove(res->results.d + NI*3,
 					res->results.d + i*3,
@@ -151,7 +160,6 @@ void verify_hit(startree* skdt,
 		debug("Range of index objs: x:[%g,%g], y:[%g,%g]\n",
 			  minx, maxx, miny, maxy);
 	}
-
 	debug("Number of field stars: %i\n", NF);
 	debug("Number of index stars: %i\n", NI);
 
@@ -169,205 +177,147 @@ void verify_hit(startree* skdt,
 		return;
 	}
 
-	// Build a tree out of the index stars in pixel space...
-	itree = kdtree_build(NULL, indexpix, NI, 2, Nleaf, KDTT_DOUBLE, KD_BUILD_BBOX);
-
-	// Find the center of the quad in pixel space. 
-	// FIXME why not take the average of AB? then the radius will be right?
-	// -the WCS tan projection is computed wrt the center of mass, so I
-	//  thought I would use that here as well... it seemed like a good idea
-	//  at the time, but maybe you're right :)
-	qc[0] = qc[1] = 0.0;
-	for (i=0; i<4; i++) {
-		qc[0] += field[2*mo->field[i]];
-		qc[1] += field[2*mo->field[i]+1];
+	sweeps = malloc(NI * sizeof(unsigned char));
+	maxsweep = 0;
+	for (i=0; i<NI; i++) {
+		sweeps[i] = skdt->sweep[res->inds[i]];
+		maxsweep = max(maxsweep, sweeps[i]);
 	}
-	qc[0] *= 0.25;
-	qc[1] *= 0.25;
 
-	// Find the radius-squared of the quad ~= distsq(qc, fA)
+	// K: number of field objects to use.
+	//K = min(NF, 2*NI);
+	K = NF;
+
+	// Make a copy of the field objects, because we're going to build a
+	// kdtree out of them and that shuffles their order.
+	fieldcopy = malloc(K * 2 * sizeof(double));
+	memcpy(fieldcopy, field, K * 2 * sizeof(double));
+
+	// Build a tree out of the field objects (in pixel space)
+	ftree = kdtree_build(NULL, fieldcopy, K, 2, Nleaf, KDTT_DOUBLE, KD_BUILD_BBOX);
+
+	// Find the midpoint of AB of the quad in pixel space.
+	qc[0] = 0.5 * (field[2*mo->field[0]  ] + field[2*mo->field[1]  ]);
+	qc[1] = 0.5 * (field[2*mo->field[0]+1] + field[2*mo->field[1]+1]);
+	// Find the radius-squared of the quad = distsq(qc, A)
 	rquad2 = distsq(field + 2*mo->field[0], qc, 2);
 
 	// p(background) = 1/(W*H) of the image.
 	logprob_background = -log(fieldW * fieldH);
 
-	logprob_distractor = logprob_background + log(distractors);
+	// p(distractor) = D / (W*H)
+	logprob_distractor = log(distractors / (fieldW * fieldH));
 
 	debug("log(p(background)) = %g\n", logprob_background);
 	debug("log(p(distractor)) = %g\n", logprob_distractor);
 
-	if (0) {
-		write_prob_terrain(itree, NF, NI, fieldW*fieldH, logprob_background,
-						   logprob_distractor, qc, verify_pix2, rquad2,
-						   field);
-	}
+	/*
+	  if (0) {
+	  write_prob_terrain(itree, NF, NI, fieldW*fieldH, logprob_background,
+	  logprob_distractor, qc, verify_pix2, rquad2,
+	  field);
+	  }
+	*/
 
 	nmatch = 0;
 	nnomatch = 0;
-	nconflict = 0;
-	map = intmap_new(INTMAP_ONE_TO_ONE);
-
-	// "prime" the intmap with the matched quad so that no other field objs
-	// can claim them.  (This is to prevent "lucky donuts")
-	for (j=0; j<4; j++) {
-		intmap_add(map, mo->star[j], mo->field[j]);
-		debug("Priming: %i -> %i.\n", mo->star[j], mo->field[j]);
-		if (DEBUGVERIFY) {
-			int k;
-			int* invperm = malloc(NI * sizeof(int));
-			int ind = -1;
-			double xy[2];
-			kdtree_inverse_permutation(itree, invperm);
-			for (k=0; k<NI; k++) {
-				if (res->inds[k] == mo->star[j]) {
-					tan_xyzarr2pixelxy(&(mo->wcstan), res->results.d + k*3, xy, xy+1);
-					ind = invperm[k];
-					break;
-				}
-			}
-			if (ind != -1) {
-				Unused double d2 = distsq(field + mo->field[j]*2, indexpix + ind*2, 2);
-				Unused double d2b = distsq(field + mo->field[j]*2, xy, 2);
-				debug("Dist: %g, %g\n", sqrt(d2), sqrt(d2b));
-			}
-			free(invperm);
-		}
-	}
 
 	Nmin = min(NI, NF);
 
 	bestlogodds = -HUGE_VAL;
-	bestnmatch = bestnnomatch = bestnconflict = -1;
+	bestnmatch = bestnnomatch = -1;
 
-	// For each field star, find the nearest index star.
-	//for (i=0; i<NF; i++) {
-	for (i=0; i<Nmin; i++) {
-		double bestd2;
-		double sigma2;
-		double cutoffd2;
-		double R2;
-		double logprob = -HUGE_VAL;
-		int ind;
+	// Add index stars, in sweeps.
+	for (s=0; s<=maxsweep; s++) {
+		for (i=0; i<NI; i++) {
+			double bestd2;
+			double sigma2;
+			//double cutoffd2;
+			double R2;
+			double logprob = -HUGE_VAL;
+			int ind;
 
-		// Skip stars that are part of the quad:
-		if (i == mo->field[0] || i == mo->field[1] || i == mo->field[2] || i == mo->field[3])
-			continue;
+			if (sweeps[i] != s)
+				continue;
 
-		// Distance from the quad center of this field star:
-		R2 = distsq(field+i*2, qc, 2);
+			// Skip stars that are part of the quad:
+			ind = res->inds[i];
+			if (ind == mo->star[0] || ind == mo->star[1] ||
+				ind == mo->star[2] || ind == mo->star[3])
+				continue;
 
-		// Variance of a field star at that distance from the quad center:
-		sigma2 = verify_pix2 * (gamma2 + R2/rquad2);
-
-		// Cutoff distance to nearest neighbour star: p(fg) == p(bg)
-		// OK, I think that should be cutoffd2= -2*sigma2*(logbg - log(2*pi) - .5*log(sigma2))
-		// FIXME - just set it to 10 sigmas.
-		cutoffd2 = 100.0 * sigma2;
-
-		ind = kdtree_nearest_neighbour_within(itree, field+i*2, cutoffd2, &bestd2);
-
-		if (ind != -1) {
+			// Distance from the quad center of this index star:
+			// (we just use this to estimate sigma2 to estimate the cutoff
+			//  distance)
+			//R2 = distsq(field+i*2, qc, 2);
+			// Variance of a field star at that distance from the quad center:
+			//sigma2 = verify_pix2 * (gamma2 + R2/rquad2);
+			// Cutoff distance to nearest neighbour star: p(fg) == p(bg)
+			// OK, I think that should be cutoffd2= -2*sigma2*(logbg - log(2*pi) - .5*log(sigma2))
+			// FIXME - just set it to 10 sigmas.
+			//cutoffd2 = 100.0 * sigma2;
+			//ind = kdtree_nearest_neighbour_within(itree, field+i*2, cutoffd2, &bestd2);
+			//if (ind != -1) {
 			// p(foreground):
-			logprob = log((1.0 - distractors) / (2.0 * M_PI * sigma2 * NI)) - (bestd2 / (2.0 * sigma2));
-			//logprob = log((1.0 - distractors) / (2.0 * M_PI * sigma2 * Nmin)) - (bestd2 / (2.0 * sigma2));
-		}
+			//logprob = log((1.0 - distractors) / (2.0 * M_PI * sigma2 * NI)) - (bestd2 / (2.0 * sigma2));
+			//}
 
-		debug("\nField obj %i/%i: rad %g quads, sigma %g.\n", i+1, NF, sqrt(R2/rquad2), sqrt(sigma2));
-		debug("NN dist: %5.1f pix, %g sigmas\n", sqrt(bestd2), sqrt(bestd2/sigma2));
 
- 		if ((ind == -1) || (logprob < logprob_distractor)) {
-			debug("Distractor.\n");
-			logprob = logprob_distractor;
-			nnomatch++;
-		} else {
-			int resind = itree->perm[ind];
-			int starkdind = res->inds[resind];
-			debug("NN: %i -> %i\n", starkdind, i);
-			if (intmap_add(map, starkdind, i) == -1) {
-				// a field object already selected star 'ind' as its nearest neighbour.
-				double oldd2;
-				bool partofquad;
-				int oldfieldi = intmap_get(map, starkdind, -1);
-				bool update = FALSE;
-				assert(oldfieldi != -1);
+			// Find nearest field star.
+			ind = kdtree_nearest_neighbour(ftree, indexpix+i*2, &bestd2);
 
-				debug("Conflict (ind %i, resind %i, starkdind %i)\n",
-					  ind, resind, starkdind);
+			// Distance from the quad center of this field star:
+			R2 = distsq(field+ftree->perm[i]*2, qc, 2);
 
-				partofquad = (starkdind==mo->star[0] ||
-							  starkdind==mo->star[1] ||
-							  starkdind==mo->star[2] ||
-							  starkdind==mo->star[3]);
+			// Variance of a field star at that distance from the 
+			// quad center:
+			// FIXME!
+			sigma2 = verify_pix2 * (1.0 + R2/rquad2);
 
-				// Allow a poor match to be replaced by a better one!
-				// (except for conflicts with the quad!)
-				if (partofquad) {
-					debug("Conflict with matched quad.\n");
-				} else {
-					oldd2 = distsq(field + oldfieldi*2, indexpix + ind*2, 2);
-					debug("Old dist %g, New dist %g.\n", sqrt(oldd2), sqrt(bestd2));
-					if (bestd2 < oldd2) {
-						double oldR2;
-						double oldsigma2;
-						double oldlogprob;
-						oldR2 = distsq(field+oldfieldi*2, qc, 2);
-						oldsigma2 = verify_pix2 * (gamma2 + oldR2/rquad2);
-						oldlogprob = log((1.0 - distractors) / (2.0 * M_PI * oldsigma2 * NI)) - (oldd2 / (2.0 * oldsigma2));
-						//oldlogprob = log((1.0 - distractors) / (2.0 * M_PI * oldsigma2 * Nmin)) - (oldd2 / (2.0 * oldsigma2));
-						debug("Updating logprob from %g to %g.\n", oldlogprob, logprob);
-						logodds -= (oldlogprob - logprob_background);
-						intmap_update(map, starkdind, i);
-						update = TRUE;
-					}
-				}
-				nconflict++;
-				if (!update)
-					continue;
+			// don't bother computing the logprob if it's tiny...
+			//if (bestd2 > 100.0 * sigma2)
+
+			debug("\nIndex star %i (sweep %i): rad %g quads, sigma %g.\n", i, s, sqrt(R2/rquad2), sqrt(sigma2));
+			debug("NN dist: %5.1f pix, %g sigmas\n", sqrt(bestd2), sqrt(bestd2/sigma2));
+				
+			logprob = log((1.0 - distractors) / (2.0 * M_PI * sigma2 * K)) - (bestd2 / (2.0 * sigma2));
+			if (logprob < logprob_distractor) {
+				debug("Distractor.\n");
+				logprob = logprob_distractor;
+				nnomatch++;
 			} else {
-				debug("Match (index star %i), logprob %g\n", ind, logprob);
+				debug("Match (field star %i), logprob %g\n", ftree->perm[ind], logprob);
 				nmatch++;
+			}
+
+			logodds += (logprob - logprob_background);
+			debug("Logodds now %g\n", logodds);
+		
+			if (logodds < logratio_tobail) {
+				break;
 			}
 		}
 
-		logodds += (logprob - logprob_background);
-		debug("Logodds now %g\n", logodds);
-		
-		if (logodds < logratio_tobail) {
-			break;
-		}
-
-		if ((logodds > bestlogodds) &&
-			((i-nconflict) >= min_nfield)) {
+		// After each sweep, check if it's the new best logprob.
+		//if ((logodds > bestlogodds) &&
+		//(i >= min_nfield)) {
+		if (logodds > bestlogodds) {
 			bestlogodds = logodds;
 			bestnmatch = nmatch;
 			bestnnomatch = nnomatch;
-			bestnconflict = nconflict;
 		}
-
 	}
 
-	/*
-	  fprintf(stderr, "Final logodds: %g\n", logodds);
-	  fprintf(stderr, "Best logodds: %g (%i field objs), ", bestlogodds, nbest+1);
-	  fprintf(stderr, "CRVAL: %g, %g\n", mo->wcstan.crval[0], mo->wcstan.crval[1]);
-	  fprintf(stderr, "%g,%i,%g,%g; %%%%%%\n", bestlogodds, nbest+1, mo->wcstan.crval[0], mo->wcstan.crval[1]);
-	*/
-
 	kdtree_free_query(res);
-	intmap_free(map);
-	kdtree_free(itree);
+	kdtree_free(ftree);
+	free(fieldcopy);
+	free(sweeps);
 	free(indexpix);
 
 	mo->logodds = bestlogodds;
 	mo->noverlap = bestnmatch;
-	mo->nconflict = bestnconflict;
-	mo->nfield = bestnmatch + bestnconflict + bestnnomatch;
-	/*
-	  mo->logodds = logodds;
-	  mo->noverlap = nmatch;
-	  mo->nconflict = nconflict;
-	  mo->nfield = NF;
-	*/
+	mo->nconflict = 0;
+	mo->nfield = bestnmatch + bestnnomatch;
 	mo->nindex = NI;
 	matchobj_compute_overlap(mo);
 }
@@ -377,6 +327,7 @@ void verify_hit(startree* skdt,
 
 
 // TEST
+/*
 static void writeFloat(FILE* fid, float f) {
 	if (fwrite(&f, sizeof(float), 1, fid) != 1) {
 		fprintf(stderr, "failed to write float.\n");
@@ -445,4 +396,4 @@ static void write_prob_terrain(kdtree_t* itree, int NF, int NI,
 	}
 	fclose(fout);
 }
-
+*/
