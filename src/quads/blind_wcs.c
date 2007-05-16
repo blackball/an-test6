754397ac3ec38d98d21ee97ff3d743e27cd56e10
Dustin Lang <dstndstn@gmail.com>
2007-05-16 20:59:33 +0000

    blind_wcs: cleanup; fold in pcm[].

--- src/quads/blind_wcs.c
+++ src/quads/blind_wcs.c
@@ -21,6 +21,7 @@
 #include "solver_callbacks.h"
 #include "mathutil.h"
 #include "svd.h"
+#include "sip.h"
 #include "sip_qfits.h"
 
 void blind_wcs_compute_2(double* starxyz,
@@ -30,32 +31,30 @@ void blind_wcs_compute_2(double* starxyz,
 						 tan_t* tan,
 						 double* p_scale) {
 	int i, j, k;
-	double starcmass[3];
-	double fieldcmass[2];
-	double cov[4];
-	double U[4], V[4], S[2], R[4];
+	double star_cm[3] = {0, 0, 0};
+	double field_cm[2] = {0, 0};
+	double cov[4] = {0, 0, 0, 0};
+	double U[4], V[4], S[2];
+	double R[4] = {0, 0, 0, 0};
 	double scale;
 	// projected star coordinates
 	double* p;
 	// relative field coordinates
 	double* f;
-	double pcm[2];
+	double pcm[2] = {0, 0};
 
-	// -get field & star positions of the matching quad.
-	for (j=0; j<3; j++)
-		starcmass[j] = 0.0;
-	for (j=0; j<2; j++)
-		fieldcmass[j] = 0.0;
+	// -get field & star centers-of-mass of the matching quad.
+	//  (this will become the tangent point)
 	for (i=0; i<N; i++) {
-		for (j=0; j<3; j++)
-			starcmass[j] += starxyz[i*3 + j];
-		for (j=0; j<2; j++)
-			fieldcmass[j] += fieldxy[i*2 + j];
+		star_cm[0] += starxyz[i*3 + 0];
+		star_cm[1] += starxyz[i*3 + 1];
+		star_cm[2] += starxyz[i*3 + 2];
+		field_cm[0] += fieldxy[i*2 + 0];
+		field_cm[1] += fieldxy[i*2 + 1];
 	}
-	// -set the tangent point to be the center of mass of the matching stars.
-	for (j=0; j<2; j++)
-		fieldcmass[j] /= (double)N;
-	normalize_3(starcmass);
+	field_cm[0] /= (double)N;
+	field_cm[1] /= (double)N;
+	normalize_3(star_cm);
 
 	// -allocate and fill "p" and "f" arrays.
 	p = malloc(N * 2 * sizeof(double));
@@ -63,29 +62,30 @@ void blind_wcs_compute_2(double* starxyz,
 	j = 0;
 
 	for (i=0; i<N; i++) {
-		// -project the stars around the quad center
-		star_coords(starxyz + i*3, starcmass, p + 2*i, p + 2*i + 1);
-		// -grab the corresponding field coords.
-		f[2*i+0] = fieldxy[2*i+0] - fieldcmass[0];
-		f[2*i+1] = fieldxy[2*i+1] - fieldcmass[1];
+		// -project the stars around the quad center of mass
+		star_coords(starxyz + i*3, star_cm, p + 2*i, p + 2*i + 1);
+		// -grab the corresponding field coords
+		f[2*i+0] = fieldxy[2*i+0] - field_cm[0];
+		f[2*i+1] = fieldxy[2*i+1] - field_cm[1];
 	}
 
-	// -compute center of mass of the projected stars.
-	pcm[0] = pcm[1] = 0.0;
-	for (i=0; i<N; i++)
-		for (j=0; j<2; j++)
-			pcm[j] += p[2*i+j];
+	// -compute the center of mass of the projected stars and subtract it out.
+	//  This will be close to zero, but we need it to be exactly zero
+	//  before we start rigid Procrustes
+	for (i=0; i<N; i++) {
+		pcm[0] += p[2*i + 0];
+		pcm[1] += p[2*i + 1];
+	}
 	pcm[0] /= (double)N;
 	pcm[1] /= (double)N;
-	// -subtract out the centers of mass.
-	for (i=0; i<N; i++)
-		for (j=0; j<2; j++)
-			p[2*i + j] -= pcm[j];
+	//printf("pcm=(%g, %g) pixels\n", pcm[0], pcm[1]);
+	for (i=0; i<N; i++) {
+		p[2*i + 0] -= pcm[0];
+		p[2*i + 1] -= pcm[1];
+	}
 
 	// -compute the covariance between field positions and projected
 	//  positions of the corresponding stars.
-	for (i=0; i<4; i++)
-		cov[i] = 0.0;
 	for (i=0; i<N; i++)
 		for (j=0; j<2; j++)
 			for (k=0; k<2; k++)
@@ -121,19 +121,63 @@ void blind_wcs_compute_2(double* starxyz,
 	}
 
 	// -compute WCS parameters.
-	{
-		double ra, dec;
-		xyzarr2radec(starcmass, &ra, &dec);
-		tan->crval[0] = rad2deg(ra);
-		tan->crval[1] = rad2deg(dec);
-		tan->crpix[0] = fieldcmass[0];
-		tan->crpix[1] = fieldcmass[1];
-		scale = rad2deg(scale);
-		tan->cd[0][0] = R[2] * scale; // CD1_1
-		tan->cd[0][1] = R[3] * scale; // CD1_2
-		tan->cd[1][0] = R[0] * scale; // CD2_1
-		tan->cd[1][1] = R[1] * scale; // CD2_2
+	xyzarr2radecdegarr(star_cm, tan->crval);
+	tan->crpix[0] = field_cm[0] + pcm[0];
+	tan->crpix[1] = field_cm[1] + pcm[1];
+	scale = rad2deg(scale);
+	// The CD rows are reversed from R because star_coords and the Intermediate
+	// World Coordinate System consider x and y to be exchanged.
+	tan->cd[0][0] = R[2] * scale; // CD1_1
+	tan->cd[0][1] = R[3] * scale; // CD1_2
+	tan->cd[1][0] = R[0] * scale; // CD2_1
+	tan->cd[1][1] = R[1] * scale; // CD2_2
+
+	/*
+	 * Did I add pcm in the right direction?
+	 * I've never seen it make more of a difference than machine epsilon.
+	  {
+		double rmsA = 0.0;
+		double rmsB = 0.0;
+		double rmsC = 0.0;
+		tan_t tan_tst;
+		memcpy(&tan_tst, tan, sizeof(tan_t));
+
+		tan_tst.crpix[0] = field_cm[0] + pcm[0];
+		tan_tst.crpix[1] = field_cm[1] + pcm[1];
+
+		for (i=0; i<N; i++) {
+			double x, y;
+			tan_xyzarr2pixelxy(&tan_tst, starxyz + i*3, &x, &y);
+			rmsA += square(x - fieldxy[i*2 + 0]) + square(y - fieldxy[i*2 + 1]);
+		}
+		rmsA = sqrt(rmsA / (double)N);
+
+		tan_tst.crpix[0] = field_cm[0];
+		tan_tst.crpix[1] = field_cm[1];
+
+		for (i=0; i<N; i++) {
+			double x, y;
+			tan_xyzarr2pixelxy(&tan_tst, starxyz + i*3, &x, &y);
+			rmsB += square(x - fieldxy[i*2 + 0]) + square(y - fieldxy[i*2 + 1]);
+		}
+		rmsB = sqrt(rmsB / (double)N);
+
+		tan_tst.crpix[0] = field_cm[0] - pcm[0];
+		tan_tst.crpix[1] = field_cm[1] - pcm[1];
+
+		for (i=0; i<N; i++) {
+			double x, y;
+			tan_xyzarr2pixelxy(&tan_tst, starxyz + i*3, &x, &y);
+			rmsC += square(x - fieldxy[i*2 + 0]) + square(y - fieldxy[i*2 + 1]);
+		}
+		rmsC = sqrt(rmsC / (double)N);
+
+		printf("rmsA = %.16g pix.\n", rmsA);
+		printf("rmsB = %.16g pix.\n", rmsB);
+		printf("rmsC = %.16g pix.\n", rmsC);
 	}
+	*/
+
 	if (p_scale) *p_scale = scale;
 
 	free(p);
