f9d4997a58a15627f5929b5f3db5924d1643639e
Dustin Lang <dstndstn@gmail.com>
2007-06-11 13:17:53 +0000

    add script for converting A.N catalog to merctree for gmaps

---
+++ src/scripts/an2gmaps.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Convert an Astrometry.net catalog to merctrees to be used in the Google Maps interface.
+
+# input filename pattern
+IN=/nobackup2/dstn/stumm/spike_free/an_clean_hp%03i.fits
+
+# output filename pattern
+OUT=merc_%02i_%02i.mkdt.fits
+
+# executable
+MERCTREE=~/an/usnob-map/execs/make-merctree2
+
+for ((i=0; i<32; i++)); do
+        for ((j=0; j<32; j++)); do
+                n=$((i*32 + j))
+                out=`printf $OUT $i $j`
+                $MERCTREE -o $out -M 32 -N $n -i $IN -H 9 -b >> log 2>&1
+        done
+done
