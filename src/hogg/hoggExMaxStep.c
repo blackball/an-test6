1315bc4cfadd19445dac139ff581ad7ba7fc5b05
David W. Hogg <dwhogg@gmail.com>
2007-06-29 20:11:41 +0000

    EM works

--- src/hogg/hoggExMaxStep.c
+++ src/hogg/hoggExMaxStep.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <math.h>
 #include "hoggTweak.h"
 double hoggExMaxStep(double *xx, double *varin, int nn, double varout,
@@ -6,21 +5,21 @@ double hoggExMaxStep(double *xx, double *varin, int nn, double varout,
 {
   int ii;
   double logpout, thisloglike, loglike, two[2], numerator;
-  printf("hoggExMaxStep: at start logpin = %lf\n",*logpin);
   logpout= log(1.0-exp(*logpin));
   loglike= 0.0;
-  numerator= 0.0;
   for (ii= 0; ii < nn; ii++){
     two[0]= *logpin+hoggLogGaussian(xx[ii],varin[ii]);
     two[1]= logpout+hoggLogGaussian(xx[ii],varout);
     thisloglike= hoggLogsum(two,2);
     loglike+= thisloglike;
     two[0]-= thisloglike;
-    two[1]= numerator;
-    numerator= hoggLogsum(two,2);
+    if (ii == 0){
+      numerator= two[0];
+    } else {
+      two[1]= numerator;
+      numerator= hoggLogsum(two,2);
+    }
   }
-  printf("hoggExMaxStep: log likelihood = %lf\n",loglike);
   *logpin= numerator-log((double) nn);
-  printf("hoggExMaxStep: at finish logpin = %lf\n",*logpin);
   return loglike;
 }
--- src/hogg/hoggTestExMaxStep.c
+++ src/hogg/hoggTestExMaxStep.c
@@ -3,6 +3,7 @@
 #include "hoggTweak.h"
 int hoggTestExMaxStep()
 {
+  int ii;
   double like1, like2, diff, xx[4], varin[4], varout, logpin;
   printf("hoggTestExMaxStep: testing improvement in log like\n");
   xx[0]= 0.1;
@@ -13,36 +14,22 @@ int hoggTestExMaxStep()
   varin[1]= 0.02;
   varin[2]= 0.02;
   varin[3]= 0.01;
-  varout= 1.0;
+  varout= 4.0;
   logpin= log(0.5);
   like1= hoggExMaxStep(xx,varin,4,varout,&logpin);
   like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
-  like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
   diff= like2-like1;
-  printf("hoggTestExMaxStep: diff = %lf\n",diff);
+  printf("hoggTestExMaxStep: first iteration diff = %lf\n",diff);
+  if (diff <= 0.0) return 1;
+  like1= like2;
+  for (ii=0;ii<100;ii++) like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
+  diff= like2-like1;
+  printf("hoggTestExMaxStep: next 100 iterations diff = %lf\n",diff);
   if (diff <= 0.0) return 1;
   like1= like2;
   like2= hoggExMaxStep(xx,varin,4,varout,&logpin);
   diff= like2-like1;
-  printf("hoggTestExMaxStep: diff = %lf\n",diff);
-  if (diff <= 0.0) return 1;
+  printf("hoggTestExMaxStep: 101st iteration diff = %lf\n",diff);
+  if (diff < 0.0) return 1;
   return 0;
 }
