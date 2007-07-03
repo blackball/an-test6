c996ae464eb7492306b397fdcb786e071bccab7b
David W. Hogg <dwhogg@gmail.com>
2007-07-03 14:05:01 +0000

    established unit tests for LSF code

--- src/hogg/hoggTest.c
+++ src/hogg/hoggTest.c
@@ -12,6 +12,8 @@ int main()
   if (status != 0) return(status);
   status= hoggTestExMaxStep();
   if (status != 0) return(status);
+  status= hoggTestLeastSquareFit();
+  if (status != 0) return(status);
   printf("hoggTest: all tests passed.\n");
   return 0;
 }
--- src/hogg/hoggTweak.h
+++ src/hogg/hoggTweak.h
@@ -2,4 +2,4 @@ double hoggLogsum(double * list,int nn);
 double hoggLogGaussian(double xx, double var);
 double hoggExMaxStep(double *xx, double *varin, int nn, double varout,
 		     double *logpin);
-
+double hoggLeastSquareFit(double *yy, double *xx, double *aa, int nn, int mm);
