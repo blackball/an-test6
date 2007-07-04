a7f43a4d7369a968f327cbb36b3c03ad3658ba7d
gmaps alias <dstndstn@gmail.com>
2007-07-04 17:10:24 +0000

    (dstn) remove unused scripts.

--- src/ontheweb/execs/start-watcher.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-Nthreads=3
-Cmd="/home/gmaps/ontheweb/execs/blindscript.sh %s"
-
-cd /home/gmaps/ontheweb-data
-rm queue
-/home/gmaps/ontheweb/execs/watcher -D -n $Nthreads -c "$Cmd"
-
