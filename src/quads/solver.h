add7decebc2886f338775dc58c08ae070901ff6c
Keir Mierle <mierle@gmail.com>
2007-07-19 22:19:11 +0000

    astyle --style=java -twpn solver.h

--- src/quads/solver.h
+++ src/quads/solver.h
@@ -34,123 +34,123 @@ struct solver_params;
 typedef int  (*handle_hit)(struct solver_params*, MatchObj*);
 
 enum {
-    PARITY_NORMAL,
-    PARITY_FLIP,
-    PARITY_BOTH
+	PARITY_NORMAL,
+	PARITY_FLIP,
+	PARITY_BOTH
 };
 
 // Per-index parameters.
 struct solver_index_params {
-    // name of the current index.
-    char *indexname;
+	// name of the current index.
+	char *indexname;
 
 	// unique id for this index.
-    int indexid;
-    int healpix;
+	int indexid;
+	int healpix;
 
-    // The index
-    codetree* codekd;
+	// The index
+	codetree* codekd;
 	idfile* idfile;
-    quadfile* quads;
-    startree* starkd;
+	quadfile* quads;
+	startree* starkd;
 
-    // Jitter in the index, in arcseconds.
-    double index_jitter;
+	// Jitter in the index, in arcseconds.
+	double index_jitter;
 
-    // Does the index have the CIRCLE header - (codes live in the circle, not the box)?
-    bool circle;
-    double cxdx_margin;
+	// Does the index have the CIRCLE header - (codes live in the circle, not the box)?
+	bool circle;
+	double cxdx_margin;
 
-    // Limits of the size of quads in the index.
-    double index_scale_upper;
-    double index_scale_lower;
+	// Limits of the size of quads in the index.
+	double index_scale_upper;
+	double index_scale_lower;
 
-    // The limits on the size of quads, in field coordinates (pixels),
-    // Derived from index_scale_* and funits_*.
-    double minAB;
-    double maxAB;
+	// The limits on the size of quads, in field coordinates (pixels),
+	// Derived from index_scale_* and funits_*.
+	double minAB;
+	double maxAB;
 };
 typedef struct solver_index_params solver_index_params;
 
 struct solver_params {
 
-    // the index we're currently dealing with.
-    solver_index_params* sips;
+	// the index we're currently dealing with.
+	solver_index_params* sips;
 
-    // the set of indices.
-    bl* indexes;
+	// the set of indices.
+	bl* indexes;
 
-    // the extreme limits of quad size, for all indexes.
-    double minminAB;
-    double maxmaxAB;
+	// the extreme limits of quad size, for all indexes.
+	double minminAB;
+	double maxmaxAB;
 
-    // for printing "index %i of %i"
-    int indexnum;
-    int nindexes;
+	// for printing "index %i of %i"
+	int indexnum;
+	int nindexes;
 
-    // Inputs:
+	// Inputs:
 
-    char* cancelfname;
-    bool cancelled;
+	char* cancelfname;
+	bool cancelled;
 
-    char* solved_in;
-    bool do_solvedserver;
+	char* solved_in;
+	bool do_solvedserver;
 
-    bool quitNow;
+	bool quitNow;
 
-    // one of PARITY_*
-    int parity;
-    double codetol;
-    int startobj;
-    int endobj;
+	// one of PARITY_*
+	int parity;
+	double codetol;
+	int startobj;
+	int endobj;
 
-    // Limits on the quad size, in field scale units.
-    double funits_lower;
-    double funits_upper;
+	// Limits on the quad size, in field scale units.
+	double funits_lower;
+	double funits_upper;
 
-    // The field
-    double* field;
-    int nfield;
+	// The field
+	double* field;
+	int nfield;
 
-    // Field limits.
-    double field_minx, field_maxx, field_miny, field_maxy;
-    // distance in pixels across the diagonal of the field
-    double field_diag;
+	// Field limits.
+	double field_minx, field_maxx, field_miny, field_maxy;
+	// distance in pixels across the diagonal of the field
+	double field_diag;
 
-    // number of field quads to try
-    int maxquads;
-    // number of quad matches to try
-    int maxmatches;
-    int fieldid;
-    int fieldnum;
+	// number of field quads to try
+	int maxquads;
+	// number of quad matches to try
+	int maxmatches;
+	int fieldid;
+	int fieldnum;
 
-    // number of fields we're going to try.
-    int nfields;
+	// number of fields we're going to try.
+	int nfields;
 
-    bool quiet;
+	bool quiet;
 
-    // the MatchObj template: if non-NULL, every MatchObj will be a memcpy
-    // of this one before the other fields are set.
-    MatchObj* mo_template;
+	// the MatchObj template: if non-NULL, every MatchObj will be a memcpy
+	// of this one before the other fields are set.
+	MatchObj* mo_template;
 
-    handle_hit handlehit;
+	handle_hit handlehit;
 
-    // internal:
-    double starttime;
-    double timeused;
+	// internal:
+	double starttime;
+	double timeused;
 
-    // Outputs:
-    // NOTE: these are only incremented, not initialized.  It's up to
-    // you to set them to zero before calling, if you're starting from scratch.
-    int numtries;
-    int nummatches;
-    int numscaleok;
-    // this is set to an absolute value.
-    int objsused;
-    // number of quads skipped because of cxdx constraints.
-    int numcxdxskipped;
+	// Outputs:
+	// NOTE: these are only incremented, not initialized.  It's up to
+	// you to set them to zero before calling, if you're starting from scratch.
+	int numtries;
+	int nummatches;
+	int numscaleok;
+	// this is set to an absolute value.
+	int objsused;
+	// number of quads skipped because of cxdx constraints.
+	int numcxdxskipped;
 
-    void* userdata;
+	void* userdata;
 };
 typedef struct solver_params solver_params;
 
