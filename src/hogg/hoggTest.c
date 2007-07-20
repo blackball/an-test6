cd98ef8c3089785e2ffbacdb3dce3c591ea78e8b
David W. Hogg <dwhogg@gmail.com>
2007-07-20 09:04:00 +0000

    start

--- src/hogg/hoggTest.c
+++ src/hogg/hoggTest.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "hoggTweak.h"
 
-int hoggTestLogsum();
-
 int main()
 {
   int status;
@@ -13,6 +11,10 @@ int main()
   status= hoggTestExMaxStep();
   if (status != 0) return(status);
   status= hoggTestLeastSquareFit();
+  if (status != 0) return(status);
+  status= hoggTestLeastSquareFit();
+  if (status != 0) return(status);
+  status= hoggTestRansacFit();
   if (status != 0) return(status);
   printf("hoggTest: all tests passed.\n");
   return 0;
--- src/hogg/hoggTweak.h
+++ src/hogg/hoggTweak.h
@@ -3,3 +3,5 @@ double hoggLogGaussian(double xx, double var);
 double hoggExMaxStep(double *xx, double *varin, int nn, double varout,
 		     double *logpin);
 double hoggLeastSquareFit(double *yy, double *xx, double *aa, int nn, int mm);
+double hoggRansacFit(double *yy, double *AA, double *xx, int nn, int mm,
+		     int *use);
