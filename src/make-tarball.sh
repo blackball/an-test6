73dbd68161d241f1fa12b6414597b335a21a828e
Dustin Lang <dstndstn@gmail.com>
2007-07-25 18:39:29 +0000

    remove tweak from top-level files

--- src/make-tarball.sh
+++ src/make-tarball.sh
@@ -30,7 +30,7 @@ cmd="svn export -N $SVN $DIR"
 echo $cmd
 $cmd || exit
 
-for x in cfitsio qfits-an an-common libkd tweak gsl-an quads; do
+for x in cfitsio qfits-an an-common libkd gsl-an quads; do
     cmd="svn export $SVN/$x $DIR/$x"
     echo $cmd
     $cmd || exit
