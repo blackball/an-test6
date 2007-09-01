49b0f36abd41236128618f88a678fe0649998dfb
Dustin Lang <dstndstn@gmail.com>
2007-09-01 15:47:57 +0000

    twiddle keywords.h

--- src/an-common/keywords.h
+++ src/an-common/keywords.h
@@ -25,7 +25,7 @@
 
 #define ATTRIB_FORMAT(style,fmt,start) __attribute__ ((format(style,fmt,start)))
 
-#if __GNUC_PREREQ(3, 0)
+#if defined __GNUC__ && __GNUC_PREREQ (3, 0)
 
 //# define Inline inline __attribute__ ((always_inline))
 # define Inline        inline
@@ -33,7 +33,6 @@
 # define Const            __attribute__ ((const))
 # define Noreturn         __attribute__ ((noreturn))
 # define Malloc           __attribute__ ((malloc))
-# define Must_check       __attribute__ ((warn_unused_result))
 # define Deprecated       __attribute__ ((deprecated))
 # define Used             __attribute__ ((used))
 # define Unused           __attribute__ ((unused))
@@ -43,9 +42,11 @@
 # define Noinline         __attribute__ ((noinline))
 
 // warn_unused_result is new in gcc-3.4
-#if __GNUC_PREREQ(3, 4)
+#if __GNUC_PREREQ (3, 4)
+# define Must_check       __attribute__ ((warn_unused_result))
 # define WarnUnusedResult __attribute__ ((warn_unused_result))
 #else
+# define Must_check
 # define WarnUnusedResult
 #endif
 
