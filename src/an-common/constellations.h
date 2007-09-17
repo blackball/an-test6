fd65dfe66f1353985761f5fbf55f9f2cc37caac2
Dustin Lang <dstndstn@gmail.com>
2007-09-17 21:34:39 +0000

    minor comments

--- src/an-common/constellations.h
+++ src/an-common/constellations.h
@@ -35,11 +35,21 @@ il* constellations_get_lines(int constellation_num);
 
 il* constellations_get_unique_stars(int constellation_num);
 
+/*
+ Returns the star IDs of the line_num'th line.
+ */
 void constellations_get_line(int constellation_num, int line_num,
 							 int* ep1, int* ep2);
 
+/*
+ Returns a newly-allocated dl* which is a list of (ra1, dec1), (ra2, dec2) coordinates
+ of the line endpoints.
+ */
 dl* constellations_get_lines_radec(int constellation_num);
 
+/*
+ RA,Dec in degrees
+ */
 void constellations_get_star_radec(int starnum, double* ra, double* dec);
 
 #endif
