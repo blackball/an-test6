98ceccfed3c4c6d420874bf286325fb9c72947b1
Keir Mierle <mierle@gmail.com>
2007-10-04 03:46:50 +0000

    Make test_ctmf pass and add to test suite properly

--- src/quads/test_ctmf.c
+++ src/quads/test_ctmf.c
@@ -38,9 +38,8 @@ void test_simple_median(CuTest* tc) {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	unsigned char results[sizeof(test_data)];
-//	memset(results,9,sizeof(test_data));
-	ctmf(test_data, results, 10,10, 1,1,4 /*r*/, 1, 512*1024);
-//	ctmf(results, test_data, 10,10, 1,1,3 /*r*/, 1, 512*1024);
+	memset(results,9,sizeof(test_data));
+	ctmf(test_data, results, 10,10, 10,10, 4 /*r*/, 1, 512*1024);
 	for (i=0; i<sizeof(test_data); i++)
 		CuAssertIntEquals(tc, 0, results[i]);
 }
