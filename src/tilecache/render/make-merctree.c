c388a000d3ea1327e99bc644e2352e65014dd0a8
Dustin Lang <dstndstn@gmail.com>
2007-10-11 22:36:18 +0000

    revert make-merctree.c to make-merctree2.c @ rev 5379

--- src/tilecache/render/make-merctree.c
+++ src/tilecache/render/make-merctree.c
@@ -1,6 +1,6 @@
 /*
   This file is part of the Astrometry.net suite.
-  Copyright 2006, Dustin Lang, Keir Mierle and Sam Roweis.
+  Copyright 2006, 2007 Dustin Lang, Keir Mierle and Sam Roweis.
 
   The Astrometry.net suite is free software; you can redistribute
   it and/or modify it under the terms of the GNU General Public License
@@ -29,12 +29,18 @@
 #include "starutil.h"
 #include "an_catalog.h"
 #include "mathutil.h"
+#include "healpix.h"
+#include "merc.h"
 
-#define OPTIONS "ho:l:m:t:d:bsSc" // T
+#define OPTIONS "ho:l:m:t:d:bsScM:N:i:H:I"
 
 void printHelp(char* progname) {
 	fprintf(stderr, "%s usage:\n"
 			"   -o <output-file>\n"
+			"   -M <Mercator grid size>\n"
+			"   -N <Mercator grid number>\n"
+			"   -i <input-catalog-pattern>\n"
+			"   -H <Nside of AN catalog healpixelization>\n"
 			"  [-m <max-y-val>]: Maximum y value of the projection (default: Pi)\n"
 			"  [-l <n-leaf-point>]: Target number of points in the leaves of the tree (default 15).\n"
 			"  [-t  <tree type>]:  {double,float,u32,u16}, default u32.\n"
@@ -43,8 +49,7 @@ void printHelp(char* progname) {
 			"    OR [-s]: build splitting planes   )\n"
 			"  [-S]: include separate splitdim array\n"
 			"  [-c]: run kdtree_check on the resulting tree\n"
-			"\n"
-			"  <input-catalog> ...\n"
+			"  [-I]: ignore input files that don't exist.\n"
 			"\n"
 			"    (Input files are Astrometry.net catalogs.)\n"
 			"\n", progname);
@@ -57,13 +62,13 @@ int main(int argc, char** args) {
 	int argchar;
 	char* progname = args[0];
 	char* outfn = NULL;
-	float maxy = M_PI;
+	char* infn = NULL;
+	double maxy = M_PI;
 	int i;
 	int Nleaf = 15;
 	time_t start;
 	merctree* mt;
 	int N;
-	int infile;
 	double* xy;
 	int exttype  = KDT_EXT_DOUBLE;
 	int datatype = KDT_DATA_NULL;
@@ -71,14 +76,37 @@ int main(int argc, char** args) {
 	int tt;
 	int buildopts = 0;
 	int checktree = 0;
+	double xlo, xhi, ylo, yhi;
+	int Nside = 0;
+
+	int Nmerc = -1;
+	int mercgrid = -1;
+	int HP;
+	il* hps;
+	bool ignore = FALSE;
 
 	start = time(NULL);
 
 	while ((argchar = getopt(argc, args, OPTIONS)) != -1)
 		switch (argchar) {
+		case 'I':
+			ignore = TRUE;
+			break;
 		case 'h':
 			printHelp(progname);
 			exit(0);
+		case 'i':
+			infn = optarg;
+			break;
+		case 'H':
+			Nside = atoi(optarg);
+			break;
+		case 'M':
+			Nmerc = atoi(optarg);
+			break;
+		case 'N':
+			mercgrid = atoi(optarg);
+			break;
 		case 'c':
 			checktree = 1;
 			break;
@@ -108,14 +136,26 @@ int main(int argc, char** args) {
 			break;
 		}
 
-	if (!outfn || (optind == argc)) {
+	if (!outfn || (optind != argc)) {
 		printHelp(progname);
 		exit(-1);
 	}
 
 	if (!(buildopts & (KD_BUILD_BBOX | KD_BUILD_SPLIT))) {
 		printf("You need bounding-boxes or splitting planes!\n");
-		printHelp(progname);
+ 		printHelp(progname);
+		exit(-1);
+	}
+
+	if ((Nmerc == -1) || (mercgrid == -1)) {
+		printf("You must specify -N and -M!\n");
+ 		printHelp(progname);
+		exit(-1);
+	}
+
+	if (Nside<1 || !infn) {
+		printf("You must specify -H and -i!\n");
+ 		printHelp(progname);
 		exit(-1);
 	}
 
@@ -127,27 +167,83 @@ int main(int argc, char** args) {
 
 	tt = kdtree_kdtypes_to_treetype(exttype, treetype, datatype);
 
+	// ranges in Mercator space.
+	{
+		int xgrid = mercgrid % Nmerc;
+		int ygrid = mercgrid / Nmerc;
+		double ymax = (maxy + M_PI) / (2.0 * M_PI);
+		double ymin = (-maxy + M_PI) / (2.0 * M_PI);
+		xlo = xgrid / (double)Nmerc;
+		xhi = (xgrid+1) / (double)Nmerc;
+		ylo = ymin + (ymax - ymin) * ygrid / (double)Nmerc;
+		yhi = ymin + (ymax - ymin) * (ygrid+1) / (double)Nmerc;
+	}
+	printf("Mercator ranges: x [%g,%g], y [%g,%g]\n", xlo, xhi, ylo, yhi);
+
 	mt = merctree_new();
 	if (!mt) {
 		fprintf(stderr, "Failed to allocate a merctree.\n");
 		exit(-1);
 	}
 
-	// Read all the catalogs to see how many points there are in total.
-	fprintf(stderr, "Reading catalogs to find total number of points...\n");
+	// Go through the healpixes and find the ones that might overlap
+	// with this rectangle in Mercator space.
+	HP = 12 * Nside * Nside;
+	hps = il_new(256);
+
+	for (i=0; i<HP; i++) {
+		double ra1, ra2;
+		double x1, x2;
+		double dec1, dec2;
+		double y1, y2;
+		double nil;
+
+		// leftmost point is (0,1); rightmost is (1,0).
+		// southmost is (0,0); northmost is (1,1).
+		healpix_to_radec(i, Nside, 0.0, 1.0, &ra1, &nil);
+		healpix_to_radec(i, Nside, 1.0, 0.0, &ra2, &nil);
+		x1 = ra2merc(ra1);
+		x2 = ra2merc(ra2);
+
+		if (x2 > x1) {
+			if ((x1 > xhi) || (x2 < xlo))
+				continue;
+		} else {
+			// wrap-around
+			if ((x1 > xhi) && (x2 < xlo))
+				continue;
+		}
+
+		healpix_to_radec(i, Nside, 0.0, 0.0, &nil, &dec1);
+		healpix_to_radec(i, Nside, 1.0, 1.0, &nil, &dec2);
+		y1 = dec2merc(dec1);
+		y2 = dec2merc(dec2);
+
+		if ((y1 > yhi) || (y2 < ylo))
+			continue;
+
+		il_append(hps, i);
+	}
+
+	// Count the total number of points in the overlapping healpixes.
 	N = 0;
-	for (infile = optind; infile<argc; infile++) {
-		char* fn;
-		an_catalog* ancat;
-		fn = args[infile];
-		ancat = an_catalog_open(fn);
-		if (!ancat) {
-			fprintf(stderr, "Failed to open Astrometry.net catalog %s\n", fn);
+	for (i=0; i<il_size(hps); i++) {
+		char fn[1024];
+		int hp;
+		an_catalog* cat;
+
+		hp = il_get(hps, i);
+		sprintf(fn, infn, hp);
+		cat = an_catalog_open(fn);
+		if (!cat) {
+			fprintf(stderr, "Failed to open catalog %s.\n", fn);
+			if (ignore)
+				continue;
 			exit(-1);
 		}
-		fprintf(stderr, "  %i in %s\n", ancat->nentries, fn);
-		N += ancat->nentries;
-		an_catalog_close(ancat);
+		fprintf(stderr, "  %i points in %s\n", cat->nentries, fn);
+		N += cat->nentries;
+		an_catalog_close(cat);
 	}
 	fprintf(stderr, "Total: %i points.\n", N);
 
@@ -160,56 +256,43 @@ int main(int argc, char** args) {
 		exit(-1);
 	}
 
-	i=0;
-	for (infile = optind; infile<argc; infile++) {
-		char* fn;
+	N = 0;
+	for (i=0; i<il_size(hps); i++) {
+		char fn[1024];
+		int hp;
+		an_catalog* cat;
 		int j;
-		an_catalog* ancat;
-		int lastgrass = 0;
 
-		fn = args[infile];
-		ancat = an_catalog_open(fn);
-		if (!ancat) {
-			fprintf(stderr, "Failed to open Astrometry.net catalog %s\n", fn);
+		hp = il_get(hps, i);
+		sprintf(fn, infn, hp);
+		cat = an_catalog_open(fn);
+		if (!cat) {
+			fprintf(stderr, "Failed to open catalog %s.\n", fn);
+			if (ignore)
+				continue;
 			exit(-1);
 		}
 
-		fprintf(stderr, "Reading %i entries for catalog file %s.\n", ancat->nentries, fn);
-
-		for (j=0; j<ancat->nentries; j++) {
-			int grass;
+		for (j=0; j<cat->nentries; j++) {
 			float vertscale;
 			an_entry* entry;
 			double x, y;
 			int o;
 
-			entry = an_catalog_read_entry(ancat);
+			entry = an_catalog_read_entry(cat);
 			if (!entry)
 				break;
 
-			grass = (j * 80 / ancat->nentries);
-			if (grass != lastgrass) {
-				fprintf(stderr, ".");
-				fflush(stderr);
-				lastgrass = grass;
-			}
-
-			x = (entry->ra / 360.0);
-			y = (maxy + asinh(tan(deg2rad(entry->dec)))) / (2.0 * maxy);
+			x =  radeg2merc(entry->ra );
+			if (x < xlo || x > xhi)
+				continue;
+			y = decdeg2merc(entry->dec);
+			if (y < ylo || y > yhi)
+				continue;
 			vertscale = 1.0 / cos(deg2rad(entry->dec));
 
-			if (x < 0.0 || x >= 1.0) {
-				fprintf(stderr, "x out of range: %g\n", x);
-				continue;
-			}
-			if (y < 0.0 || y > 1.0) {
-				// This is to be expected for points in the polar caps.
-				//fprintf(stderr, "y out of range: %g\n", y);
-				continue;
-			}
-
-			xy[i*2] = x;
-			xy[i*2+1] = y;
+			xy[N*2] = x;
+			xy[N*2+1] = y;
 
 			for (o=0; o<entry->nobs; o++) {
 				bool red = FALSE, blue = FALSE, ir = FALSE;
@@ -249,33 +332,28 @@ int main(int argc, char** args) {
 
 				flux = mag2flux(ob->mag) * vertscale;
 				if (red)
-					mt->flux[i].rflux += flux;
+					mt->flux[N].rflux += flux;
 				if (blue)
-					mt->flux[i].bflux += flux;
+					mt->flux[N].bflux += flux;
 				if (ir)
-					mt->flux[i].nflux += flux;
+					mt->flux[N].nflux += flux;
 			}
 
-			i++;
+			N++;
 		}
-		an_catalog_close(ancat);
-		fprintf(stderr, "\n");
+
+		an_catalog_close(cat);
 	}
+	fprintf(stderr, "Total: %i points overlap.\n", N);
 
-	// The total number of points kept:
-	N = i;
+	il_free(hps);
 
-	fprintf(stderr, "Kep: %i points within range.\n", N);
+	xy = realloc(xy, N * 2 * sizeof(double));
+	mt->flux = realloc(mt->flux, N * sizeof(merc_flux));
 
 	fprintf(stderr, "Creating kdtree...\n");
 
 	mt->tree = kdtree_new(N, 2, Nleaf);
-	// Set the range...
-	{
-		double lo[] = { 0.0, 0.0 };
-		double hi[] = { 1.0, 1.0 };
-		kdtree_set_limits(mt->tree, lo, hi);
-	}
 
 	if (datatype != exttype) {
 		fprintf(stderr, "Converting data...\n");
