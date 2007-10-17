ebf4478c1f942867f8edc1df8f5d845906a55389
gmaps alias <dstndstn@gmail.com>
2007-10-17 16:14:58 +0000

    (dstn) simple

--- src/ontheweb/web/fill.php
+++ src/ontheweb/web/fill.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'HTML/QuickForm.php';
 require_once 'HTML/QuickForm/Renderer/QuickHtml.php';
-
 require_once 'common2.php';
 
 main();
@@ -12,17 +11,42 @@ function main() {
 	session_start();
 	if (!$_SESSION['logged-in']) {
 		// Back to login page.
-		header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/" . $loginpage);
+		header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/" . $login_page);
 		exit;
 	}
 
-	echo "<p> Logged in as " . $_SESSION['yourname'] . "</p>";
+	echo "<p> Logged in as <b>" . $_SESSION['yourname'] . "</b> ";
 	$logout = get_logout_form();
 	$renderer =& new HTML_QuickForm_Renderer_QuickHtml();
 	$logout->accept($renderer);
 	echo $renderer->toHtml();
+	echo "</p>";
 
 	echo "<p>Hello there.</p>";
+
+	$urlFormDefaults = array('imgurl' => 'http://');
+
+	$urlform = new HTML_QuickForm('urlform', 'post');
+	$urlform->removeAttribute('name');
+	$urlform->setDefaults($urlFormDefaults);
+	$urlform->addElement('text', 'imgurl', '', array('size' => 40));
+	$urlform->addElement('submit', 'submit', 'Submit');
+
+	$vals = $urlform->exportValues();
+	if (($vals['submit'] == 'Submit') && $urlform->validate()) {
+		$urlform->freeze();
+		$urlform->process('process_urlform', false);
+	} else {
+		$renderer =& new HTML_QuickForm_Renderer_QuickHtml();
+		$urlform->accept($renderer);
+		echo $renderer->toHtml();
+	}
+	exit;
+}
+
+function process_urlform($vals) {
+	echo "Yep<p>";
+	print_r($vals);
 }
 
 ?>
