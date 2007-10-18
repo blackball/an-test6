d1d08f7de2cd284c85fb4c63dc32d1d19437aed5
gmaps alias <dstndstn@gmail.com>
2007-10-18 15:22:50 +0000

    (dstn) collapsible scale box

--- src/ontheweb/web/fill.php
+++ src/ontheweb/web/fill.php
@@ -42,8 +42,21 @@ function main() {
 	if ($_REQUEST['long']) {
 		global $unitmap;
 
+		$longformDefaults = array('x_col' => 'X',
+								  'y_col' => 'Y',
+								  'parity' => 2,
+								  'fstype' => 'ul',
+								  'fsl' => 0.1,
+								  'fsu' => 180,
+								  'tweak' => 1,
+								  'tweak_order' => 2,
+								  'imgurl' => "http://",
+								  'scaleunits' => 'degreewidth',
+								  );
+
 		$longform = new HTML_QuickForm('longform', 'post');
 		$longform->removeAttribute('name');
+		$longform->setDefaults($longformDefaults);
 		$longform->addElement('text', 'imgurl', '', array('size' => 40));
 		$longform->addElement('file', 'imgfile', '', array('size' => 40));
 		$longform->addElement('file', 'fitsfile', '', array('size' => 40));
@@ -58,17 +71,61 @@ function main() {
 									'onkeyup' =>'sourceChanged()',
 									'id' => 'selectsource'));
 
+		$longform->addElement('select', 'scaleunits', '', $unitmap,
+							  array('onchange' =>'unitsChanged()',
+									'onkeyup' =>'unitsChanged()',
+									'id' => 'selectunits'));
+		$longform->addElement('radio', 'fstype', null, null, 'ul',
+							  array('id'=>'fstype-ul',
+									'onclick'=>'scalechanged()'));
+		$longform->addElement('radio', 'fstype', null, null, 'ev',
+							  array('id'=>'fstype-ev',
+									'onclick'=>'scalechanged()'));
+		$longform->addElement('text', 'fsl', '',
+							  array('size'=>5,
+									'id'=>'fsl',
+									'onkeyup' => 'scalechanged()',
+									'onfocus' => 'setFsUl()'));
+		$longform->addElement('text', 'fsu', '',
+							  array('size'=>5,
+									'id'=>'fsu',
+									'onkeyup' => 'scalechanged()',
+									'onfocus' => 'setFsUl()'));
+		$longform->addElement('text', 'fse', '',
+							  array('size'=>5,
+									'id'=>'fse',
+									'onkeyup' => 'scalechanged()',
+									'onfocus' => 'setFsEv()'));
+		$longform->addElement('text', 'fsv', '',
+							  array('size'=>5,
+									'id'=>'fsv',
+									'onkeyup' => 'scalechanged()',
+									'onfocus' => 'setFsEv()',));
+
 		$longform->addElement('submit', 'submit', 'Submit');
+
+		$longform->validate();
 
 		//$longform->addElement('select', 'fsunit', 'units', $unitmap, array('onchange=);
 		$renderer =& new HTML_QuickForm_Renderer_QuickHtml();
 		$longform->accept($renderer);
 
 		$replace = array();
-		$flds = array('imgurl', 'imgfile', 'fitsfile', 'textfile', 'src', 'submit');
+		$flds = array('imgurl', 'imgfile', 'fitsfile', 'textfile',
+					  'src', 'scaleunits', 'fsl', 'fsu', 'fse', 'fsv',
+					  'submit');
 		foreach ($flds as $fld) {
 			$replace['##' . $fld . '##'] = $renderer->elementToHtml($fld);
+			$replace['##' . $fld . '-err##'] = $longform->getElementError($fld);
 		}
+		// pseudo-fields that can have errors.
+		$flds = array('fstype');
+		foreach ($flds as $fld) {
+			$replace['##' . $fld . '-err##'] = $longform->getElementError($fld);
+		}
+
+		$replace['##fstype-ul##']    = $renderer->elementToHtml('fstype', 'ul');
+		$replace['##fstype-ev##']    = $renderer->elementToHtml('fstype', 'ev');
 		$template = file_get_contents('template-long-body.html');
 		$template = str_replace(array_keys($replace), array_values($replace), $template);
 		echo '<' . '?xml version="1.0" encoding="UTF-8"?' . '>';
