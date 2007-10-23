c1fe1f380e47d2a61c1922f10324f4914b2ce48e
gmaps alias <dstndstn@gmail.com>
2007-10-23 18:11:11 +0000

    (dstn) more cleaning & verification

--- src/tilecache/an/portal/views.py
+++ src/tilecache/an/portal/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as authlogin
 from django.contrib.auth import logout as authlogout
 from django.newforms import widgets
+import re
 import settings
 
 # Adding a user:
@@ -65,7 +66,7 @@ class FullForm(forms.Form):
 		attrs={'onfocus':'setFsEv()',
 			   'onkeyup':'scalechanged()',
 			   'size':'5',
-			   }), required=False)
+			   }), required=False, min_value=0)
 	scaleerr = forms.DecimalField(widget=forms.TextInput(
 		attrs={'onfocus':'setFsEv()',
 			   'onkeyup':'scalechanged()',
@@ -123,6 +124,7 @@ class FullForm(forms.Form):
 	def clean(self):
 		"""Take a shower"""
 		xysrc = self.getclean('xysrc')
+		print 'clean() called; xysrc is', xysrc
 		if (xysrc == 'url') or (xysrc == 'fitsurl'):
 			urlerr = self.geterror('url')
 			urlval = self.getclean('url')
@@ -132,12 +134,28 @@ class FullForm(forms.Form):
 				if urlval is None or (len(urlval) == 0):
 					self._errors['url'] = ['URL is required']
 		elif (xysrc == 'file') or (xysrc == 'fitsfile'):
-			if (not 'file' in self._errors):
+			if not self.geterror('file'):
 				file = self.getclean('file')
 				print 'file is',repr(file)
 				print 'file in self.files is',repr('file' in self.files and self.files['file'] or None)
 				if not file:
 					self._errors['file'] = ['You must upload a file.']
+
+		if (xysrc == 'fitsfile') or (xysrc == 'fitsurl'):
+			xcol = self.getclean('xcol')
+			ycol = self.getclean('ycol')
+			fitscol = re.compile('^[\w_-]+$')
+			if not self.geterror('xcol'):
+				if not xcol or len(xcol) == 0:
+					self._errors['xcol'] = ['You must specify the X column name.']
+				elif not fitscol.match(xcol):
+					self._errors['xcol'] = ['X column name must be alphanumeric.']
+			if not self.geterror('ycol'):
+				if not ycol or len(ycol) == 0:
+					self._errors['ycol'] = ['You must specify the Y column name.']
+				elif not fitscol.match(ycol):
+					self._errors['ycol'] = ['Y column name must be alphanumeric.']
+
 		if self.getclean('tweak'):
 			order = self.getclean('tweakorder')
 			print 'tweak order', order
@@ -145,6 +163,26 @@ class FullForm(forms.Form):
 				self._errors['tweakorder'] = ['Tweak order is required if tweak is enabled.']
 
 		print 'form clean(): scaletype is', self.getclean('scaletype')
+		scaletype = self.getclean('scaletype')
+		if scaletype == 'ul':
+			if not self.geterror('scalelower') and not self.geterror('scaleupper'):
+				lower = self.getclean('scalelower')
+				upper = self.getclean('scaleupper')
+				if lower is None:
+					self._errors['scalelower'] = ['You must enter a lower bound.']
+				if upper is None:
+					self._errors['scaleupper'] = ['You must enter an upper bound.']
+				if (lower > upper):
+					self._errors['scalelower'] = ['Lower bound must be less than upper bound.']
+		elif scaletype == 'ev':
+			if not self.geterror('scaleest') and not self.geterror('scaleerr'):
+				est = self.getclean('scaleest')
+				err = self.getclean('scaleerr')
+				if est is None:
+					self._errors['scaleest'] = ['You must enter a scale estimate.']
+				if err is None:
+					self._errors['scaleerr'] = ['You must enter a scale error bound.']
+
 		return self.cleaned_data
 
 
@@ -266,6 +304,10 @@ def newlong(request):
 	# Note, if there are *ANY* errors, the form will have no 'cleaned_data'
 	# array.
 
+	print 'Errors:'
+	if form._errors:
+		for k,v in form._errors.items():
+			print '  ',k,'=',v
 
 	#print 'scaletype is', form.getclean('scaletype')
 	#scaletype = form['scaletype'].field
@@ -277,11 +319,18 @@ def newlong(request):
 	scaletype = form['scaletype'].field
 	widg = scaletype.widget
 	attrs = widg.attrs
-	render = widg.get_renderer('scaletype', form.getclean('scaletype'), attrs)
+	if request.POST:
+		val = widg.value_from_datadict(request.POST, None, 'scaletype')
+	else:
+		val = form.getclean('scaletype') or scaletype.initial
+	render = widg.get_renderer('scaletype', val, attrs)
+	#print 'scaletype val is', val
 	r0 = render[0]
 	r1 = render[1]
 	r0txt = r0.tag()
 	r1txt = r1.tag()
+	#print 'checkbox 0:', r0txt
+	#print 'checkbox 1:', r1txt
 
 	ctxt = {
 		'form' : form,
@@ -289,7 +338,7 @@ def newlong(request):
 		'scale_ee' : r1txt,
 		}
 	errfields = [ 'scalelower', 'scaleupper', 'scaleest', 'scaleerr',
-				  'tweakorder', ]
+				  'tweakorder', 'xcol', 'ycol', 'scaletype' ]
 	for f in errfields:
 		ctxt[f + '_err'] = len(form[f].errors) and form[f].errors[0] or None
 
