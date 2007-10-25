9531c182c3a2a544d856ae7410f6f1d188279a35
Dustin Lang <dstndstn@gmail.com>
2007-10-25 15:43:23 +0000

    add FileMultipart

--- src/tilecache/an/multipart.py
+++ src/tilecache/an/multipart.py
@@ -89,18 +89,29 @@ class Multipart(StateMachine):
     def add_part(self, part):
         self.parts.append(part)
 
+    def get_part_body_state(self, mainbody):
+        return PartBodyState(self, mainbody)
+
+    def get_body_state(self):
+        ct = self.mainheader.headers['Content-Type']
+        if ct.startswith('multipart/form-data'):
+            return self.get_multipart_body_state(ct)
+        else:
+            log('Content-type is "%s", not multipart/form-data.' % ct)
+        return None
+
+    def get_multipart_body_state(self, ct):
+        return MultipartBodyState(self, ct)
+
     def main_header_done(self):
         log('main header finished.')
         for k,v in self.mainheader.headers.items():
             log('  ' + str(k) + " = " + str(v))
-
-        ct = self.mainheader.headers['Content-Type']
-        if ct.startswith('multipart/form-data'):
-            self.body = MultipartBodyState(self, ct)
-            self.state = self.body
-        else:
-            log('Content-type is "%s", not multipart/form-data.' % ct)
-            self.error
+        self.body = self.get_body_state()
+        self.state = self.body
+        if not self.state:
+            log('No body state found.')
+            self.error = True
 
 class HeaderState(StateMachineState):
     headers = None
@@ -181,7 +192,10 @@ class MultipartBodyState(StateMachineState):
         return PartHeaderState(self.machine, self)
 
     def get_body_parser(self):
-        return PartBodyState(self.machine, self)
+        # call up to the parent so that subclassers only have to
+        # override one class...
+        #PartBodyState(self.machine, self)
+        return self.machine.get_part_body_state(self)
 
     def process(self):
         stream = self.machine
@@ -219,12 +233,14 @@ class MultipartBodyState(StateMachineState):
         self.bodyparser = self.get_body_parser()
 
     def part_body_done(self):
-        if not self.currentpart: #len(self.parts) == 0:
+        if not self.currentpart:
             log('Preamble ended.')
         else:
-            log('Body ended.  Got %d bytes of data.' % len(self.bodyparser.data))
-            log('Data is: ***%s***' % self.bodyparser.data)
-            self.currentpart['data'] = self.bodyparser.data
+            datalen = self.bodyparser.get_data_length()
+            data = self.bodyparser.get_data()
+            log('Body ended.  Got %d bytes of data.' % datalen)
+            log('Data is: ***%s***' % data)
+            self.currentpart['data'] = data
         self.headerparser = self.get_header_parser()
         self.inheader = True
 
@@ -274,9 +290,9 @@ class PartBodyState(StateMachineState):
             stream.discard_data(relen)
             if match.group('dashdash'):
                 # it's the end of the last part.
-                self.body.last_part_body_done()
+                self.last_part_body_done()
             else:
-                self.body.part_body_done()
+                self.part_body_done()
             return
 
         # not found.  chomp the data up to the last CR, if it
@@ -288,6 +304,18 @@ class PartBodyState(StateMachineState):
         self.handle_data(stream.data[:ind])
         stream.discard_data(ind)
 
+    def get_data_length(self):
+        return len(self.data)
+
+    def get_data(self):
+        return self.data
+
+    def part_body_done(self):
+        self.body.part_body_done()
+
+    def last_part_body_done(self):
+        self.body.last_part_body_done()
+
     def handle_data(self, data):
         log('PartBodyState: handling data ***%s***' % data)
         self.data += data
@@ -296,163 +324,63 @@ class PartBodyState(StateMachineState):
         self.data = ''
 
 
-#class MainBodyState(StateMachineState):
-#    def process(self):
-#        stream = self.machine
-#        stream.error = True
 
 
+class FileMultipart(Multipart):
+    writefields = {}
 
-class multipart:
-    boundary = None
-    contentlength = 0
-    #endheader = False
-    inparthdr = False
-    formparts = []
-    fileform = 'file'
-    tempfile = None
+    def get_part_body_state(self, mainbody):
+        superme = super(FileMultipart, self)
+        currentpart = mainbody.currentpart
+        if not currentpart:
+            return superme.get_part_body_state(mainbody)
 
-    def __init__(self, fin):
-        self.fin = fin
+        key = 'field'
+        fnkey = 'filename'
+        if not ((key in currentpart) and \
+                (fnkey in currentpart)):
+            return superme.get_part_body_state(mainbody)
 
-    def readmore(self):
-        newdata = self.fin.read(4096)
-        self.data += newdata
-        matched = False
-        log('read %d bytes; now have %d.' % (len(newdata), len(self.data)))
-        log('data starts with:\n\n***%s***\n' % self.data[0:64])
-        if not self.boundary:
-            log('looking for content-type...')
-            ctre = re.compile(r'^Content-Type: multipart/form-data; boundary=(?P<boundary>[' + ALPHANUM + r']+)' + CRLF)
-            res = ctre.match(self.data)
-            if not res:
-                log('no match.')
-            if res:
-                log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                self.boundary = res.group('boundary')
-                log('boundary: "%s"' % self.boundary)
-                end = res.end(0)
-                self.data = self.data[end:]
-                matched = True
-        if not self.contentlength:
-            log('looking for content-length...')
-            ctre = re.compile(r'^Content-Length: (?P<length>[0-9]+)' + CRLF)
-            res = ctre.match(self.data)
-            if not res:
-                log('no match.')
-            if res:
-                log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                self.contentlength = int(res.group('length'))
-                log('length: "%d"' % self.contentlength)
-                end = res.end(0)
-                self.data = self.data[end:]
-                matched = True
+        fn = currentpart[fnkey]
+        field = currentpart[key]
+        filename = self.get_filename(field, fn, currentpart)
+        if not filename:
+            return superme.get_part_body_state(mainbody)
+        return FileBodyState(self, mainbody, filename)
 
-        #if not self.endheader:
-        #    log('looking for end of header')
-        #    ctre = re.compile(r'^\r\n')
-        #    res = ctre.match(self.data)
-        #    if not res:
-        #        log('no match.')
-        #    if res:
-        #        log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-        #        self.endheader = True
-        #        end = res.end(0)
-        #        self.data = self.data[end:]
-        #        matched = True
-        if self.boundary:
-            log('looking for boundary\n      (--%s)' % self.boundary)
-            ctre = re.compile(CRLF + '--' + self.boundary + r'(--)?' + CRLF)
-            res = ctre.match(self.data)
-            if not res:
-                log('no match.')
-            if res:
-                log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                end = res.end(0)
-                self.data = self.data[end:]
-                self.inparthdr = True
-                matched = True
-        
-        if self.inparthdr:
-            log('looking for content-disposition')
-            ctre = re.compile(r'^Content-Disposition: form-data; name="(?P<name>[' + ALPHANUM + r']+)"' +
-                              r'(; filename="(?P<filename>[' + ALPHANUM + r']+)")?' + CRLF)
-            res = ctre.match(self.data)
+    def get_filename(self, field, filename, currentpart):
+        if not field in self.writefields:
+            return None
+        return self.writefields[field]
 
-            # HACK
-            maintype = None
-            subtype = None
-            fieldname = None
-            filename = None
+class FileBodyState(PartBodyState):
+    filename = None
+    fid = None
+    datalen = 0
 
-            if not res:
-                log('no match.')
-            if res:
-                log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                fieldname = res.group('name')
-                filename = res.group('filename')
-                log('field: %s' % fieldname)
-                end = res.end(0)
-                self.data = self.data[end:]
-                matched = True
-            log('looking for content-type')
-            ctre = re.compile(r'^Content-Type: (?P<type>[' + ALPHANUM + r']+)/(?P<subtype>[' + ALPHANUM + r']+)' + CRLF)
-            res = ctre.match(self.data)
-            if not res:
-                log('no match.')
-            if res:
-                log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                maintype = res.group('type')
-                subtype = res.group('subtype')
-                end = res.end(0)
-                self.data = self.data[end:]
-                matched = True
-            
-            log('looking for end of part header.')
-            if self.data.startswith(CRLF):
-                log('found it.')
-                self.data = self.data[2:]
-                self.inparthdr = False
-                self.inbody = True
-                self.formparts.append({'field':fieldname, 'data':'', 'type':maintype, 'subtype':subtype})
-                return
+    def __init__(self, machine, mainbody, filename):
+        self.filename = filename
+        self.fid = open(filename, 'wb')
+        super(FileBodyState, self).__init__(machine, mainbody)
+        self.data = None
 
-        if self.inbody:
-            log('in body.')
-            if not self.data.startswith(CRLF):
-                # append start of data to this part's data.
-                i = self.data.find(CRLF)
-                if (i == -1):
-                    # no CRLF, append the whole thing.
-                    log('chomping:\n***%s***' % self.data)
-                    (self.formparts[-1])['data'] += self.data
-                    self.data = ''
-                else:
-                    log('chomping:\n***%s***' % self.data[0:i])
-                    (self.formparts[-1])['data'] += self.data[0:i]
-                    self.data = self.data[i:]
+    def handle_data(self, data):
+        self.datalen += len(data)
+        self.fid.write(data)
 
-            if len(self.data) > 2:
-                log('looking for boundary')
-                ctre = re.compile(CRLF + '--' + self.boundary + r'(--)?' + CRLF)
-                res = ctre.match(self.data)
-                if not res:
-                    log('no match.')
-                if res:
-                    log('matched: "%s"' % self.data[res.start(0):res.end(0)])
-                    end = res.end(0)
-                    self.data = self.data[end:]
-                    self.inparthdr = True
-                    self.inbody = False
-                    matched = True
-                else:
-                    log('no match.  Eating the CRLF.')
-                    (self.formparts[-1])['data'] += self.data[0:2]
-                    self.data = self.data[2:]
+    def get_data_length(self):
+        return self.datalen
 
+    def part_body_done(self):
+        self.fid.close()
+        self.fid = None
+        super(FileBodyState, self).part_body_done()
 
 if __name__ == '__main__':
-    mp = Multipart(sys.stdin)
+    #mp = Multipart(sys.stdin)
+    mp = FileMultipart(sys.stdin)
+    mp.writefields['file'] = '/tmp/contents-file'
+    #mp.blocksize = 1
     while mp.readmore():
         pass
     if mp.error:
