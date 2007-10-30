76599f11c4c8c6d8fb82875870a5cacd848acfe5
gmaps alias <dstndstn@gmail.com>
2007-10-30 21:22:56 +0000

    (dstn+keir) add watcher script

---
+++ src/tilecache/an/portal/start-watcher-test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+Nthreads=4
+Cmd="/home/gmaps/test/tilecache/an/portal/watcher-script-test.py an_remote_test %s"
+
+cd /home/gmaps/test/job-queue
+rm queue
+
+/home/gmaps/test/ontheweb/execs/watcher -D -n $Nthreads -c "$Cmd"
+
