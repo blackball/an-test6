4e05425c9905ecffc6f0fe0f7e4ecabb6c4284c7
gmaps alias <dstndstn@gmail.com>
2007-11-06 00:41:29 +0000

    (dstn) start SIAP

--- src/tilecache/an/vo/urls.py
+++ src/tilecache/an/vo/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('an.vo.views',
-					   (r'^siap/$', 'siap'),
+					   #(r'^siap(-pointed)?/$', 'siap'),
+					   (r'^siap-pointed/$', 'siap_pointed'),
+					   (r'^getimage/$', 'getimage'),
                        )
--- src/tilecache/an/vo/views.py
+++ src/tilecache/an/vo/views.py
@@ -10,8 +10,33 @@ from django.template import Context, RequestContext, loader
 
 from an import gmaps_config
 from an import settings
-from an.portal.log import log
+from an.vo.log import log
 
-def siap(request):
+INT_COVERS = 0
+INT_ENCLOSED = 1
+INT_CENTER = 2
+INT_OVERLAPS = 3
+
+def siap_pointed(request):
+    try:
+        pos_str = request.GET['POS']
+        size_str = request.GET['SIZE']
+        pos = map(float, pos_str.split(','))
+        size = map(float, size_str.split(','))
+        intersect = 
+        if 'INTERSECT' in request.GET:
+        if len(pos) != 2:
+            raise ValueError('POS must contain two values (RA and Dec); got %i' % len(pos))
+        if not (len(size) in [1, 2]):
+            raise ValueError('SIZE must contain one or two values; got %i' % len(size))
+            
+
+        log('POS:', pos, 'SIZE:', size)
+        
+    except (KeyError, ValueError),e:
+        log('key error:', e)
+    return HttpResponse('not implemented')
+
+def getimage(request):
     return HttpResponse('not implemented')
 
