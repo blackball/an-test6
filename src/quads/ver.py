b313f94fa61a9b377efeec3045b486947a3cecc1
Dustin Lang <dstndstn@gmail.com>
2007-11-09 23:00:32 +0000

    add python verify-playing code and example (job test-200711-76777953)

---
+++ src/quads/ver.py
@@ -0,0 +1,64 @@
+import pyfits
+from numpy import *
+from matplotlib.pylab import figure, plot, xlabel, ylabel, loglog, clf
+from matplotlib.pylab import semilogy, show, find
+
+if __name__ == '__main__':
+
+    ixy = pyfits.open('ver/index.xy.fits')
+    ixy = ixy[1].data
+    ix = ixy.field(0)
+    iy = ixy.field(1)
+    NI = len(ix)
+    ixy = zeros((NI,2))
+    ixy[:,0] = ix
+    ixy[:,1] = iy
+
+    fxy = pyfits.open('ver/field.xy.fits')
+    fxy = fxy[1].data
+    fx = fxy.field(0)
+    fy = fxy.field(1)
+    NF = len(fx)
+    fxy = zeros((NF,2))
+    fxy[:,0] = fx
+    fxy[:,1] = fy
+
+    mf = pyfits.open('ver/match.fits')
+    mf = mf[1].data
+    quad = mf.field('quadpix')[0]
+    quad = quad[0:8].reshape(4,2)
+    qx = quad[:,0]
+    qy = quad[:,1]
+    #qc = mean(quad, axis=0)
+    #cx = qc[0]
+    #cy = qc[1]
+    cx = mean(qx)
+    cy = mean(qy)
+
+    iok = find( (ix > min(fx)) *
+                (ix < max(fx)) *
+                (iy > min(fy)) *
+                (iy < max(fy)) )
+    #ix = ix[iok]
+    okix = []
+    okiy = []
+    for i in iok:
+        okix.append(ix[i])
+        okiy.append(iy[i])
+    ix = okix
+    iy = okiy
+
+    figure(1)
+    clf()
+    I = [0, 2, 1, 3, 0]
+    plot(
+        [cx], [cy], 'ro',
+        qx[I], qy[I], 'r-',
+        ix, iy, 'rx',
+        fx, fy, 'b+',
+        )
+    #plot(ixy[:,0], ixy[:,1], 'rs',
+    #     fxy[:,0], fxy[:,1], 'b+')
+
+    
+    
