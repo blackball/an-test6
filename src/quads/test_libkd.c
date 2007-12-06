c707e4f5ac799bd9780ef9aa9d74b852c2806111
Dustin Lang <dstndstn@gmail.com>
2007-12-06 17:27:03 +0000

    test flsB()

--- src/quads/test_libkd.c
+++ src/quads/test_libkd.c
@@ -118,7 +118,8 @@ void test_2(CuTest* ct) {
 
     for (i=0; i<N; i++) {
         int L1 = node_level(i);
-        int L2 = fls(i+1) - 1;
+        //int L2 = fls(i+1) - 1;
+        int L2 = flsB(i+1);
         printf("%i %i\n", L1, L2);
     }
 }
