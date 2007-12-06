b0438728687cca04da450034ac9d0b020387e80d
Dustin Lang <dstndstn@gmail.com>
2007-12-06 17:26:09 +0000

    add flsB()

--- src/libkd/fls.h
+++ src/libkd/fls.h
@@ -14,16 +14,29 @@
 #ifndef _ASM_GENERIC_BITOPS_FLS_H_
 #define _ASM_GENERIC_BITOPS_FLS_H_
 
+#include <stdint.h>
+#include <assert.h>
+
+/**
+ * fls - find last (most-significant) bit set
+ *
+ * @x: the word to search
+ *
+ * This is defined the same way as ffs.
+ *
+ * Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
+ */
 static inline int fls(int x);
+
+/**
+ * flsB()  =  fls() - 1.
+ *
+ * Note that x MUST be > 0.
+ */
+static inline uint8_t flsB(uint32_t x);
 
 #if AN_I386
 
-/**
- * fls - find last bit set
- * @x: the word to search
- *
- * This is defined the same way as ffs().
- */
 static inline int fls(int x) {
     int r;
     __asm__("bsrl %1,%0\n\t"
@@ -33,14 +46,18 @@ static inline int fls(int x) {
     return r+1;
 } 
 
+static inline uint8_t flsB(uint32_t x) {
+    int r;
+    assert(x);
+    __asm__("bsrl %1,%0\n\t"
+            "jnz 1f\n\t"
+            "movl $-1,%0\n"
+            "1:" : "=r" (r) : "rm" (x));
+    return r;
+} 
+
 #elif AN_X86_64
 
-/**
- * fls - find last bit set
- * @x: the word to search
- *
- * This is defined the same way as ffs.
- */
 static __inline__ int fls(int x) {
     int r;
     __asm__("bsrl %1,%0\n\t"
@@ -49,19 +66,47 @@ static __inline__ int fls(int x) {
     return r+1;
 } 
 
+static inline uint8_t flsB(uint32_t x) {
+    int r;
+    assert(x);
+    __asm__("bsrl %1,%0\n\t"
+            "cmovzl %2,%0"
+            : "=&r" (r) : "rm" (x), "rm" (-1));
+    return r;
+}
+
 #else
 
-/**
- * fls - find last (most-significant) bit set
- * @x: the word to search
- *
- * This is defined the same way as ffs.
-  * Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
- */
 static inline int fls(int x) {
     int r = 32;
     if (!x)
         return 0;
+    if (!(x & 0xffff0000u)) {
+        x <<= 16;
+        r -= 16;
+    }
+    if (!(x & 0xff000000u)) {
+        x <<= 8;
+        r -= 8;
+    }
+    if (!(x & 0xf0000000u)) {
+        x <<= 4;
+        r -= 4;
+    }
+    if (!(x & 0xc0000000u)) {
+        x <<= 2;
+        r -= 2;
+    }
+    if (!(x & 0x80000000u)) {
+        x <<= 1;
+        r -= 1;
+    }
+    return r;
+}
+
+static inline uint8_t flsB(uint32_t x) {
+    int r = 31;
+    assert(x);
     if (!(x & 0xffff0000u)) {
         x <<= 16;
         r -= 16;
