a2d7b1d67369090e4b9814d3283576d4a00f7f2d
Dustin Lang <dstndstn@gmail.com>
2008-01-09 15:21:52 +0000

    simplify testing framework

--- src/quads/test_libkd.c
+++ src/quads/test_libkd.c
@@ -1,6 +1,6 @@
 /*
   This file is part of the Astrometry.net suite.
-  Copyright 2007 Dustin Lang.
+  Copyright 2007, 2008 Dustin Lang.
 
   The Astrometry.net suite is free software; you can redistribute
   it and/or modify it under the terms of the GNU General Public License
