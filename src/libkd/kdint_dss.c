9fb9f3e99803932c617fea5f04bcd720cb4c62d4
Dustin Lang <dstndstn@gmail.com>
2008-01-10 19:18:52 +0000

    update copyright

--- src/libkd/kdint_dss.c
+++ src/libkd/kdint_dss.c
@@ -1,6 +1,6 @@
 /*
   This file is part of libkd.
-  Copyright 2006, 2007 Dustin Lang and Keir Mierle.
+  Copyright 2006-2008 Dustin Lang and Keir Mierle.
 
   libkd is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
