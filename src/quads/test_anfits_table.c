9eef27c3b283f11f9cd98132beec5f78c2593a83
Dustin Lang <dstndstn@gmail.com>
2008-01-28 01:26:00 +0000

    add anfits_table test

---
+++ src/quads/test_anfits_table.c
@@ -0,0 +1,46 @@
+#include "anfits.h"
+
+#include "cutest.h"
+
+char* get_tmpfile() {
+    return "/tmp/test-anfits-1";
+}
+
+void test_one_column_write_read(CuTest* ct) {
+    anfits_table_t* tab, *outtab;
+    int i, N2;
+    int N = 6;
+    double outdata[6];
+    double* indata;
+    char* fn;
+    char* colname = "X";
+
+    fn = get_tmpfile();
+    outtab = anfits_table_new_for_writing(fn);
+    CuAssertPtrNotNull(ct, outtab);
+    
+    anfits_table_add_column(outtab, colname, ANTYPE_DOUBLE);
+    CuAssertIntEquals(ct, anfits_table_ncols(outtab), 1);
+
+    for (i=0; i<N; i++) {
+        outdata[i] = i*i;
+    }
+
+    for (i=0; i<N; i++) {
+        CuAssertIntEquals(anfits_table_write_row(outtab, outdata+i), 0);
+    }
+
+    CuAssertIntEquals(ct, anfits_table_close(outtab), 0);
+
+    tab = anfits_table_open(fn);
+    CuAssertPtrNotNull(ct, tab);
+
+    CuAssertIntEquals(ct, anfits_table_nrows(tab), N);
+
+    anfits_table_read_column(tab, colname, ANTYPE_DOUBLE, &indata);
+    CuAssertPtrNotNull(ct, indata);
+
+    CuAssertIntEquals(ct, memcmp(outdata, indata, sizeof(outdata)), 0);
+}
+
+
