a57e2c48c779fa20f2a78459fa04a8df1acc365b
Sam Roweis <roweis@cs.nyu.edu>
2008-02-05 06:33:36 +0000

    dedup using htmllink and md5

--- src/picasaweb/testPipeline.py
+++ src/picasaweb/testPipeline.py
@@ -20,19 +20,32 @@ def main():
   pws = pwInit()
   pwAuth(gdata_authtoken=atoken,gdata_user=puser)
 
-  # a dictionary, keyed by entry.id.text with value being time seen
+  # a dictionary, keyed by entry.GetHtmlLink().href value=md5smu
   masterDict = {}
+
+  currentPhotos=pws.GetFeed("http://picasaweb.google.com/data/feed/api/user/"+pws.email+"?kind=photo")
+  if currentPhotos and currentPhotos.entry:
+    print "Photos currently loaded in service account %s:" % pws.email
+    for e in currentPhotos.entry:
+      thisc=pws.GetFeed(e.GetCommentsUri()).entry[0].content.text
+      thislink=thisc[12:thisc.find(' on ')]
+      thismd5=thisc[thisc.find('md5sum=')+7:]
+      masterDict[thislink]=thismd5
+      print "  %s (md5sum=%s)" % (thislink,thismd5)
 
   while True:
     allE = getAllTagEntries(tag)
     if allE:
       for e in allE:
+        thislink=e.GetHtmlLink().href
         if pws.email in [a.email.text for a in e.author]:
           print "WARNING: tag found on image %s for user %s" % (e.id.text,pws.email)
-        elif e.id.text not in masterDict:
+        elif thislink in masterDict:
+          print "  skipping photo %s (already loaded)" % thislink
+        elif thislink not in masterDict:
           # get new urls as local files
-          masterDict[e.id.text] = time()
           (localfilename,md5sum)=downloadEntry(e,verbose=True)
+          masterDict[thislink] = md5sum
           thisalbumname=makeAlbumnameFromMD5(md5sum)
           thisphotouser=e.id.text[e.id.text.find('/user/')+6:].split('/')[0]
           thiscaptiontext="%s from %s" % (e.content.src.split('/')[-1],thisphotouser)
@@ -45,6 +58,8 @@ def main():
           thisphotoentry=uploadPhoto(localfilename,thispalbum,caption=thiscaptiontext,verbose=True)
           insertTag(thistag,thisphotoentry.gphoto_id.text,thispalbum)
           insertComment(thiscomment,thisphotoentry.gphoto_id.text,thispalbum,verbose=True)
+        else:
+          print "  UNKNOWN error in entry"
     else:
       print "No images found with tag="+tag
 
