db09a9d1ac42aa6bcb7645ea472599c754ce2c6a
Dustin Lang <dstndstn@gmail.com>
2008-02-11 02:59:36 +0000

    make xylist2fits compile again

--- src/quads/xylist2fits.c
+++ src/quads/xylist2fits.c
@@ -45,14 +45,7 @@ void print_help(char* progname) {
            progname, AN_FILETYPE_XYLS);
 }
 
-typedef pl xyarray;
-#define mk_xyarray(n)         pl_new(n)
-#define free_xyarray(l)       pl_free(l)
-#define xya_ref(l, i)    (xy*)pl_get((l), (i))
-#define xya_set(l, i, v)      pl_set((l), (i), (v))
-#define xya_size(l)           pl_size(l)
-
-static xyarray *readxysimple(char* fn);
+static xy_t* readxysimple(char* fn);
 
 extern char *optarg;
 extern int optind, opterr, optopt;
@@ -61,9 +54,8 @@ int main(int argc, char** args) {
     char* infn = NULL;
     char* outfn = NULL;
     int c;
-    xyarray* xya;
-    xylist* ls;
-    int i, N;
+    xy_t* xy;
+    xylist_t* ls;
     int doubleformat = 0;
     char* xname = NULL;
     char* yname = NULL;
@@ -113,8 +105,8 @@ int main(int argc, char** args) {
     	printf("reading input...\n");
 	}
 
-	xya = readxysimple(infn);
-    if (!xya)
+	xy = readxysimple(infn);
+    if (!xy)
         exit(-1);
 
     ls = xylist_open_for_writing(outfn);
@@ -124,7 +116,10 @@ int main(int argc, char** args) {
         xylist_set_xtype(ls, TFITS_BIN_TYPE_D);
         xylist_set_ytype(ls, TFITS_BIN_TYPE_D);
     }
-    hdr = xylist_get_header(ls);
+    if (antype)
+        ls->antype = antype;
+
+    hdr = xylist_get_primary_header(ls);
     qfits_header_add(hdr, "WRITER", basename(args[0]), "This file was written by the program...", NULL);
     boilerplate_add_fits_headers(hdr);
     fits_add_long_comment(hdr, "Command line that produced this file:");
@@ -135,49 +130,25 @@ int main(int argc, char** args) {
         xylist_set_xname(ls, xname);
     if (yname)
         xylist_set_yname(ls, yname);
-    if (xylist_write_header(ls))
+
+    if (xylist_write_primary_header(ls) ||
+        xylist_write_header(ls) ||
+        xylist_write_field(ls, xy) ||
+        xylist_fix_header(ls) ||
+        xylist_close(ls)) {
+        fprintf(stderr, "Failed to write xylist FITS file.\n");
         exit(-1);
-
-    if (antype) {
-        ls->antype = antype;
     }
-
-    N = xya_size(xya);
-	if (verbose){
-    	printf("writing %i fields...\n", N);
-	}
-    for (i=0; i<N; i++) {
-        int j, M;
-        xy* list = xya_ref(xya, i);
-        if (!list) {
-            fprintf(stderr, "list %i is null.", i);
-            continue;
-        }
-        xylist_write_new_field(ls);
-        M = xy_size(list);
-        for (j=0; j<M; j++) {
-            double vals[2];
-            vals[0] = xy_refx(list, j);
-            vals[1] = xy_refy(list, j);
-            xylist_write_entries(ls, vals, 1);
-        }
-        xylist_fix_field(ls);
-    }
-    xylist_fix_header(ls);
-    xylist_close(ls);
-
     return 0;
 }
 
 // Read a simple xy file; column 1 is x column 2 is y;
 // whitespace/comma seperated.
 // Lines starting with "#" are ignored.
-xyarray *readxysimple(char* fn) {
-    uint numxy=0;
-    xyarray *thepix = NULL;
+xy_t* readxysimple(char* fn) {
     FILE* fid;
-    int jj;
     dl* xylist;
+    xy_t* xy;
 
     fid = fopen(fn, "r");
     if (!fid) {
@@ -230,21 +201,8 @@ xyarray *readxysimple(char* fn) {
         //cursor += nread;
         dl_append(xylist, x);
         dl_append(xylist, y);
-        numxy++;
     }
-    thepix = mk_xyarray(1);
-
-    xya_set(thepix, 0, mk_xy(numxy) );
-
-    if (xya_ref(thepix, 0) == NULL) {
-        fprintf(stderr, "ERROR (readxy) - out of memory at field %u\n", 0);
-        free_xyarray(thepix);
-        return NULL;
-    }
-    for (jj = 0; jj < numxy; jj++) {
-        xy_setx(xya_ref(thepix, 0), jj, dl_get(xylist, jj*2));
-        xy_sety(xya_ref(thepix, 0), jj, dl_get(xylist, jj*2+1));
-    }
-    free(xylist);
-    return thepix;
+    xy = xy_alloc(dl_size(xylist)/2, FALSE, FALSE);
+    xy_from_dl(xy, xylist, FALSE, FALSE);
+    return xy;
 }
