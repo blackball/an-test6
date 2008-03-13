40c6532e8090d58f10f9753ccfa26325b87c5432
Dustin Lang <dstndstn@gmail.com>
2008-03-13 22:04:09 +0000

    fill in bands

--- src/quads/bands.py
+++ src/quads/bands.py
@@ -1,6 +1,57 @@
 
 # I can never remember where all the bands are...
 
+bands = [
+    #              min  max  mid +-
+    ('usnob', 'B', 350, 540, 0, 0),
+    ('usnob', 'R', 590, 690, 0, 0),
+    ('usnob', 'N', 715, 900, 0, 0),
+    ('2mass', 'J',   0,   0, 1240, 0),
+    ('2mass', 'H',   0,   0, 1600, 0),
+    ('2mass', 'Ks',  0,   0, 2160, 0),
+    ('sdss',  'u',   0,   0, 354,  57/2.),
+    ('sdss',  'g',   0,   0, 477, 137/2.),
+    ('sdss',  'r',   0,   0, 623, 137/2.),
+    ('sdss',  'i',   0,   0, 763, 153/2.),
+    ('sdss',  'z',   0,   0, 913,  95/2.),
+    ('spitzer', '1', 0,   0, 3600, 0),
+    ('spitzer', '2', 0,   0, 4500, 0),
+    ('spitzer', '3', 0,   0, 5800, 0),
+    ('spitzer', '4', 0,   0, 8000, 0),
+    ('galex',   'nuv', 135, 175, 0, 0),
+    ('galex',   'fuv', 175, 280, 0, 0),
+    ]
+
+# USNOB
+# http://www.iop.org/EJ/article/1538-3881/125/2/984/202452.tb1.html
+# (B)
+# O: 350-500 nm
+# J: 385-540 nm
+#  : 395-540
+# (R)
+# E: 620-670 nm
+# F: 610-690 nm
+#  : 630-690
+#  : 590-690
+# (N)
+# N: 730-900 nm
+#  : 715-900
+
+# 2MASS
+# http://www.ipac.caltech.edu/2mass/releases/allsky/doc/sec1_1.new.html
+# J: 1.24 um
+# H: 1.66 um
+# Ks: 2.16 um
+
+# SDSS
+# http://www.astro.princeton.edu/PBOOK/camera/camera.htm
+# 5 channels
+# u: 354 +- 57/2 nm
+# g: blue-green    477 +- 137/2 nm
+# r: red           623 +- 137/2 nm
+# i: far-red       763 +- 153/2 nm
+# z: near-infrared 913 +- 95/2 nm
+
 # Spitzer - IRAC
 # http://www.spitzer.caltech.edu/technology/irac.shtml
 # 4 channels
@@ -8,13 +59,12 @@
 # 3.6, 4.5, 5.8, and 8 microns
 # 256 x 256 pixels
 
-# SDSS
-# http://www.astro.princeton.edu/PBOOK/camera/camera.htm
-# 5 channels
-# u: 354 +- 57 nm
-# g: blue-green    477 +- 137 nm
-# r: red           623 +- 137 nm
-# i: far-red       763 +- 153 nm
-# z: near-infrared 913 +- 95 nm
+# GALEX
+# http://www.galex.caltech.edu/QUICKFACTS/quickfacts.html
+# fuv: 135 to 175 nm
+# nuv: 175 to 280 nm
 
+# HST
+# many!
+# http://www.stsci.edu/hst/acs/documents/handbooks/cycle17/c05_imaging2.html#356971
 
