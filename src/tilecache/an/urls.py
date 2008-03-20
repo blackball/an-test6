4483130e476cb9e773b0749ac6859f161a133730
gmaps alias <dstndstn@gmail.com>
2008-03-20 15:10:10 +0000

    (dstn) add browser for faint-motion results

--- src/tilecache/an/urls.py
+++ src/tilecache/an/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
                        (r'^hoggthinksimg', 'an.portal.hoggthinks.image'),
                        (r'^hoggthinks', 'an.portal.hoggthinks.form'),
                        (r'^easy-gmaps', 'an.portal.easy_gmaps.tile'),
+                       (r'^faint', 'an.portal.faint.get'),
                        #
                        (r'^$', 'an.portal.newjob.newlong'),
                        # This is a fake placeholder to allow {% url %} and reverse() to resolve an.media to /anmedia.
