3d6be0ea0bfe98889a32eb5d189d427a098e2db2
gmaps alias <dstndstn@gmail.com>
2008-11-28 21:30:01 +0000

    svn merge svn+ssh://astrometry.net/svn/branches/astrometry/alpha/ -r 6141:6519 .

--- src/ontheweb/execs/start-watcher-alpha.sh
+++ src/ontheweb/execs/start-watcher-alpha.sh
@@ -12,12 +12,14 @@ rm queue
 # We have to tell it to watch the current epoch's directory; new epochs
 # will automatically be watched when their directories appear.
 Epoch=`pwd`/`date +%Y%m`
+
 # In case the watcher is created before the current epoch's directory has been
 # created, create it now.
 mkdir -p $Epoch
 
 # This determines which filenames are handled.
 Pattern="/input\$"
+echo pattern is $Pattern
 
-/home/gmaps/alpha/ontheweb/execs/watcher -D -n $Nthreads -c "$Cmd" -w $Epoch -p $Pattern 
+/home/gmaps/alpha/ontheweb/execs/watcher -D -p $Pattern -n $Nthreads -c "$Cmd" -w $Epoch
 
