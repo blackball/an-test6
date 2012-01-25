d9b9613110ed315b6ae1fb953bd23fb9c1bbc172
Dustin Lang <dstndstn@gmail.com>
2012-01-25 18:18:52 +0000

    remove old cruft

--- src/hogg/hoggMath.h
+++
@@ -1,2 +0,0 @@
-#define PI 3.1415926535897932384626
-#define square(x) ((x)*(x))
--- src/hogg/hoggTweak.h
+++
@@ -1,7 +0,0 @@
-double hoggLogsum(double * list,int nn);
-double hoggLogGaussian(double xx, double var);
-double hoggExMaxStep(double *xx, double *varin, int nn, double varout,
-		     double *logpin);
-double hoggLeastSquareFit(double *yy, double *xx, double *aa, int nn, int mm);
-double hoggRansacTrial(double *yy, double *AA, double *xx, int nn, int mm,
-		       int *use);
